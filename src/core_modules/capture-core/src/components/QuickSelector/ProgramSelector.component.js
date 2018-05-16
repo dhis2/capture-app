b7be8f3ad7a548cb3ca4abce305fb42263cea39b
Lukas Wijgaart van Dijk <lukasewvd@gmail.com>
2018-05-16 11:09:12 +0200

    Added autocomplete field for porgram

--- src/core_modules/capture-core/src/components/QuickSelector/ProgramSelector.component.js
+++ src/core_modules/capture-core/src/components/QuickSelector/ProgramSelector.component.js
@@ -1,6 +1,10 @@
 // @flow
+/* eslint-disable */
+
 import React, { Component } from 'react';
 import { withStyles } from 'material-ui-next/styles';
+
+import  ACSelect from './AutocompleteSelect.component';
 
 import programs from 'capture-core/metaDataMemoryStores/programCollection/programCollection';
 import List, { ListItem, ListItemText } from 'material-ui-next/List';
@@ -130,7 +134,7 @@ class ProgramSelector extends Component<Props> {
                     <FormControl className={this.props.classes.form}>
                         <InputLabel htmlFor="program-selector">Program</InputLabel>
                         <Select
-                            value={this.props.selectedProgram}
+                            value={this.props.selectedProgram ? this.props.selectedProgram : ''}
                             onChange={this.handleChange}
                             inputProps={{
                                 name: 'program',
@@ -144,6 +148,7 @@ class ProgramSelector extends Component<Props> {
                         </Select>
                     </FormControl>
                     <br />
+                    <ACSelect options={programsArray} selected={this.props.selectedProgram ? this.props.selectedProgram : ''} handleChange={this.props.handleClickProgram} placeholder="Program" />
                 </Paper>
             </div>
         );
