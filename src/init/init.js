3ca801c49ce99b9b05a17cbb68cc3dff91626aa3
Viktor Varland <viktor@dhis2.org>
2018-06-12 11:30:42 +0200

    DHIS2-3996 Add support for discard dialog when user has unsaved events

--- src/init/init.js
+++ src/init/init.js
@@ -8,7 +8,6 @@ import CurrentLocaleData from 'capture-core/utils/localeData/CurrentLocaleData';
 import { setD2, getTranslation } from 'capture-core/d2/d2Instance';
 import { formatterOptions } from 'capture-core/utils/string/format.const';
 
-
 import 'moment/locale/nb';
 
 import loadMetaData from 'capture-core/metaDataStoreLoaders/baseLoader/metaDataLoader';
@@ -125,4 +124,3 @@ export async function initialize() {
     setLocaleData(uiLocale);
     await initializeMetaData(dbLocale);
 }
-
