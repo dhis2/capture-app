d689584d19f4d995248526f031a60ee437cb3695
Muhammad Adeel <imadeelshahid@gmail.com>
2018-08-07 18:17:59 +0200

    refactor: simplify isValidOrgUnit func.

--- src/core_modules/capture-core/src/utils/validators/form/orgUnit.validator.js
+++ src/core_modules/capture-core/src/utils/validators/form/orgUnit.validator.js
@@ -4,15 +4,15 @@
  *
  * @export
  * @param {string} value
- * @param {boolean} [isEmptyValid=false] By default empty is invalid. In your form you should precheck this and not call isValidUrl if empty.
+ * @param {boolean} [isEmptyValid=false] By default empty is invalid. In your form you should precheck this and not call isNumber if empty.
  * @returns
  */
 const isValidOrgUnit = (value: string, isEmptyValid: boolean = false) => {
-    if (!value) {
+    if (!isEmptyValid && !value) {
         return isEmptyValid;
     }
 
-    return value;
+    return typeof value === 'string' && value.length > 0;
 };
 
 export default isValidOrgUnit;
