c149e7826b6c3dc687580a6545822cd1ca16f83d
Muhammad Adeel <adeel@dhis2.org>
2018-08-15 14:35:27 +0200

    AgeField: use natural years, month, days instead of absolute numbers

--- src/core_modules/capture-core/src/components/FormFields/AgeField/AgeField.component.js
+++ src/core_modules/capture-core/src/components/FormFields/AgeField/AgeField.component.js
@@ -25,15 +25,51 @@ const textFieldStyle = {
     width: 60,
 };
 
-function calculatedValues(m) {
-    const days = moment().diff(m, 'days');
-    const months = moment().diff(m, 'months');
-    const years = moment().diff(m, 'years');
+function monthsDiff(d) {
+    const now = new Date();
+    let months = (now.getFullYear() - d.getFullYear()) * 12;
+    months += now.getMonth() - d.getMonth();
+
+    if (now.getDate() < d.getDate()) {
+        months -= 1;
+    }
+
+    return months;
+}
+
+function daysDiff(d) {
+    const now = new Date();
+
+    if (d.getDate() === now.getDate()) {
+        return 0;
+    }
+
+    if (now.getDate() > d.getDate()) {
+        return now.getDate() - d.getDate();
+    }
+
+    if (d.getDate() > now.getDate()) {
+        const lastDate = new Date(d.getFullYear(), d.getMonth() + 1, 0);
+        const d2 = new Date(d.getFullYear(), d.getMonth() + 1, now.getDate());
+        let diff = lastDate.getDate() - d.getDate();
+        diff += d2.getDate();
+        return diff;
+    }
+
+    return 0;
+}
+
+function calculatedValues(d) {
+    const now = new Date();
+    const years = now.getFullYear() - d.getFullYear();
+
+    const months = monthsDiff(d) % 12;
+    const days = daysDiff(d);
 
     return {
-        days: days < 0 ? '' : days,
-        months: months < 0 ? '' : months,
-        years: years < 0 ? '' : years,
+        days,
+        months,
+        years,
     };
 }
 
@@ -49,7 +85,7 @@ class D2AgeField extends Component<Props> {
       let months = '';
       let days = '';
       if (props.value) {
-          const v = calculatedValues(moment(props.value));
+          const v = calculatedValues(new Date(props.value));
           years = v.years;
           months = v.months;
           days = v.days;
@@ -67,50 +103,37 @@ class D2AgeField extends Component<Props> {
       this.props.onBlur(this.state.date);
   }
 
-  updateDate = (updateOn) => {
-      const { date, years, months, days } = this.state;
-      const m = moment(date);
+  updateDate = () => {
+      let { years, months, days } = this.state;
 
-      if (updateOn === 'years') {
-          m.year(moment().subtract(years, 'years').year());
-      } else if (updateOn === 'months') {
-          m.month(moment().subtract(months, 'months').month());
-      } else if (updateOn === 'days') {
-          m.date(moment().subtract(days, 'days').date());
-      }
+      years = years === '' ? 0 : years;
+      months = months === '' ? 0 : months;
+      days = days === '' ? 0 : days;
+
+      years = parseInt(years, 10);
+      months = parseInt(months, 10);
+      days = parseInt(days, 10);
+
+      const d = new Date();
+      d.setFullYear(d.getFullYear() - years);
+      d.setMonth(d.getMonth() - months);
+      d.setDate(d.getDate() - days);
 
       this.setState({
-          ...calculatedValues(m),
-          date: m.format('MM/DD/YYYY'),
+          months: months % 12,
+          days: daysDiff(d),
+          date: moment(d).format('MM/DD/YYYY'),
       }, this.emitChange);
   }
 
-  onYearsChange = (evt) => {
-      const years = parseInt(evt.target.value, 10);
-      if (!isNaN(years) && years >= 0) {
-          this.setState({ years }, () => this.updateDate('years'));
-      }
-  }
-
-  onMonthsChange = (evt) => {
-      const months = parseInt(evt.target.value, 10);
-      if (!isNaN(months) && months >= 0) {
-          this.setState({ months }, () => this.updateDate('months'));
-      }
-  }
-
-  onDaysChange = (evt) => {
-      const days = parseInt(evt.target.value, 10);
-      if (!isNaN(days) && days >= 0) {
-          this.setState({ days }, () => this.updateDate('days'));
-      }
-  }
+  onYearsChange = evt => this.setState({ years: evt.target.value })
+  onMonthsChange = evt => this.setState({ months: evt.target.value })
+  onDaysChange = evt => this.setState({ days: evt.target.value })
 
   handleCalendarBlur = (date) => {
-      const m = moment(date);
       this.setState({
           date,
-          ...calculatedValues(m),
+          ...calculatedValues(new Date(date)),
       }, this.emitChange);
   }
 
@@ -136,6 +159,7 @@ class D2AgeField extends Component<Props> {
                   type="text"
                   label={i18n.t('Years')}
                   value={this.state.years}
+                  onBlur={this.updateDate}
                   onChange={this.onYearsChange}
               />
               <TextField
@@ -143,6 +167,7 @@ class D2AgeField extends Component<Props> {
                   type="text"
                   label={i18n.t('Months')}
                   value={this.state.months}
+                  onBlur={this.updateDate}
                   onChange={this.onMonthsChange}
               />
               <TextField
@@ -150,6 +175,7 @@ class D2AgeField extends Component<Props> {
                   type="text"
                   label={i18n.t('Days')}
                   value={this.state.days}
+                  onBlur={this.updateDate}
                   onChange={this.onDaysChange}
               />
           </div>
