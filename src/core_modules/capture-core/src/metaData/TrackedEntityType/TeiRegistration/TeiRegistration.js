5efb677647bb715d4cb88eba8ec1085869a6723a
Joakim Storløkken Melseth <joakim.melseth@gmail.com>
2019-05-13 13:10:19 +0200

    Fix: DHIS2-6923 tei registration fixes (#110)
    - Filter program list on tracked entity type
    - Show the correct tracked entity type in the button text

--- src/core_modules/capture-core/src/metaData/TrackedEntityType/TeiRegistration/TeiRegistration.js
+++ src/core_modules/capture-core/src/metaData/TrackedEntityType/TeiRegistration/TeiRegistration.js
@@ -3,10 +3,12 @@
 import isFunction from 'd2-utilizr/src/isFunction';
 import { RenderFoundation } from '../../RenderFoundation';
 import { InputSearchGroup } from '../../InputSearchGroup';
+import { TrackedEntityType } from '../../TrackedEntityType';
 
 export default class TeiRegistration {
     _form: RenderFoundation;
     _inputSearchGroups: ?Array<InputSearchGroup>;
+    _trackedEntityType: TrackedEntityType;
 
     constructor(initFn: ?(_this: TeiRegistration) => void) {
         initFn && isFunction(initFn) && initFn(this);
@@ -24,5 +26,12 @@ export default class TeiRegistration {
     }
     get inputSearchGroups(): ?Array<InputSearchGroup> {
         return this._inputSearchGroups;
+    }
+
+    set trackedEntityType(type: TrackedEntityType) {
+        this._trackedEntityType = type;
+    }
+    get trackedEntityType(): TrackedEntityType {
+        return this._trackedEntityType;
     }
 }
