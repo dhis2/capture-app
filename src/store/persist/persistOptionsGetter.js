40c5153677ad48225c7c5a6ac3ade8f1c10944a5
Joakim <joakim.melseth@gmail.com>
2019-06-25 10:57:01 +0200

    fix: DHIS2-6273 Specify object stores to persist

--- src/store/persist/persistOptionsGetter.js
+++ src/store/persist/persistOptionsGetter.js
@@ -4,5 +4,6 @@ import getCustomStorage from './storage/customStorageGetter';
 export default function getPersistOptions() {
     return {
         storage: getCustomStorage(),
+        whitelist: ['offline', 'networkStatus'],
     };
 }
