edf4c8a3ae924fd191ba8eb657c519dcd77fae5e
Joakim Storløkken Melseth <joakim@dhis2.org>
2019-09-13 14:21:31 +0200

    fix: scroll not working for "assigned to" filter (#131)

---
+++ src/core_modules/capture-core/components/FormFields/UserField/SearchSuggestionsUp.component.js
@@ -0,0 +1,55 @@
+// @flow
+import * as React from 'react';
+import SearchContext from './Search.context';
+import defaultClasses from './searchSuggestionsUp.module.css';
+
+type Props = {
+    children: React.Node,
+    onHighlightNext: () => void,
+    onHighlightPrev: () => void,
+};
+
+const SearchSuggestionsUp = (props: Props) => {
+    const { children, onHighlightNext, onHighlightPrev } = props;
+    const listUpEl = React.useRef(null);
+    React.useLayoutEffect(() => {
+        if (listUpEl.current) {
+            listUpEl.current.scrollTop = listUpEl.current.scrollHeight;
+        }
+    }, []);
+    const { suggestionName } = React.useContext(SearchContext);
+    const handleKeyDown = React.useCallback((event) => {
+        if (event.keyCode === 38) {
+            onHighlightNext();
+            event.stopPropagation();
+            event.preventDefault();
+        } else if (event.keyCode === 40) {
+            onHighlightPrev();
+            event.stopPropagation();
+            event.preventDefault();
+        }
+    }, [onHighlightNext, onHighlightPrev]);
+
+    return (
+        <div
+            name={suggestionName}
+            tabIndex={-1}
+            onKeyDown={handleKeyDown}
+            role={'none'}
+        >
+            <div
+                ref={listUpEl}
+                className={defaultClasses.listUp}
+            >
+                <div
+                    name={suggestionName}
+                    className={defaultClasses.listUpInner}
+                >
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SearchSuggestionsUp;
