b4d8563c719ce0ac44f4df236b6dfeef5b9f1374
Joakim Storløkken Melseth <joakim.melseth@gmail.com>
2019-09-20 13:37:08 +0200

    fix: event list downloads (#134) (#135)

--- src/core_modules/capture-core/components/Pages/MainPage/EventsList/ListWrapper/DownloadDialog.container.js
+++ src/core_modules/capture-core/components/Pages/MainPage/EventsList/ListWrapper/DownloadDialog.container.js
@@ -1,11 +1,16 @@
 // @flow
 import { connect } from 'react-redux';
 import DownloadDialog from './DownloadDialog.component';
+import { makeProgramStageIdSelector } from './downloadDialog.selectors';
 
-// TODO: Change programId to current programStageId
-const mapStateToProps = (state: Object, props: { listId: string }) => ({
-    request: state.workingLists[props.listId] && state.workingLists[props.listId].currentRequest,
-});
+const mapStateToProps = () => {
+    const programStageIdSelector = makeProgramStageIdSelector();
+
+    return (state: Object, props: { listId: string }) => ({
+        request: state.workingLists[props.listId] && state.workingLists[props.listId].currentRequest,
+        programStageId: programStageIdSelector(state),
+    });
+};
 
 // $FlowSuppress
 export default connect(mapStateToProps)(DownloadDialog);
---
+++ src/core_modules/capture-core/components/Pages/MainPage/EventsList/ListWrapper/downloadDialog.selectors.js
@@ -0,0 +1,13 @@
+// @flow
+import { createSelector } from 'reselect';
+import { getEventProgramThrowIfNotFound } from '../../../../../metaData';
+
+const getProgramId = state => state.currentSelections.programId;
+// $FlowFixMe
+export const makeProgramStageIdSelector = () => createSelector(
+    getProgramId,
+    (programId) => {
+        const eventProgram = getEventProgramThrowIfNotFound(programId);
+        return eventProgram.stage.id;
+    }
+);
