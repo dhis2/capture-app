353502be7ea3e745e3754ee24ac481256dd11482
JoakimSM <joakim.melseth@gmail.com>
2019-10-11 16:53:45 +0200

    fix: add placeholder to date and age field

--- src/core_modules/capture-ui/internal/AgeInput/AgeDateInput.component.js
+++ src/core_modules/capture-ui/internal/AgeInput/AgeDateInput.component.js
@@ -1,7 +1,6 @@
 // @flow
 import React, { Component } from 'react';
 import moment from 'moment';
-import i18n from '@dhis2/d2-i18n';
 import UIDate from '../../DateAndTimeFields/DateField/Date.component';
 import orientations from '../../constants/orientations.const';
 import withFocusSaver from '../../HOC/withFocusSaver';
@@ -19,7 +18,6 @@ class AgeDateInput extends Component<Props> {
         return (
             <UIDate
                 value={value || ''}
-                placeholder={i18n.t('mm/dd/yyyy')}
                 calendarMaxMoment={moment()}
                 width={150}
                 calendarWidth={350}
