09c9dde43931ed85c51e7b47919c429879743532
Joakim Storløkken Melseth <joakim@dhis2.org>
2019-11-08 11:14:49 +0100

    fix: DHIS2-7749 use IndexedDB storage only

---
+++ src/core_modules/capture-core/utils/exceptions/DisplayException.js
@@ -0,0 +1,6 @@
+// @flow
+export function DisplayException(message: string, innerError: any) {
+    this.message = message;
+    this.innerError = innerError;
+    this.toString = () => this.message;
+}
