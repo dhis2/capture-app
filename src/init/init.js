4bb5b7576c11e0465cfee9d682816d618ec4fa59
Joakim Storløkken Melseth <joakim@dhis2.org>
2019-11-13 12:35:10 +0100

    feat: DHIS2-7174 Improve cache versioning and facilitate downgrade (#249) (cherry picked from commit f24cebe78c6ecf458cf950a5ab55d249fc2703bd)

--- src/init/init.js
+++ src/init/init.js
@@ -170,7 +170,6 @@ export async function initializeAsync() {
     } catch (error) {
         throw new DisplayException(i18n.t('Possible reason: The browser or mode (e.g. privacy mode) is not supported. See log for details.'), error);
     }
-    await initControllersAsync();
 
     // set locale data
     const uiLocale = userSettings.keyUiLocale;
