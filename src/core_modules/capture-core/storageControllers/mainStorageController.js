f24cebe78c6ecf458cf950a5ab55d249fc2703bd
Joakim Storløkken Melseth <joakim@dhis2.org>
2019-11-13 12:35:10 +0100

    feat: DHIS2-7174 Improve cache versioning and facilitate downgrade (#249)

--- src/core_modules/capture-core/storageControllers/mainStorageController.js
+++ src/core_modules/capture-core/storageControllers/mainStorageController.js
@@ -4,16 +4,23 @@ import { mainStores } from './stores';
 
 const MAIN_STORAGE_KEY = 'dhis2ca';
 
-function getCacheVersion() {
+function getMajorCacheVersion() {
+    const appVersion = process.env.REACT_APP_VERSION;
+    // $FlowSuppress: Prechecked
+    const appMajorVersion = Number(appVersion.split('.')[0]);
+    return (appMajorVersion - 30) * 1000;
+}
+
+function getMinorCacheVersion() {
     const appCacheVersionAsString = process.env.REACT_APP_CACHE_VERSION;
-    if (!appCacheVersionAsString) {
-        throw new Error('cache version not specified');
-    }
     const appCacheVersion = Number(appCacheVersionAsString);
-    if (Number.isNaN(appCacheVersion) || !Number.isSafeInteger(appCacheVersion)) {
-        throw new Error('invalid cache version');
-    }
     return appCacheVersion;
+}
+
+function getCacheVersion() {
+    const majorCacheVersion = getMajorCacheVersion();
+    const minorCacheVersion = getMinorCacheVersion();
+    return (majorCacheVersion + minorCacheVersion);
 }
 
 function createStorageController(storageName: string, AdapterClasses: Array<any>) {
