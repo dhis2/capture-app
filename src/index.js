0501a0e40a253e4c49e04e2e6acc718f662c64c9
paschalidi <paschalidi@outlook.com>
2020-04-09 21:28:05 +0200

    refactor: adds import of typeface-roboto in the init file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'capture-core-utils/extensions/asyncForEachArray';
 import 'capture-core-utils/extensions/arrayToHashMap';
 import './locales';
+import 'typeface-roboto';
 import './init/entry';
