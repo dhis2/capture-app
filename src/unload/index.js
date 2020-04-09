0fce6765d05b04704300f68a08e4aca933dcaa39
paschalidi <paschalidi@outlook.com>
2020-04-09 21:34:59 +0200

    fix: removes debugger

--- src/unload/index.js
+++ src/unload/index.js
@@ -4,7 +4,6 @@ import cleanUpCommon from 'capture-core/cleanUp/cleanUp';
 export default function addBeforeUnloadEventListener(store: ReduxStore) {
     window.addEventListener('beforeunload', async (e) => {
         cleanUpCommon(store);
-        debugger
         if (store.getState().offline.outbox.length > 0) {
             const msg = 'Unsaved events!';
             e.returnValue = msg;
