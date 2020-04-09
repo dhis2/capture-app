f359853f20cde742cb588f1d93420458aa08e0e7
paschalidi <paschalidi@outlook.com>
2020-04-09 21:31:07 +0200

    refactor: exports directly from index

--- src/unload/index.js
+++ src/unload/index.js
@@ -1,2 +1,15 @@
 // @flow
-export { default as addBeforeUnloadEventListener } from './addBeforeUnloadEventListener';
+import cleanUpCommon from 'capture-core/cleanUp/cleanUp';
+
+export default function addBeforeUnloadEventListener(store: ReduxStore) {
+    window.addEventListener('beforeunload', async (e) => {
+        cleanUpCommon(store);
+        debugger
+        if (store.getState().offline.outbox.length > 0) {
+            const msg = 'Unsaved events!';
+            e.returnValue = msg;
+            return msg;
+        }
+        return undefined;
+    });
+}
