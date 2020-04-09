0b1199a4e9b4fc3c072640f33296bbc5eeaed90f
paschalidi <paschalidi@outlook.com>
2020-04-09 21:28:51 +0200

    refactor: reorders getStore

--- src/store/getStore.js
+++ src/store/getStore.js
@@ -7,10 +7,8 @@ import { createLogger } from 'redux-logger';
 import { connectRouter, routerMiddleware } from 'connected-react-router';
 import { buildReducersFromDescriptions } from 'capture-core/trackerRedux/trackerReducer';
 import environments from 'capture-core/constants/environments';
-
 import type { BrowserHistory } from 'history/createBrowserHistory';
 import type { HashHistory } from 'history/createHashHistory';
-
 import { createOffline } from '@redux-offline/redux-offline';
 import offlineConfig from '@redux-offline/redux-offline/lib/defaults';
 import { effectConfig, discardConfig, queueConfig } from 'capture-core/trackerOffline/trackerOfflineConfig';
@@ -22,13 +20,7 @@ import epics from '../epics/trackerCapture.epics';
 
 export default function getStore(history: BrowserHistory | HashHistory, onRehydrated: () => void) {
     const epicMiddleware = createEpicMiddleware();
-    const middleWares = [epicMiddleware, routerMiddleware(history)];
 
-    if (process.env.NODE_ENV !== environments.prod) {
-        middleWares.push(
-            createLogger({ }),
-        );
-    }
 
     const reducersFromDescriptions = buildReducersFromDescriptions(reducerDescriptions);
 
@@ -37,12 +29,12 @@ export default function getStore(history: BrowserHistory | HashHistory, onRehydr
         router: connectRouter(history),
     });
 
+    // https://github.com/redux-offline/redux-offline/blob/32e4c98ec782672347b42a7936631df5c6340b77/docs/api/config.md
     const {
         middleware: offlineMiddleware,
         enhanceReducer: offlineEnhanceReducer,
         enhanceStore: offlineEnhanceStore,
-    } = createOffline({
-        ...offlineConfig,
+    } = createOffline({ ...offlineConfig,
         discard: discardConfig,
         effect: effectConfig,
         persistCallback: onRehydrated,
@@ -50,14 +42,17 @@ export default function getStore(history: BrowserHistory | HashHistory, onRehydr
         persistOptions: getPersistOptions(),
     });
 
+    const middleWares = [epicMiddleware, routerMiddleware(history), offlineMiddleware];
+    if (process.env.NODE_ENV !== environments.prod) {
+        middleWares.push(createLogger({}));
+    }
+
     const store = createStore(
-        enableBatching(offlineEnhanceReducer(rootReducer)), composeWithDevTools(
+        enableBatching(offlineEnhanceReducer(rootReducer)),
+        composeWithDevTools(
             compose(
                 offlineEnhanceStore,
-                applyMiddleware(
-                    ...middleWares,
-                    offlineMiddleware,
-                ),
+                applyMiddleware(...middleWares),
             ),
         ),
     );
