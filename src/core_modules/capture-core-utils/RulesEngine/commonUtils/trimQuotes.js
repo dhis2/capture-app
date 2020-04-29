07ee3f1e48cdd08ee195c65edcfec5846dcfc201
paschalidi <paschalidi@outlook.com>
2020-04-29 12:23:47 +0200

    refactor: trimQuotes check if input is string and if it exists

--- src/core_modules/capture-core-utils/RulesEngine/commonUtils/trimQuotes.js
+++ src/core_modules/capture-core-utils/RulesEngine/commonUtils/trimQuotes.js
@@ -1,17 +1,22 @@
 // @flow
 
+import isString from 'd2-utilizr/lib/isString';
+
 export default function trimQuotes(input: string) {
-    let trimmingComplete = false;
-    let beingTrimmed = input;
+    if (input && isString(input)) {
+        let trimmingComplete = false;
+        let beingTrimmed = input;
 
-    while (!trimmingComplete) {
-        const beforeTrimming = beingTrimmed;
-        beingTrimmed = beingTrimmed.replace(/^'/, '').replace(/'$/, '');
-        beingTrimmed = beingTrimmed.replace(/^"/, '').replace(/"$/, '');
+        while (!trimmingComplete) {
+            const beforeTrimming = beingTrimmed;
+            beingTrimmed = beingTrimmed.replace(/^'/, '').replace(/'$/, '');
+            beingTrimmed = beingTrimmed.replace(/^"/, '').replace(/"$/, '');
 
-        if (beforeTrimming.length === beingTrimmed.length) {
-            trimmingComplete = true;
+            if (beforeTrimming.length === beingTrimmed.length) {
+                trimmingComplete = true;
+            }
         }
+        return beingTrimmed;
     }
-    return beingTrimmed;
+    return input;
 }
