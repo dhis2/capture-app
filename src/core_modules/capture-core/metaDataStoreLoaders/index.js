e61262a87f596d3e5f1f49c99dfcfbad26375a47
JoakimSM <joakim.melseth@gmail.com>
2020-05-18 17:49:16 +0200

    refactor: loadMetaData files and function names

--- src/core_modules/capture-core/metaDataStoreLoaders/index.js
+++ src/core_modules/capture-core/metaDataStoreLoaders/index.js
@@ -1,3 +1,3 @@
 // @flow
-export { loadWithContext as loadMetaData } from './baseLoader';
+export { loadMetaData } from './baseLoader';
 export { loadSystemSettings } from './systemSettings/loadSystemSetttings';
