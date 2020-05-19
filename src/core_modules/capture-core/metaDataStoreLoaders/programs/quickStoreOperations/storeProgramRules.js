7487e67aa94994ca4b98a950edd82c398ff076d5
JoakimSM <joakim.melseth@gmail.com>
2020-05-19 22:58:18 +0200

    refactor: add comment

--- src/core_modules/capture-core/metaDataStoreLoaders/programs/quickStoreOperations/storeProgramRules.js
+++ src/core_modules/capture-core/metaDataStoreLoaders/programs/quickStoreOperations/storeProgramRules.js
@@ -44,6 +44,7 @@ const convert = (() => {
         return apiProgramRules
             .map(apiProgramRule => ({
                 ...apiProgramRule,
+                // Adding the program id and program stage id directly to the main object instead of using the container object with id as the only property
                 program: undefined,
                 programStage: undefined,
                 programId: apiProgramRule.program && apiProgramRule.program.id,
