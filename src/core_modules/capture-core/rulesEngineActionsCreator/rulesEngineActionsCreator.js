428fea27b8ddc6a13dbc32dbf27d14eaf6966b6e
paschalidi <paschalidi@outlook.com>
2020-05-20 21:49:23 +0200

    chore: fix rebase

--- src/core_modules/capture-core/rulesEngineActionsCreator/rulesEngineActionsCreator.js
+++ src/core_modules/capture-core/rulesEngineActionsCreator/rulesEngineActionsCreator.js
@@ -37,7 +37,6 @@ export function getRulesActionsForEvent(
     const rulesEffects = runRulesForSingleEvent(
         program,
         foundation,
-        formId,
         orgUnit,
         currentEvent,
         allEventsData,
