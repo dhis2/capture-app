c3bf556d39bc613603b96fcdd55567ffc7685361
Christos Paschalidis <paschalidi@outlook.com>
2020-05-22 14:40:15 +0200

    Merge pull request #684 from dhis2/cp/refactors-rules-engine/adds-hasValue-addDays-and-removes-snapshots
    chore: adds 3 last PRs

--- src/core_modules/capture-core-utils/RulesEngine/d2Functions/index.js
+++ src/core_modules/capture-core-utils/RulesEngine/d2Functions/index.js
@@ -52,7 +52,11 @@ export default function d2Functions(dateUtils, variableService, variablesHash) {
         'd2:addDays': {
             name: 'd2:addDays',
             parameters: 2,
-            dhisFunction: (params) => {},
+            dhisFunction: (params) => {
+                const date = params[0];
+                const daysToAdd = params[1];
+                return dateUtils.addDays(date, daysToAdd);
+            },
         },
         'd2:zing': {
             name: 'd2:zing',
@@ -150,13 +154,13 @@ export default function d2Functions(dateUtils, variableService, variablesHash) {
             dhisFunction: (params) => {
                 const variableName = params[0];
                 const variableObject = variablesHash[variableName];
-                let valueFound = false;
-                if (variableObject) {
-                    if (variableObject.hasValue) {
-                        valueFound = true;
-                    }
-                } else {
+                if (!variableObject) {
                     log.warn(`could not find variable to check if has value: ${variableName}`);
+                }
+
+                let valueFound = false;
+                if (variableObject && variableObject.hasValue) {
+                    valueFound = true;
                 }
                 return valueFound;
             },
