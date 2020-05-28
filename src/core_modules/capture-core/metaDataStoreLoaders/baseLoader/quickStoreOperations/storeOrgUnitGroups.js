150aebb6f2211096ed8ea8f5bf1d8a8ae8411214
JoakimSM <joakim.melseth@gmail.com>
2020-05-28 16:58:25 +0200

    chore: comment

--- src/core_modules/capture-core/metaDataStoreLoaders/baseLoader/quickStoreOperations/storeOrgUnitGroups.js
+++ src/core_modules/capture-core/metaDataStoreLoaders/baseLoader/quickStoreOperations/storeOrgUnitGroups.js
@@ -8,6 +8,8 @@ const convert = response =>
     response.organisationUnitGroups
         .map(group => ({
             ...group,
+            // Adding the organisation unit ids directly to the main object instead of using the container object with id as the only property
+            // The reason being that we don't want the container object to be stored in IndexedDB.
             organisationUnits: undefined,
             organisationUnitIds: (group.organisationUnits || [])
                 .map(orgUnit => orgUnit.id),
