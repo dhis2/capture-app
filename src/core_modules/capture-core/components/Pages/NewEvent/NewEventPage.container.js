c80b4fe9e02df5ce797e84e4890d6185c77a1660
Christos Paschalidis <paschalidi@outlook.com>
2020-06-08 19:06:41 +0200

    fix: inProgress

--- src/core_modules/capture-core/components/Pages/NewEvent/NewEventPage.container.js
+++ src/core_modules/capture-core/components/Pages/NewEvent/NewEventPage.container.js
@@ -4,8 +4,9 @@ import NewEventPage from './NewEventPage.component';
 import dataEntryHasChanges from '../../DataEntry/common/dataEntryHasChanges';
 
 const mapStateToProps = (state: ReduxState) => {
-    const isUserInteractionInProgress = state.currentSelections.complete &&
-      dataEntryHasChanges(state, 'singleEvent-newEvent') &&
+    const isUserInteractionInProgress =
+      (state.currentSelections.complete &&
+      dataEntryHasChanges(state, 'singleEvent-newEvent')) ||
       state.newEventPage.showAddRelationship;
     return {
         isSelectionsComplete: !!state.currentSelections.complete,
