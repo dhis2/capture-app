92b0bebc4fdd68963fb118ac008324d4776eda3c
Christos Paschalidis <paschalidi@outlook.com>
2020-06-08 14:28:33 +0200

    chore: adds missing export

--- src/core_modules/capture-core/components/LockedSelector/index.js
+++ src/core_modules/capture-core/components/LockedSelector/index.js
@@ -1,6 +1,7 @@
 export {
     searchPageSelectorBatchActionTypes,
     lockedSelectorActionTypes,
+    updateSearchSelectionsFromUrl,
     resetOrgUnitIdFromSearchPage,
     setOrgUnitFromSearchPage,
     setProgramIdFromSearchPage,
