f390eaeb64a7f44ef033c9f6954268df282ff1a2
Christos Paschalidis <paschalidi@outlook.com>
2020-06-11 13:10:15 +0200

    feat: adds the SingleSelect Menu with groups

--- src/core_modules/capture-core/components/Pages/Search/SearchPage.js
+++ src/core_modules/capture-core/components/Pages/Search/SearchPage.js
@@ -1,5 +1,5 @@
 // @flow
-import React from 'react';
+import React, { useState } from 'react';
 import i18n from '@dhis2/d2-i18n';
 import { LockedSelector } from '../../LockedSelector';
 import Paper from '@material-ui/core/Paper/Paper';
@@ -17,22 +17,24 @@ const getStyles = (theme: Theme) => ({
         paddingLeft: 8,
     },
     dataEntryPaper: {
-        marginBottom: theme.typography.pxToRem(10),
+        // marginBottom: theme.typography.pxToRem(10),
         padding: theme.typography.pxToRem(10),
+        marginBottom: 2000,
+
     },
 });
 
 type Props = {
   classes: {
-    headerContainer: string,
+    container: string,
     header: string,
     dataEntryPaper: string,
-    showAllEvents: string,
   },
 }
 
-const SearchPage = ({ classes }: Props) => (
-    <>
+const SearchPage = ({ classes }: Props) => {
+    const [selectedOption, choseSelected] = useState();
+    return (<>
         <LockedSelector />
         <div className={classes.container}>
             <Paper className={classes.dataEntryPaper}>
@@ -40,23 +42,25 @@ const SearchPage = ({ classes }: Props) => (
                     {i18n.t('Search')}
                 </div>
 
-                <SingleSelect className="select">
-                    <optgroup label="Firefox">
-                        <option value="2.0 or higher">
-                            Firefox 2.0 or higher
-                        </option>
-                        <option value="1.5.x">Firefox 1.5.x</option>
-                        <option value="1.0.x">Firefox 1.0.x</option>
-                    </optgroup>
-                    <optgroup label="Microsoft Internet Explorer">
-                        <SingleSelectOption value="1" label="option one" />
-                        <SingleSelectOption value="2" label="option two" />
-                        <SingleSelectOption value="3" label="option three" />
-                    </optgroup>
+                <SingleSelect onChange={({ selected }) => { choseSelected(selected); }} selected={selectedOption} >
+                    <h5>Group 1</h5>
+                    <SingleSelectOption value="1" label="option one" />
+                    <SingleSelectOption value="2" label="option two" />
+                    <SingleSelectOption value="3" label="option three" />
+                    <SingleSelectOption value="4" label="option four" />
+                    <SingleSelectOption value="5" label="option five" />
+                    <SingleSelectOption value="6" label="option six" />
+                    <h5>Group 2</h5>
+                    <SingleSelectOption value="7" label="option seven" />
+                    <SingleSelectOption value="8" label="option eight" />
+                    <SingleSelectOption value="9" label="option nine" />
+                    <SingleSelectOption value="10" label="option ten" />
+                    <SingleSelectOption value="11" label="option eleven" />
+                    <SingleSelectOption value="12" label="option twelve" />
                 </SingleSelect>
             </Paper>
         </div>
-    </>
-);
+    </>);
+};
 
 export default withStyles(getStyles)(SearchPage);
