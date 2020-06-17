730d1350be5cc5bec49228cddb89ef3eaaa6f7fb
paschalidi <paschalidi@outlook.com>
2020-06-17 16:25:25 +0200

    chore: reverts

---
+++ flow/rxjs.js
@@ -0,0 +1,25 @@
+// @flow
+
+declare module 'rxjs/add/operator/concatMap' {
+    declare module.exports: any;
+}
+
+declare module 'rxjs/add/operator/switchMap' {
+    declare module.exports: any;
+}
+
+declare module 'rxjs/add/operator/filter' {
+    declare module.exports: any;
+}
+
+declare module 'rxjs/add/operator/map' {
+    declare module.exports: any;
+}
+
+declare module 'rxjs/add/operator/takeUntil' {
+    declare module.exports: any;
+}
+
+declare module 'rxjs/observable/fromPromise' {
+    declare module.exports: any;
+}
