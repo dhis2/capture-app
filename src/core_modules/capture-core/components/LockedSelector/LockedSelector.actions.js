2e42e5218270cba25c4da0ab9b13b1cf6c4de764
paschalidi <paschalidi@outlook.com>
2020-06-26 10:07:59 +0200

    chore: updates comment

--- src/core_modules/capture-core/components/LockedSelector/LockedSelector.actions.js
+++ src/core_modules/capture-core/components/LockedSelector/LockedSelector.actions.js
@@ -30,7 +30,6 @@ export const setOrgUnitFromLockedSelector = (id: string, orgUnit: Object) => act
 export const setProgramIdFromLockedSelector = (id: string) => actionCreator(lockedSelectorActionTypes.PROGRAM_ID_SET)(id);
 export const setCategoryOptionFromLockedSelector = (categoryId: string, categoryOption: Object) => actionCreator(lockedSelectorActionTypes.CATEGORY_OPTION_SET)({ categoryId, categoryOption });
 
-
 export const resetOrgUnitIdFromLockedSelector = () => actionCreator(lockedSelectorActionTypes.ORG_UNIT_ID_RESET)();
 export const resetProgramIdFromLockedSelector = () => actionCreator(lockedSelectorActionTypes.PROGRAM_ID_RESET)();
 export const resetCategoryOptionFromLockedSelector = (categoryId: string) => actionCreator(lockedSelectorActionTypes.CATEGORY_OPTION_RESET)({ categoryId });
@@ -39,7 +38,8 @@ export const resetAllCategoryOptionsFromLockedSelector = () => actionCreator(loc
 export const openNewEventPageFromLockedSelector = (programId: string, orgUnitId: string) => actionCreator(lockedSelectorActionTypes.NEW_EVENT_OPEN)({ programId, orgUnitId });
 
 
-// url specific
+// these actions are being triggered only when the user updates the url from the url bar.
+// this way we keep our stored data in sync with the page the user is.
 export const updateSelectionsFromUrl = (data: Object) => actionCreator(lockedSelectorActionTypes.SELECTIONS_FROM_URL_UPDATE)(data);
 export const validSelectionsFromUrl = () => actionCreator(lockedSelectorActionTypes.SELECTIONS_FROM_URL_VALID)();
 export const invalidSelectionsFromUrl = (error: string) => actionCreator(lockedSelectorActionTypes.SELECTIONS_FROM_URL_INVALID)({ error });
