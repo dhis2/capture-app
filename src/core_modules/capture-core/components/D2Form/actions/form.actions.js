ecbe83e5317100f71d59b6b6732f91e074647970
paschalidi <paschalidi@outlook.com>
2020-06-29 13:24:08 +0200

    chore: annotates strongly the object

--- src/core_modules/capture-core/components/D2Form/actions/form.actions.js
+++ src/core_modules/capture-core/components/D2Form/actions/form.actions.js
@@ -5,6 +5,6 @@ export const actionTypes = {
     ADD_FORM_DATA: 'AddFormData',
 };
 
-export function addFormData(formId: string, formValues: Object) {
+export function addFormData(formId: string, formValues: $Shape<{||}> = {}) {
     return actionCreator(actionTypes.ADD_FORM_DATA)({ formValues, formId });
 }
