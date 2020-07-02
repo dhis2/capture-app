4aa04d32d5d2375d8b2156e27e64078af15e6122
paschalidi <paschalidi@outlook.com>
2020-07-02 14:47:47 +0200

    chore: adds flow-typed

---
+++ flow-typed/npm/loglevel_v1.x.x.js
@@ -0,0 +1,44 @@
+// flow-typed signature: eee5db07c56fea4e90d44e172a9e6712
+// flow-typed version: c6154227d1/loglevel_v1.x.x/flow_>=v0.104.x
+
+declare module "loglevel" {
+  declare type LevelName = "trace" | "debug" | "info" | "warn" | "error";
+  declare type LevelValue = 0 | 1 | 2 | 3 | 4 | 5;
+  declare type Level = LevelValue | LevelName;
+
+  declare type Logger = {
+    getLevel(): LevelValue,
+    setLevel(level: Level, persist?: boolean): void,
+    setDefaultLevel(level: Level): void,
+    enableAll(): void,
+    disableAll(): void,
+    trace(...args: Array<any>): void,
+    debug(...args: Array<any>): void,
+    info(...args: Array<any>): void,
+    warn(...args: Array<any>): void,
+    error(...args: Array<any>): void,
+    methodFactory: (
+      methodName: LevelName,
+      logLevel: LevelValue,
+      loggerName: string
+    ) => () => void,
+    ...
+  };
+
+  declare type LogLevelModule = Logger & {
+    noConflict(): LogLevelModule,
+    getLogger(name: string): Logger,
+    levels: {
+      TRACE: 0,
+      DEBUG: 1,
+      INFO: 2,
+      WARN: 3,
+      ERROR: 4,
+      SILENT: 5,
+      ...
+    },
+    ...
+  };
+
+  declare module.exports: LogLevelModule;
+}
