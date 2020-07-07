b1fba630d61244fdbb1739063a5e098e159bd5c1
paschalidi <paschalidi@outlook.com>
2020-07-07 16:20:00 +0100

    chore: adds simple tests for LockedSelector

--- cypress/integration/LockedSelector.feature
+++ cypress/integration/LockedSelector.feature
@@ -1,5 +1,13 @@
 Feature: using the LockedSelector to navigate
 
-Scenario: While you are in the main page
-  Given click the "New" button to add a new event
-  Then you should see text with info
+Scenario: There is text in the page which explains to the user that they need to select org unit and program to get started
+
+  Given you are in the main page with no selections made
+  When you click the "New" button to add a new event
+  Then text should inform you to select both organisation unit and program
+
+Scenario: There is text in the page which explains to the user that they need to select a program to get started
+
+  Given you are in the main page with organisation unit selected
+  When you click the "New" button to add a new event
+  Then text should inform you to select program
--- cypress/integration/LockedSelector/index.js
+++ cypress/integration/LockedSelector/index.js
@@ -1,15 +1,30 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
+import { Given, Then, When } from 'cypress-cucumber-preprocessor/steps';
 
-Given('click the "New" button to add a new event', () => {
+Given('you are in the main page with no selections made', () => {
     cy.visit('/');
     cy.get('[data-test="dhis2-uicore-button"]')
         .contains('New')
         .should('exist');
-    cy.get('[data-test="dhis2-uicore-button"]').click();
 });
-Then('you should see text with info', () => {
+When('you click the "New" button to add a new event', () => {
+    cy.get('[data-test="dhis2-uicore-button"]').contains('New').click();
+});
+Then('text should inform you to select both organisation unit and program', () => {
     cy.get('[data-test="dhis2-capture-paper"]')
         .contains('Select a registering unit and program above to get started')
+        .should('exist');
+});
+
+
+Given('you are in the main page with organisation unit selected', () => {
+    cy.visit('/#/orgUnitId=DiszpKrYNg8');
+    cy.get('[data-test="dhis2-uicore-button"]')
+        .contains('New')
+        .should('exist');
+});
+Then('text should inform you to select program', () => {
+    cy.get('[data-test="dhis2-capture-paper"]')
+        .contains('Select a program to start reporting')
         .should('exist');
 });
