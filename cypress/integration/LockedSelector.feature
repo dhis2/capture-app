d9fad2e5d275ad798c6516c18da16b1a40afe3dd
paschalidi <paschalidi@outlook.com>
2020-07-07 13:34:49 +0100

    chore: uploads artifacts and changes default times

--- cypress/integration/LockedSelector.feature
+++ cypress/integration/LockedSelector.feature
@@ -3,4 +3,4 @@ Feature: using the LockedSelector to navigate
 
 Scenario: While you are in the main page
   Given click the "New" button to add a new event
-  Then you should see
+  Then you should see text with info
--- cypress/integration/LockedSelector/index.js
+++ cypress/integration/LockedSelector/index.js
@@ -3,12 +3,12 @@ import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
 
 Given('click the "New" button to add a new event', () => {
     cy.visit('/');
-    cy.get('[data-test="dhis2-uicore-button"]')
+    cy.get('[data-test="dhis2-uicore-button"]', { timeout: 20000 })
         .contains('New')
         .should('exist');
     cy.get('[data-test="dhis2-uicore-button"]').click();
 });
-Then('you should see', () => {
+Then('you should see text with info', () => {
     cy.get('[data-test="dhis2-capture-paper"]')
         .contains('Select a registering unit and program above to get started')
         .should('exist');
