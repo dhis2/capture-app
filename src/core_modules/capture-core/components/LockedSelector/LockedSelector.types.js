320750e38a63bdde11fb9fc013c1a429062b8e1c
paschalidi <paschalidi@outlook.com>
2020-08-05 13:53:39 +0300

    chore: adds types

---
+++ src/core_modules/capture-core/components/LockedSelector/LockedSelector.types.js
@@ -0,0 +1,43 @@
+// @flow
+export type OwnProps = {|
+  isUserInteractionInProgress?: boolean,
+  customActionsOnOrgUnitIdReset?: Array<any>,
+  customActionsOnProgramIdReset?: Array<any>,
+|}
+
+export type PropsFromRedux ={|
+  selectedOrgUnitId: string,
+  selectedProgramId: string,
+  ready: boolean,
+|}
+
+export type DispatchersFromRedux = {|
+  onOpenNewEventPage: (selectedProgramId: string, selectedOrgUnitId: string) => void,
+  onOpenSearchPage: (selectedProgramId: string, selectedOrgUnitId: string) => void,
+  onSetOrgUnit: (id: string, orgUnit: Object) => void,
+  onResetOrgUnitId: () => void,
+  onSetProgramId: (id: string) => void,
+  onSetCategoryOption: (categoryId: string, categoryOptionId: string) => void,
+  onResetCategoryOption: (categoryId: string) => void,
+  onResetAllCategoryOptions: () => void,
+  onStartAgain: () => void,
+  onResetProgramId: (baseAction: ReduxAction<any, any>) => void,
+|}
+
+
+export type Props = {|
+  ...OwnProps,
+  ...CssClasses,
+  ...DispatchersFromRedux,
+  ...PropsFromRedux,
+|}
+
+
+export type State = {|
+  openStartAgainWarning: boolean;
+  openOrgUnitWarning: boolean;
+  openProgramWarning: ?Object;
+  openCatComboWarning: boolean;
+  categoryIdToReset: string;
+  openNewEventWarning: boolean;
+|};
