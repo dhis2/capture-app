5fc29742092488343adb32045819d44db1f6b9d7
paschalidi <paschalidi@outlook.com>
2020-08-07 11:17:32 +0300

    chore: cleans up the types from the D2Form

--- src/core_modules/capture-core/components/D2Form/D2Form.types.js
+++ src/core_modules/capture-core/components/D2Form/D2Form.types.js
@@ -17,16 +17,10 @@ export type PropsFromRedux = $ReadOnly<{|
   isFormInReduxStore: boolean,
 |}>
 
-export type DispatchersFromRedux = $ReadOnly<{|
-  dispatchRemoveFormData: () => void,
-  dispatchRemoveSectionFormData: (formSectionId: string) => void,
-|}>
-
 export type Props = {|
   ...CssClasses,
   ...OwnProps,
   ...PropsFromRedux,
-  ...DispatchersFromRedux,
 |}
 
 export type PropsForPureComponent = $Diff<Props, FormRef>
