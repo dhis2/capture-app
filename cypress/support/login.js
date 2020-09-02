0abe0991ced68e53d451fd7b9142a597b1a9a34d
JoakimSM <joakim.melseth@gmail.com>
2020-09-02 18:42:19 +0200

    chore: removing unused code

--- cypress/support/login.js
+++ cypress/support/login.js
@@ -1,29 +1,3 @@
-const loginEndPoint = 'dhis-web-commons/security/login.action';
-
-/**
- * This is done through cy.request(...)
- * because Cypress doesn't allow multiple domains per test:
- * https://docs.cypress.io/guides/guides/web-security.html#One-Superdomain-per-Test
- */
-Cypress.Commands.add('loginBasicAuth', () => {
-    // AuthBoundary
-    const username = Cypress.env('dhis2_username');
-    const password = Cypress.env('dhis2_password');
-    const loginUrl = Cypress.env('dhis2_base_url');
-    const loginAuth = `Basic ${btoa(`${username}:${password}`)}`;
-
-    return cy.request({
-        url: `${loginUrl}/${loginEndPoint}`,
-        method: 'POST',
-        body: {
-            j_username: username,
-            j_password: password,
-            '2fa_code': '',
-        },
-        headers: { Authorization: loginAuth },
-    });
-});
-
 Cypress.Commands.add('loginThroughForm', () => {
     const username = Cypress.env('dhis2_username');
     const password = Cypress.env('dhis2_password');
