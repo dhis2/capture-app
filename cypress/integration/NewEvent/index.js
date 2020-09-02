70bcd96deb74ebc91d9d58194cc35fae5581a0a7
JoakimSM <joakim.melseth@gmail.com>
2020-09-02 18:22:27 +0200

    chore: add clearCookies comment

--- cypress/integration/NewEvent/index.js
+++ cypress/integration/NewEvent/index.js
@@ -1,5 +1,13 @@
 
 beforeEach(() => {
+    /*
+        Cypress automatically clears all cookies between tests except cross domain cookies.
+        When we are working with a remote instance, we need to clear the login cookie associated with the remote domain.
+        Currently, calling clearCookies with domain:null is a workaround to get rid of the cross domain cookies.
+        Related Cypress issue https://github.com/cypress-io/cypress/issues/408
+    */
+    cy.clearCookies({ domain: null });
+
     cy.loginThroughForm();
 });
 
