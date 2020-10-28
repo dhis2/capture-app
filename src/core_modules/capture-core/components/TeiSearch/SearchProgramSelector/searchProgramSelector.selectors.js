9164558c5ead91ea1f0867afdbfeb7f5305f09b1
paschalidi <paschalidi@outlook.com>
2020-10-28 09:00:11 +0100

    chore: boolean

--- src/core_modules/capture-core/components/TeiSearch/SearchProgramSelector/searchProgramSelector.selectors.js
+++ src/core_modules/capture-core/components/TeiSearch/SearchProgramSelector/searchProgramSelector.selectors.js
@@ -11,9 +11,12 @@ export const makeProgramOptionsSelector = () => createSelector(
     (trackedEntityTypeId: string) =>
         Array.from(programCollection.values())
             .filter(program =>
-                program instanceof TrackerProgram && program.trackedEntityType &&
-                program.trackedEntityType.id === trackedEntityTypeId &&
-                program.access.data.read,
+                Boolean(
+                    program instanceof TrackerProgram &&
+                          program.trackedEntityType &&
+                          program.trackedEntityType.id === trackedEntityTypeId &&
+                          program.access.data.read,
+                ),
             )
             .map(p => ({ value: p.id, label: p.name })),
 );
