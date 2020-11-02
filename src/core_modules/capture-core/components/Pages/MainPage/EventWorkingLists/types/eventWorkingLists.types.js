0ee90451073ba259788a4541ddfa2e499f6c2b28
JoakimSM <joakim.melseth@gmail.com>
2020-11-02 16:45:46 +0100

    chore: annotation

--- src/core_modules/capture-core/components/Pages/MainPage/EventWorkingLists/types/eventWorkingLists.types.js
+++ src/core_modules/capture-core/components/Pages/MainPage/EventWorkingLists/types/eventWorkingLists.types.js
@@ -3,11 +3,11 @@
 export type EventsMainProperties = { [eventId: string]: Object};
 export type EventsDataElementValues = { [eventId: string]: Object};
 
-export type ClientConfig = {
+export type ClientConfig = {|
     filters: { [id: string]: any },
     sortById: string,
     sortByDirection: string,
     currentPage: number,
     rowsPerPage: number,
     columnOrder: Array<Object>,
-};
+|};
