217a844c0d96fb610f3de082c53fa55a1378e78b
paschalidi <paschalidi@outlook.com>
2020-11-03 09:56:33 +0100

    chore: comes from ui

--- src/core_modules/capture-core/components/InefficientSelectionsMessage/index.js
+++ src/core_modules/capture-core/components/InefficientSelectionsMessage/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper/Paper';
 import Grid from '@material-ui/core/Grid/Grid';
 import { withStyles } from '@material-ui/core';
-import { colors } from '@dhis2/ui-core';
+import { colors } from '@dhis2/ui';
 
 const StyledPaper = withStyles({
     root: {
