d341ac907bac75f18cc3ccc84224cf620456b0f0
JoakimSM <joakim.melseth@gmail.com>
2020-11-05 17:21:00 +0100

    refactor: context providers

---
+++ src/core_modules/capture-core/components/Pages/MainPage/WorkingLists/ContextBuilder/ContextProviders/workingListsListViewBuilderContextProvider.types.js
@@ -0,0 +1,32 @@
+// @flow
+import type {
+    DataSource,
+    SelectRow,
+    Sort,
+    SetColumnOrder,
+    CustomRowMenuContents,
+    UpdateFilter,
+    ClearFilter,
+    SelectRestMenuItem,
+    ChangePage,
+    ChangeRowsPerPage,
+    StickyFilters,
+} from '../../../../../ListView';
+
+export type Props = $ReadOnly<{|
+    updating: boolean,
+    updatingWithDialog: boolean,
+    dataSource?: DataSource,
+    onSelectListRow: SelectRow,
+    onSortList: Sort,
+    onSetListColumnOrder: SetColumnOrder,
+    customRowMenuContents?: CustomRowMenuContents,
+    onUpdateFilter: UpdateFilter,
+    onClearFilter: ClearFilter,
+    onSelectRestMenuItem: SelectRestMenuItem,
+    onChangePage: ChangePage,
+    onChangeRowsPerPage: ChangeRowsPerPage,
+    stickyFilters?: StickyFilters,
+    rowsCount?: number,
+    children: React$Node,
+|}>;
