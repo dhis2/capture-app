469e3ba54b22926512fcf46237790089211d56f5
JoakimSM <joakim.melseth@gmail.com>
2020-11-12 16:20:12 +0100

    chore: annotate

--- src/core_modules/capture-core/components/Pages/MainPage/EventWorkingListsCommon/index.js
+++ src/core_modules/capture-core/components/Pages/MainPage/EventWorkingListsCommon/index.js
@@ -2,10 +2,4 @@
 export { useDefaultColumnConfig } from './useDefaultColumnConfiguration';
 export { useColumns } from './useColumns';
 export { useDataSource } from './useDataSource';
-export type {
-    EventRecords,
-    MainColumnConfig,
-    MetadataColumnConfig,
-    EventWorkingListsColumnConfig,
-    EventWorkingListsColumnConfigs,
-} from './types';
+export * from './types';
