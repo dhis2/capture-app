8e4ba6b28f674914e386cd97b1945e914a4cc12a
paschalidi <paschalidi@outlook.com>
2020-11-17 11:10:49 +0000

    fix: flex on horizontal only

--- src/core_modules/capture-core/components/D2Form/D2Form.component.js
+++ src/core_modules/capture-core/components/D2Form/D2Form.component.js
@@ -137,7 +137,7 @@ class D2Form extends React.PureComponent<PropsForPureComponent> {
         const sections = metaDataSectionsAsArray.map(section => (passOnProps.formHorizontal ? this.renderHorizontal(section, passOnProps) : this.renderVertical(section, passOnProps, classes)));
 
         return (
-            <div className={classes.displayFlex} data-test="dhis2-capture-d2-form-component">
+            <div className={passOnProps.formHorizontal ? classes.displayFlex : ''} data-test="dhis2-capture-d2-form-component">
                 {
                     isFormInReduxStore
                         ?
