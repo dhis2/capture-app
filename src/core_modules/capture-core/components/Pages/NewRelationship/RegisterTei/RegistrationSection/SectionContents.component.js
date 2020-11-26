5b59256b276811666552a0be2269c6c82246abda
paschalidi <paschalidi@outlook.com>
2020-11-26 12:57:13 +0000

    chore: reverts ;)

--- src/core_modules/capture-core/components/Pages/NewRelationship/RegisterTei/RegistrationSection/SectionContents.component.js
+++ src/core_modules/capture-core/components/Pages/NewRelationship/RegisterTei/RegistrationSection/SectionContents.component.js
@@ -3,12 +3,10 @@ import * as React from 'react';
 import { ProgramSelector } from './ProgramSelector';
 import { RegUnitSelector } from './RegUnitSelector';
 
-const SectionContents = () => {
-    return (
-        <>
-            <RegUnitSelector />
-            <ProgramSelector />
-        </>
-    );
-};
+const SectionContents = () => (
+    <>
+        <RegUnitSelector />
+        <ProgramSelector />
+    </>
+);
 export default SectionContents;
