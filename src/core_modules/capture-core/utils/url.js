7445483e8ac313f1d3a9b64425934d95006a3e1e
paschalidi <paschalidi@outlook.com>
2020-11-26 12:01:23 +0000

    refactor: cleans up the older func

--- src/core_modules/capture-core/utils/url.js
+++ src/core_modules/capture-core/utils/url.js
@@ -3,6 +3,7 @@ type Url = {programId?: string, orgUnitId?: string, trackedEntityTypeId?: string
 
 export const urlArguments = ({ programId, orgUnitId, trackedEntityTypeId }: Url): string => {
     const argArray = [];
+    debugger;
     if (programId) {
         argArray.push(`programId=${programId}`);
     } else if (trackedEntityTypeId) {
@@ -10,24 +11,6 @@ export const urlArguments = ({ programId, orgUnitId, trackedEntityTypeId }: Url)
     }
     if (orgUnitId) {
         argArray.push(`orgUnitId=${orgUnitId}`);
-    }
-
-    return argArray.join('&');
-};
-
-type Url = {programId?: string, orgUnitId?: string, trackedEntityTypeId?: string}
-
-export const urlThreeArguments = ({ programId, orgUnitId, trackedEntityTypeId }: Url): string => {
-    const argArray = [];
-
-    if (orgUnitId) {
-        argArray.push(`orgUnitId=${orgUnitId}`);
-    }
-
-    if (programId) {
-        argArray.push(`programId=${programId}`);
-    } else if (trackedEntityTypeId) {
-        argArray.push(`trackedEntityTypeId=${trackedEntityTypeId}`);
     }
 
     return argArray.join('&');
