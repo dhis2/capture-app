4d8001cc4255fcea4f59eeb217ed2b955a3a1593
paschalidi <paschalidi@outlook.com>
2020-11-26 12:03:39 +0000

    chore: order

--- src/core_modules/capture-core/components/App/appSync.selectors.js
+++ src/core_modules/capture-core/components/App/appSync.selectors.js
@@ -2,22 +2,22 @@
 import { createSelector } from 'reselect';
 
 const programIdSelector = state => state.currentSelections.programId;
-const orgUnitIdSelector = state => state.currentSelections.orgUnitId;
 const trackedEntityTypeIdSelector = state => state.currentSelections.trackedEntityTypeId;
+const orgUnitIdSelector = state => state.currentSelections.orgUnitId;
 const viewEventIdSelector = state => state.viewEventPage.eventId;
 const eventIdSelector = state => state.editEventPage.eventId;
 
 // $FlowFixMe[missing-annot] automated comment
 export const paramsSelector = createSelector(
     programIdSelector,
-    orgUnitIdSelector,
     trackedEntityTypeIdSelector,
+    orgUnitIdSelector,
     eventIdSelector,
     viewEventIdSelector,
-    (programId: ?string, orgUnitId: ?string, trackedEntityTypeId: ?string, eventId: ?string, viewEventId: ?string) => ({
+    (programId: ?string, trackedEntityTypeId: ?string, orgUnitId: ?string, eventId: ?string, viewEventId: ?string) => ({
         programId,
-        orgUnitId,
         trackedEntityTypeId,
+        orgUnitId,
         eventId,
         viewEventId,
     }),
