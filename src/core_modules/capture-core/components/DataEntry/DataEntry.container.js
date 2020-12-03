774a28af08e9631f9e43021518abb6f1cf2834fc
paschalidi <paschalidi@outlook.com>
2020-12-03 11:07:10 +0000

    chore: lints

--- src/core_modules/capture-core/components/DataEntry/DataEntry.container.js
+++ src/core_modules/capture-core/components/DataEntry/DataEntry.container.js
@@ -3,12 +3,10 @@ import { connect } from 'react-redux';
 import DataEntry from './DataEntry.component';
 import { updateFormField } from './actions/dataEntry.actions';
 
-const mapStateToProps = (state: Object, props: { id: string }) => {
-    return {
-        itemId: state.dataEntries[props.id] && state.dataEntries[props.id].itemId,
-        dataEntryExists: !!state.dataEntries[props.id],
-    };
-};
+const mapStateToProps = (state: Object, props: { id: string }) => ({
+    itemId: state.dataEntries[props.id] && state.dataEntries[props.id].itemId,
+    dataEntryExists: !!state.dataEntries[props.id],
+});
 
 const mapDispatchToProps = (dispatch: ReduxDispatch) => ({
     onUpdateFieldInner: (
