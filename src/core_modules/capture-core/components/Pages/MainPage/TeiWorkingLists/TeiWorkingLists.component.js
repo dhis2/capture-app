0d7367d84dc4568ab470b8450e38f3595d0e3e94
JoakimSM <joakim.melseth@gmail.com>
2020-12-08 19:50:51 +0100

    fix: list container

--- src/core_modules/capture-core/components/Pages/MainPage/TeiWorkingLists/TeiWorkingLists.component.js
+++ src/core_modules/capture-core/components/Pages/MainPage/TeiWorkingLists/TeiWorkingLists.component.js
@@ -1,9 +1,22 @@
 // @flow
-import React from 'react';
+import React, { type ComponentType } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
 import { TeiWorkingListsReduxProvider } from './ReduxProvider';
+import type { Props } from './teiWorkingLists.types';
 
-export const TeiWorkingLists = () => (
-    <TeiWorkingListsReduxProvider
-        storeId={'teiList'}
-    />
+const getStyles = ({ typography }) => ({
+    listContainer: {
+        padding: typography.pxToRem(24),
+    },
+});
+
+const TeiWorkingListsPlain = ({ classes: { listContainer } }: Props) => (
+    <Paper className={listContainer}>
+        <TeiWorkingListsReduxProvider
+            storeId={'teiList'}
+        />
+    </Paper>
 );
+
+export const TeiWorkingLists: ComponentType<$Diff<Props, CssClasses>> = withStyles(getStyles)(TeiWorkingListsPlain);
