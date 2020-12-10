9c654d1e667c077a0670714ad4fc7efdd16a3fe8
Christos Paschalidis <paschalidi@outlook.com>
2020-12-10 16:05:33 +0000

    chore: adds restricted (#1299)

--- src/core_modules/capture-core/api/apiSpecifications/metaPrograms.apiSpecification.js
+++ src/core_modules/capture-core/api/apiSpecifications/metaPrograms.apiSpecification.js
@@ -6,6 +6,7 @@ export default new ApiSpecification((o) => {
     o.modelName = 'programs';
     o.modelGetterType = getterTypes.LIST;
     o.queryParams = {
+        restrictToCaptureScope: true,
         fields: 'id,version,programTrackedEntityAttributes[trackedEntityAttribute[id,optionSet[id,version]]],programStages[id,programStageDataElements[dataElement[id,optionSet[id,version]]]]',
     };
     o.converter = d2Model => d2Model;
