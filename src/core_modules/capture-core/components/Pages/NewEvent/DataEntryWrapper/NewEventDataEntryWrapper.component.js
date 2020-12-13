43880d3c01833c2e49bd9c549751a3de4a89dadd
paschalidi <paschalidi@outlook.com>
2020-12-14 00:38:20 +0100

    chore: commits only the class refactoring

--- src/core_modules/capture-core/components/Pages/NewEvent/DataEntryWrapper/NewEventDataEntryWrapper.component.js
+++ src/core_modules/capture-core/components/Pages/NewEvent/DataEntryWrapper/NewEventDataEntryWrapper.component.js
@@ -3,44 +3,25 @@
 import * as React from 'react';
 import i18n from '@dhis2/d2-i18n';
 import withStyles from '@material-ui/core/styles/withStyles';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import Paper from '@material-ui/core/Paper/Paper';
-import Button from '../../../Buttons/Button.component';
-import DataEntry from '../DataEntry/DataEntry.container';
-import EventsList from '../RecentlyAddedEventsList/RecentlyAddedEventsList.container';
+import { Button } from '@dhis2/ui';
+import DataEntry from '../../../DataEntries/SingleEventRegistrationEntry/DataEntryWrapper/DataEntry/DataEntry.container';
+import EventsList from '../../../DataEntries/SingleEventRegistrationEntry/DataEntryWrapper/RecentlyAddedEventsList/RecentlyAddedEventsList.container';
 import type { ProgramStage, RenderFoundation } from '../../../../metaData';
 
-
-const getStyles = (theme: Theme) => ({
-    headerContainer: {
+const getStyles = () => ({
+    flexContainer: {
         display: 'flex',
-        justifyContent: 'space-between',
         alignItems: 'center',
+        justifyContent: 'center',
     },
-    // $FlowFixMe[cannot-spread-inexact] automated comment
-    header: {
-        flexGrow: 1,
-        ...theme.typography.title,
-        fontSize: 18,
-        fontWeight: 500,
-        paddingLeft: 8,
-    },
-    dataEntryPaper: {
-        marginBottom: theme.typography.pxToRem(10),
-        padding: theme.typography.pxToRem(10),
-    },
-    showAllEvents: {
-        marginBottom: 10,
+    flexEnd: {
+        justifyContent: 'flex-end',
+        marginLeft: 'auto',
     },
 });
 
 type Props = {
-    classes: {
-        headerContainer: string,
-        header: string,
-        dataEntryPaper: string,
-        showAllEvents: string,
-    },
+    ...CssClasses,
     formHorizontal: ?boolean,
     onFormLayoutDirectionChange: (formHorizontal: boolean) => void,
     formFoundation: ?RenderFoundation,
@@ -48,71 +29,56 @@ type Props = {
 }
 
 
-class NewEventDataEntryWrapper extends React.Component<Props> {
+class NewEventDataEntryWrapperPlain extends React.Component<Props> {
     cancelButtonInstance: ?any;
-
-    handleGoBackToAllEvents = () => {
-        this.cancelButtonInstance && this.cancelButtonInstance.handleCancel();
-    }
 
     setCancelButtonInstance = (cancelButtonInstance: ?any) => {
         this.cancelButtonInstance = cancelButtonInstance;
     }
 
-    renderHeader() {
-        return (
-            <div
-                className={this.props.classes.headerContainer}
-            >
-                <div
-                    className={this.props.classes.header}
-                >
-                    {i18n.t('New event')}
-                </div>
-                <div>
-                    {this.renderHeaderButtons()}
-                </div>
-            </div>
-        );
-    }
-
-    renderHeaderButtons() {
-        const { formFoundation } = this.props;
-        if (!formFoundation || formFoundation.customForm) {
-            return null;
-        }
-
-        return (
-            <Button
-                onClick={() => this.props.onFormLayoutDirectionChange(!this.props.formHorizontal)}
-                secondary
-            >
-                {this.props.formHorizontal ? i18n.t('Switch to form view') : i18n.t('Switch to row view')}
-            </Button>
-        );
-    }
-
     render() {
-        const { classes, formFoundation, formHorizontal, stage } = this.props;
+        const {
+            classes,
+            formFoundation,
+            formHorizontal,
+            stage,
+            onFormLayoutDirectionChange,
+        } = this.props;
+
         return (
-            <div>
-                <Button className={classes.showAllEvents} onClick={this.handleGoBackToAllEvents}>
-                    <ChevronLeft />
-                    {i18n.t('Show all events')}
-                </Button>
-                <Paper className={classes.dataEntryPaper} data-test="dhis2-capture-new-event-page">
-                    {this.renderHeader()}
-                    <DataEntry
-                        stage={stage}
-                        cancelButtonRef={this.setCancelButtonInstance}
-                        formFoundation={formFoundation}
-                        formHorizontal={formHorizontal}
-                    />
-                </Paper>
+            <>
+                <div className={classes.flexContainer}>
+                    <div className={classes.flexEnd}>
+                        {
+                            !formFoundation || formFoundation.customForm ?
+                                null
+                                :
+                                <Button
+                                    onClick={() => onFormLayoutDirectionChange(!formHorizontal)}
+                                    small
+                                >
+                                    {
+                                        formHorizontal
+                                            ?
+                                            i18n.t('Switch to form view')
+                                            :
+                                            i18n.t('Switch to row view')
+                                    }
+                                </Button>
+
+                        }
+                    </div>
+                </div>
+                <DataEntry
+                    stage={stage}
+                    cancelButtonRef={this.setCancelButtonInstance}
+                    formFoundation={formFoundation}
+                    formHorizontal={formHorizontal}
+                />
                 <EventsList />
-            </div>
+            </>
         );
     }
 }
 
-export default withStyles(getStyles)(NewEventDataEntryWrapper);
+export const NewEventDataEntryWrapperComponent = withStyles(getStyles)(NewEventDataEntryWrapperPlain);
