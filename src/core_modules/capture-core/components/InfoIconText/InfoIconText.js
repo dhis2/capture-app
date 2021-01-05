a43961ac2df3618605fdf839bf29ec3bcab0df68
paschalidi <paschalidi@outlook.com>
2021-01-05 12:14:23 +0100

    chore: no space

--- src/core_modules/capture-core/components/InfoIconText/InfoIconText.js
+++ src/core_modules/capture-core/components/InfoIconText/InfoIconText.js
@@ -5,7 +5,6 @@ import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 import Grid from '@material-ui/core/Grid';
 import { colors } from '@dhis2/ui';
 
-
 const InfoOutlinedIconWithStyles = withStyles({
     root: {
         fontSize: 14,
