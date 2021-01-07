616373b8db4ff9e4e51f3c0a4b2c573d42271022
paschalidi <paschalidi@outlook.com>
2021-01-07 15:47:57 +0100

    chore: reverts loading indicator

--- src/core_modules/capture-core/components/Pages/Enrollment/EnrollmentPage.component.js
+++ src/core_modules/capture-core/components/Pages/Enrollment/EnrollmentPage.component.js
@@ -5,39 +5,41 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import { LockedSelector } from '../../LockedSelector';
 import type { Props } from './EnrollmentPage.types';
 import { enrollmentPageStatuses } from './EnrollmentPage.constants';
-import { LoadingMaskForPage } from '../../LoadingMasks';
+import LoadingMaskForPage from '../../LoadingMasks/LoadingMaskForPage.component';
 
 const getStyles = () => ({
     container: {
         padding: '8px 24px 16px 24px',
     },
+    loadingMask: {
+        height: '100vh',
+    },
 });
 
 const EnrollmentPagePlain = ({ classes, enrollmentPageStatus }) => (<>
-    {
-        enrollmentPageStatus === enrollmentPageStatuses.LOADING ?
-            <LoadingMaskForPage />
-            :
-            <>
-                <LockedSelector />
+    <LockedSelector />
 
-                <div data-test="dhis2-capture-enrollment-page-content" className={classes.container} >
+    <div data-test="dhis2-capture-enrollment-page-content" className={classes.container} >
 
-                    {
-                        enrollmentPageStatus === enrollmentPageStatuses.DEFAULT &&
-                        <div>default</div>
-                    }
+        {
+            enrollmentPageStatus === enrollmentPageStatuses.DEFAULT &&
+                <div>default</div>
+        }
 
-
-                    {
-                        enrollmentPageStatus === enrollmentPageStatuses.ERROR &&
-                        <div>
-                            error
-                        </div>
-                    }
+        {
+            enrollmentPageStatus === enrollmentPageStatuses.LOADING &&
+                <div className={classes.loadingMask}>
+                    <LoadingMaskForPage />
                 </div>
-            </>
-    }
+        }
+
+        {
+            enrollmentPageStatus === enrollmentPageStatuses.ERROR &&
+                <div>
+                    error
+                </div>
+        }
+    </div>
 </>);
 
 export const EnrollmentPageComponent: ComponentType<$Diff<Props, CssClasses>> =
