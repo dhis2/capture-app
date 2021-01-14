ec00869a9ad2ec0305872789e6892a93e37c9212
paschalidi <paschalidi@outlook.com>
2021-01-14 13:39:41 +0100

    chore: lints

--- src/core_modules/capture-core/components/Pages/Enrollment/EnrollmentPage.component.js
+++ src/core_modules/capture-core/components/Pages/Enrollment/EnrollmentPage.component.js
@@ -1,6 +1,5 @@
 // @flow
 import React from 'react';
-import { compose } from 'redux';
 import type { ComponentType } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { LockedSelector } from '../../LockedSelector';
