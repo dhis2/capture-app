f42f97058e8f62b4d4f4de86b2bce0013e462c57
paschalidi <paschalidi@outlook.com>
2021-01-21 09:05:39 +0100

    chore: cleans up the QuickSelector

--- src/core_modules/capture-core/components/LockedSelector/QuickSelector/QuickSelector.container.js
+++ src/core_modules/capture-core/components/LockedSelector/QuickSelector/QuickSelector.container.js
@@ -2,28 +2,13 @@
 
 import { connect } from 'react-redux';
 import QuickSelector from './QuickSelector.component';
-import { convertValue } from '../../../converters/clientToView';
-import { dataElementTypes } from '../../../metaData/DataElement';
 
-const buildEnrollmentsAsOptions = (enrollments = [], selectedProgramId) =>
-    enrollments
-        .filter(({ program }) => program === selectedProgramId)
-        .map(({ created, enrollment }) => (
-            {
-                label: convertValue(created, dataElementTypes.DATETIME),
-                value: enrollment,
-            }
-        ));
 const mapStateToProps = (state: Object) => {
     const {
-        enrollmentPage,
         organisationUnits,
         currentSelections: {
             programId: selectedProgramId,
             orgUnitId: selectedOrgUnitId,
-            teiId: selectedTeiId,
-            trackedEntityTypeId: selectedTet,
-            enrollmentId: selectedEnrollmentId,
             categoriesMeta,
         },
         router: {
@@ -31,9 +16,6 @@ const mapStateToProps = (state: Object) => {
                 query: {
                     programId: routerProgramId,
                     orgUnitId: routerOrgUnitId,
-                    teiId: routerTeiId,
-                    trackedEntityTypeId: routerTet,
-                    enrollmentId: routerEnrollmentId,
                 },
                 pathname,
             },
@@ -42,21 +24,13 @@ const mapStateToProps = (state: Object) => {
 
     const programId = routerProgramId || selectedProgramId;
     const orgUnitId = routerOrgUnitId || selectedOrgUnitId;
-    const enrollmentId = routerEnrollmentId || selectedEnrollmentId;
-    const teiId = routerTeiId || selectedTeiId;
-    const trackedEntityTypeId = routerTet || selectedEnrollmentId;
-    const enrollmentsAsOptions = buildEnrollmentsAsOptions(enrollmentPage.enrollments, programId);
 
     return {
         selectedProgramId: programId,
         selectedOrgUnitId: orgUnitId,
-        selectedEnrollmentId: enrollmentId,
         selectedCategories: categoriesMeta,
         selectedOrgUnit: orgUnitId ? organisationUnits[orgUnitId] : null,
         currentPage: pathname.substring(1),
-        selectedTeiName: enrollmentPage.trackedEntityInstanceDisplayName,
-        enrollmentsAsOptions,
-        enrollmentLockedSelectReady: enrollmentPage.enrollments && enrollmentPage.enrollments.length,
     };
 };
 
