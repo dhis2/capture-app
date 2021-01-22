378c7fe0a8ca56892245f2525c2db737c61ed62a
paschalidi <paschalidi@outlook.com>
2021-01-22 16:19:31 +0000

    chore: back

--- src/core_modules/capture-core/components/App/withAppUrlSync.js
+++ src/core_modules/capture-core/components/App/withAppUrlSync.js
@@ -49,10 +49,6 @@ const specificationForPages = {
         orgUnitIdProperties,
     ],
     [pageKeys.VIEW_EVENT]: [
-        {
-            urlKey: 'viewEventId',
-            propKey: 'viewEventId',
-        },
         eventIdProperties,
     ],
     [pageKeys.SEARCH]: [
