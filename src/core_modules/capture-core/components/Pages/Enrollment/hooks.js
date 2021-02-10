6d754a9de90e6c87b5b97fd26e713d45def91143
paschalidi <paschalidi@outlook.com>
2021-02-10 17:48:09 +0000

    chore: lints

--- src/core_modules/capture-core/components/Pages/Enrollment/hooks.js
+++ src/core_modules/capture-core/components/Pages/Enrollment/hooks.js
@@ -6,8 +6,8 @@ export const useEnrollmentInfo = (enrollmentId: string, programId: string) => {
     const tetId = useSelector(({ enrollmentPage }) => enrollmentPage.tetId);
     const programHasEnrollments = enrollments && enrollments.some(({ program }) => programId === program);
     const enrollmentsOnProgramContainEnrollmentId = enrollments && enrollments
-    .filter(({ program }) => program === programId)
-    .some(({ enrollment }) => enrollmentId === enrollment);
+        .filter(({ program }) => program === programId)
+        .some(({ enrollment }) => enrollmentId === enrollment);
 
     return { programHasEnrollments, enrollmentsOnProgramContainEnrollmentId, tetId };
 };
