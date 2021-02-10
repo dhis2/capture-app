50cb1468d875900645a2fb42d1f3f15512da5e6c
paschalidi <paschalidi@outlook.com>
2021-02-10 17:47:50 +0000

    chore: adds flow into hooks.js

--- src/core_modules/capture-core/components/Pages/Enrollment/hooks.js
+++ src/core_modules/capture-core/components/Pages/Enrollment/hooks.js
@@ -1,11 +1,13 @@
+// @flow
 import { useSelector } from 'react-redux';
 
 export const useEnrollmentInfo = (enrollmentId: string, programId: string) => {
-    const enrollments: Object = useSelector(({ enrollmentPage }) => enrollmentPage.enrollments);
+    const enrollments = useSelector(({ enrollmentPage }) => enrollmentPage.enrollments);
+    const tetId = useSelector(({ enrollmentPage }) => enrollmentPage.tetId);
     const programHasEnrollments = enrollments && enrollments.some(({ program }) => programId === program);
     const enrollmentsOnProgramContainEnrollmentId = enrollments && enrollments
-        .filter(({ program }) => program === programId)
-        .some(({ enrollment }) => enrollmentId === enrollment);
+    .filter(({ program }) => program === programId)
+    .some(({ enrollment }) => enrollmentId === enrollment);
 
-    return { programHasEnrollments, enrollmentsOnProgramContainEnrollmentId };
+    return { programHasEnrollments, enrollmentsOnProgramContainEnrollmentId, tetId };
 };
