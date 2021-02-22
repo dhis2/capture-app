acaa35ead2be44141ccf519449b1a60ce6ffb5d1
paschalidi <paschalidi@outlook.com>
2021-02-22 11:47:34 +0000

    chore: adds /

--- src/core_modules/capture-core/utils/navigateToTrackedEntityDashboard.js
+++ src/core_modules/capture-core/utils/navigateToTrackedEntityDashboard.js
@@ -3,7 +3,7 @@ import { config } from 'd2';
 export const navigateToTrackedEntityDashboard = (teiId, orgUnitId, scopeSearchParam, currentUrl) => {
     const { baseUrl } = config;
     const instanceBaseUrl = baseUrl.split('/api')[0];
-    const base64Url = btoa(`capture-app/index.html#${currentUrl}`);
+    const base64Url = btoa(`/capture-app/index.html#${currentUrl}`);
 
     // `returnUrl` serves the purpose that the tracker capture app can sent the user back to the capture app.
     // In the future when there is not gonna be linking between the two apps this wont be needed.
