14df4f79e8a25d6fcf7a19dbdd489797bf68e476
paschalidi <paschalidi@outlook.com>
2021-02-24 16:13:20 +0000

    chore: base64

--- src/core_modules/capture-core/utils/navigateToTrackedEntityDashboard.js
+++ src/core_modules/capture-core/utils/navigateToTrackedEntityDashboard.js
@@ -3,17 +3,7 @@ import { config } from 'd2';
 export const navigateToTrackedEntityDashboard = (teiId, orgUnitId, scopeSearchParam, currentUrl) => {
     const { baseUrl } = config;
     const instanceBaseUrl = baseUrl.split('/api')[0];
-
-    const returnUrl =
-      // when developing locally the following will always be false
-      window.location.href.includes(instanceBaseUrl)
-          ?
-          window.location.href.split(instanceBaseUrl)[1]
-          :
-          `/dhis-web-capture/#${currentUrl}`;
-
-    const base64Url = btoa(returnUrl);
-
+    const base64Url = btoa(`/dhis-web-capture/#${currentUrl}`);
 
     // `returnUrl` serves the purpose that the tracker capture app can sent the user back to the capture app.
     // In the future when there is not gonna be linking between the two apps this wont be needed.
