6095c2457958ed069b2399dcd01643f4cd58cf70
paschalidi <paschalidi@outlook.com>
2021-02-24 13:25:38 +0000

    chore: is this a hack?

--- src/core_modules/capture-core/utils/navigateToTrackedEntityDashboard.js
+++ src/core_modules/capture-core/utils/navigateToTrackedEntityDashboard.js
@@ -3,7 +3,17 @@ import { config } from 'd2';
 export const navigateToTrackedEntityDashboard = (teiId, orgUnitId, scopeSearchParam, currentUrl) => {
     const { baseUrl } = config;
     const instanceBaseUrl = baseUrl.split('/api')[0];
-    const base64Url = btoa(`/capture-app/index.html#${currentUrl}`);
+
+    const returnUrl =
+      // when developing locally the following will always be false
+      window.location.href.includes(instanceBaseUrl)
+          ?
+          window.location.href.split(instanceBaseUrl)[1]
+          :
+          `/dhis-web-capture/#${currentUrl}`;
+
+    const base64Url = btoa(returnUrl);
+
 
     // `returnUrl` serves the purpose that the tracker capture app can sent the user back to the capture app.
     // In the future when there is not gonna be linking between the two apps this wont be needed.
