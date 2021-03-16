389f0b49ef16239c4b7dc13fc80bc0141d911f31
paschalidi <paschalidi@outlook.com>
2021-03-16 12:39:33 +0100

    chore: cleans up

--- src/core_modules/capture-core/components/Pages/Enrollment/Event/Edit/EnrollmentEventEditPage.component.js
+++ src/core_modules/capture-core/components/Pages/Enrollment/Event/Edit/EnrollmentEventEditPage.component.js
@@ -7,9 +7,7 @@ import { LockedSelector } from '../../../../LockedSelector';
 import type { Props } from './EnrollmentEventEditPage.types';
 import { withErrorMessageHandler } from '../../../../../HOC';
 import { SingleLockedSelect } from '../../../../LockedSelector/QuickSelector/SingleLockedSelect/SingleLockedSelect.component';
-import { useExtras } from '../../hooks';
-import { enrollmentPageStatuses } from '../../EnrollmentPage.constants';
-import { enrollmentEventEditPageStatuses } from "./enrollmentEventEditPage.constants";
+import { enrollmentEventEditPageStatuses } from './enrollmentEventEditPage.constants';
 
 
 const styles = ({ typography }) => ({
@@ -25,79 +23,65 @@ const styles = ({ typography }) => ({
 });
 
 
-const ExtraSelectors = ({ width, classes }) => {
-    const {
-        onTeiSelectionReset,
-        selectedEnrollmentId,
-        teiDisplayName,
-        enrollmentsAsOptions,
-        trackedEntityName,
-        enrollmentLockedSelectReady,
-        onEnrollmentSelectionSet,
-        onEnrollmentSelectionReset,
-    } = useExtras();
+const ExtraSelectors = ({ width, classes }) => (<>
+    <Grid item xs={12} sm={width * 3} md={width * 2} lg={2} className={classes.orgUnitSelector}>
+        <SingleLockedSelect
+            ready
+            onClear={() => {}}
+            options={[{
+                label: 'name',
+                value: 'alwaysPreselected',
 
-    return (<>
-        <Grid item xs={12} sm={width * 3} md={width * 2} lg={2} className={classes.orgUnitSelector}>
-            <SingleLockedSelect
-                ready
-                onClear={onTeiSelectionReset}
-                options={[{
-                    label: 'name',
-                    value: 'alwaysPreselected',
+            }]}
+            selectedValue="alwaysPreselected"
+            title={'trackedEntityName'}
+        />
+    </Grid>
+    <Grid item xs={12} sm={width * 3} md={width * 2} lg={2} className={classes.orgUnitSelector}>
+        <SingleLockedSelect
+            ready
+            onClear={() => {}}
+            options={[{
+                label: 'name',
+                value: 'alwaysPreselected',
 
-                }]}
-                selectedValue="alwaysPreselected"
-                title={'trackedEntityName'}
-            />
-        </Grid>
-        <Grid item xs={12} sm={width * 3} md={width * 2} lg={2} className={classes.orgUnitSelector}>
-            <SingleLockedSelect
-                ready
-                onClear={onTeiSelectionReset}
-                options={[{
-                    label: 'name',
-                    value: 'alwaysPreselected',
+            }]}
+            selectedValue="alwaysPreselected"
+            title={'trackedEntityName'}
+        />
+    </Grid>
+    <Grid item xs={12} sm={width * 3} md={width * 2} lg={2} className={classes.orgUnitSelector}>
+        <SingleLockedSelect
+            ready
+            onClear={() => {}}
+            options={[{
+                label: 'name',
+                value: 'alwaysPreselected',
 
-                }]}
-                selectedValue="alwaysPreselected"
-                title={'trackedEntityName'}
-            />
-        </Grid>
-        <Grid item xs={12} sm={width * 3} md={width * 2} lg={2} className={classes.orgUnitSelector}>
-            <SingleLockedSelect
-                ready
-                onClear={onTeiSelectionReset}
-                options={[{
-                    label: 'name',
-                    value: 'alwaysPreselected',
+            }]}
+            selectedValue="alwaysPreselected"
+            title={'trackedEntityName'}
+        />
+    </Grid>
+    <Grid item xs={12} sm={width * 3} md={width * 2} lg={2} className={classes.orgUnitSelector}>
+        <SingleLockedSelect
+            ready
+            onClear={() => {}}
+            options={[{
+                label: 'name',
+                value: 'alwaysPreselected',
 
-                }]}
-                selectedValue="alwaysPreselected"
-                title={'trackedEntityName'}
-            />
-        </Grid>
-        <Grid item xs={12} sm={width * 3} md={width * 2} lg={2} className={classes.orgUnitSelector}>
-            <SingleLockedSelect
-                ready
-                onClear={onTeiSelectionReset}
-                options={[{
-                    label: 'name',
-                    value: 'alwaysPreselected',
-
-                }]}
-                selectedValue="alwaysPreselected"
-                title={'trackedEntityName'}
-            />
-        </Grid>
-    </>);
-};
+            }]}
+            selectedValue="alwaysPreselected"
+            title={'trackedEntityName'}
+        />
+    </Grid>
+</>);
 
 const EnrollmentEventEditPagePlain = ({ pageStatus, classes }) => (<>
     <LockedSelector pageToPush="enrollment/event/edit" renderExtraSelectors={(width, extraClasses) => <ExtraSelectors width={width} classes={extraClasses} />} />
 
     <div data-test="enrollment-event-edit-page-content" className={classes.container} >
-
         {
             pageStatus === enrollmentEventEditPageStatuses.DEFAULT &&
             <div className={classes.title}>Enrollment: Edit Event</div>
