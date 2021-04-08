1360124e90457a07e7dbe3b6e2033bbe07092f69
Christos Paschalidis <paschalidi@outlook.com>
2021-04-08 11:25:09 +0100

    fix: DHIS2-10711 duplicated calls on search page (#1572)
    * fix: duplicates
    
    * chore: changes
    
    * chore: initial values are back
    
    * chore: removes unused reducers
    
    * chore: lints
    
    * chore: adds null
    
    * chore: fixes the relationship part
    
    * chore: lints

--- src/core_modules/capture-core/components/Pages/New/NewPage.container.js
+++ src/core_modules/capture-core/components/Pages/New/NewPage.container.js
@@ -58,7 +58,7 @@ export const NewPage: ComponentType<{||}> = () => {
         useSelector(({ activePage }) => activePage.selectionsError && activePage.selectionsError.error);
 
     const ready: boolean =
-        useSelector(({ activePage }) => !activePage.lockedSelectorLoads && !activePage.isDataEntryLoading);
+        useSelector(({ activePage }) => (!activePage.isDataEntryLoading));
 
     const currentScopeId: string =
         useSelector(({ currentSelections }) => currentSelections.programId || currentSelections.trackedEntityTypeId);
