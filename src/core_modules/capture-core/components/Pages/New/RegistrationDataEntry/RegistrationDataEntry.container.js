9e78fb15bf3a718371e24c2fa35750eb37582439
paschalidi <paschalidi@outlook.com>
2021-04-09 12:17:58 +0300

    chore: cleans up also on unmount

--- src/core_modules/capture-core/components/Pages/New/RegistrationDataEntry/RegistrationDataEntry.container.js
+++ src/core_modules/capture-core/components/Pages/New/RegistrationDataEntry/RegistrationDataEntry.container.js
@@ -1,6 +1,6 @@
 // @flow
 import { useDispatch, useSelector } from 'react-redux';
-import React, { useCallback, type ComponentType } from 'react';
+import React, { useCallback, type ComponentType, useEffect } from 'react';
 import { RegistrationDataEntryComponent } from './RegistrationDataEntry.component';
 import type { OwnProps } from './RegistrationDataEntry.types';
 import {
@@ -8,6 +8,8 @@ import {
     startSavingNewTrackedEntityInstanceWithEnrollment,
 } from './RegistrationDataEntry.actions';
 import { useDuplicates } from '../../../PossibleDuplicatesDialog/useDuplicates';
+import { cleanUpDataEntry } from '../NewPage.actions';
+import { NEW_TEI_DATA_ENTRY_ID } from '../NewPage.component';
 
 export const RegistrationDataEntry: ComponentType<OwnProps>
   = ({ selectedScopeId, dataEntryId, setScopeId }) => {
@@ -24,6 +26,8 @@ export const RegistrationDataEntry: ComponentType<OwnProps>
       const { onReviewDuplicates } = useDuplicates(dataEntryId, selectedScopeId);
 
       const dataEntryIsReady = useSelector(({ dataEntries }) => (!!dataEntries[dataEntryId]));
+
+      useEffect(() => () => dispatch(cleanUpDataEntry(NEW_TEI_DATA_ENTRY_ID)), [dispatch]);
 
       return (
           <RegistrationDataEntryComponent
