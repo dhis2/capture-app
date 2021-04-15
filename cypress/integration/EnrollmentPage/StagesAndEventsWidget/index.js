d74ab7cd3e14efaed24545d2c8d8b1d8f84b3b76
Joakim Storløkken Melseth <joakim.melseth@gmail.com>
2021-04-15 11:57:09 +0200

    test: DHIS2-10218 stage tests

---
+++ cypress/integration/EnrollmentPage/StagesAndEventsWidget/index.js
@@ -0,0 +1,33 @@
+beforeEach(() => {
+    cy.loginThroughForm();
+});
+
+Given('you open the enrollment dummy page', () => {
+    cy.visit('#/enrollmentDummy');
+});
+
+Then('the program stages should be displayed', () => {
+    cy.get('[data-test="stages-and-events-widget"]')
+        .within(() => {
+            cy.contains('Birth').should('exist');
+            cy.contains('Baby Postnatal').should('exist');
+        });
+});
+
+When('you click the stages and events widget toggle open close button', () => {
+    cy.get('[data-test="stages-and-events-widget"]')
+        .within(() => {
+            cy.get('[data-test="widget-open-close-toggle-button"]')
+                .click();
+        });
+});
+
+Then('the stages and events widget should be closed', () => {
+    cy.get('[data-test="stages-and-events-widget"]')
+        .within(() => {
+            cy.get('[data-test="widget-contents"]')
+                .children()
+                .should('not.exist');
+            cy.contains('Birth').should('not.exist');
+        });
+});
