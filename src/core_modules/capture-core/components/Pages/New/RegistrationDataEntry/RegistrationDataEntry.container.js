fbec5ec16538812cec5a58f9dda6c403c6cc121f
Christos Paschalidis <paschalidi@outlook.com>
2021-04-20 11:36:08 +0100

    fix: DHIS2-10206 displays dialog when data has entered and user is exiting the page (#1592)
    * docs: Update README
    
    * refactor: exports shared steps & fixes all tests (#1483)
    
    * chore: only the fail true is on
    
    * chore: fixes existing tests
    
    * chore: reduces time
    
    * chore: drops time more
    
    * chore: adds 1000 ms
    
    * chore: bring time to 20000
    
    * chore: fixes new page
    
    * chore: bring time down to 7000
    
    * chore: time is 12000
    
    * chore: timer is 14000
    
    * chore: time equals 12000
    
    * feat: extracts the functions
    
    * chore: drops time to 6000
    
    * chore: up to 12000
    
    * chore: 15000
    
    * chore: rebase and splits the shared steps
    
    * chore: addmission date and missing steps
    
    * chore: random and
    
    * chore: fixes the tests
    
    * chore: typo
    
    * fix: program error bug (#1532)
    
    * chore: solves the bug
    
    * chore: covers with tests
    
    * chore: adds missing test cases
    
    * chore: removes the dhis2-capture- prefix (#1410)
    
    * fix: the bug (#1536)
    
    * chore(release): cut 1.15.1 [skip ci]
    
    ## [1.15.1](https://github.com/dhis2/capture-app/compare/v1.15.0...v1.15.1) (2021-03-04)
    
    ### Bug Fixes
    
    * the bug ([#1536](https://github.com/dhis2/capture-app/issues/1536)) ([fd2616f](https://github.com/dhis2/capture-app/commit/fd2616f92b768249601be6e020527e9f89cd19ef))
    
    * chore: forward-ports the last two PRs  (#1588)
    
    * fix: DHIS2-10738 pressing the back button after a fallback crashes app (#1581)
    
    * chore: sets to false
    
    * chore: covers bug case
    
    * fix: the bug (#1580)
    
    * chore: fix the rerun (#1589)
    
    * chore: reverts
    
    * chore: saves
    
    * chore: fixes the bug
    
    * chore: removes unused onCleanup
    
    * chore: improves
    
    * chore: cleans up and lints
    
    * chore: fixes
    
    * chore: imports from correct action
    
    * chore: cleans up also on unmount
    
    * chore: adds curr state
    
    * chore: cleans up
    
    * fix: the crash by removing only the correct entries on cleanUp
    
    * chore: fixes the problem with the window
    
    Co-authored-by: Joakim Storløkken Melseth <joakim@dhis2.org>
    Co-authored-by: @dhis2-bot <apps@dhis2.org>
    Co-authored-by: Joakim Storløkken Melseth <joakim.melseth@gmail.com>

--- src/core_modules/capture-core/components/Pages/New/RegistrationDataEntry/RegistrationDataEntry.container.js
+++ src/core_modules/capture-core/components/Pages/New/RegistrationDataEntry/RegistrationDataEntry.container.js
@@ -1,12 +1,14 @@
 // @flow
 import { useDispatch, useSelector } from 'react-redux';
-import React, { useCallback, type ComponentType } from 'react';
+import React, { useCallback, type ComponentType, useEffect } from 'react';
 import { RegistrationDataEntryComponent } from './RegistrationDataEntry.component';
 import type { OwnProps } from './RegistrationDataEntry.types';
 import {
     startSavingNewTrackedEntityInstance,
     startSavingNewTrackedEntityInstanceWithEnrollment,
 } from './RegistrationDataEntry.actions';
+import { cleanUpDataEntry } from '../NewPage.actions';
+import { NEW_RELATIONSHIP_EVENT_DATA_ENTRY_ID, NEW_SINGLE_EVENT_DATA_ENTRY_ID, NEW_TEI_DATA_ENTRY_ID } from '../NewPage.component';
 
 export const RegistrationDataEntry: ComponentType<OwnProps>
   = ({ selectedScopeId, dataEntryId, setScopeId }) => {
@@ -21,6 +23,12 @@ export const RegistrationDataEntry: ComponentType<OwnProps>
           [dispatch]);
 
       const dataEntryIsReady = useSelector(({ dataEntries }) => (!!dataEntries[dataEntryId]));
+
+      useEffect(() => () => {
+          dispatch(cleanUpDataEntry(NEW_TEI_DATA_ENTRY_ID));
+          dispatch(cleanUpDataEntry(NEW_SINGLE_EVENT_DATA_ENTRY_ID));
+          dispatch(cleanUpDataEntry(NEW_RELATIONSHIP_EVENT_DATA_ENTRY_ID));
+      }, [dispatch]);
 
       return (
           <RegistrationDataEntryComponent
