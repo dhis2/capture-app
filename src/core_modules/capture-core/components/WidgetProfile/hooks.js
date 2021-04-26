090183b25b3ac619f98bd8d23cc494b69a26cdcb
huocha <huongtranguyen.ndht@gmail.com>
2021-04-26 14:09:32 +0200

    feat: [DHIS2-10948] request data profile widget

--- src/core_modules/capture-core/components/WidgetProfile/hooks.js
+++ src/core_modules/capture-core/components/WidgetProfile/hooks.js
@@ -1,60 +1,56 @@
 
+
+import { useState, useEffect, useCallback } from 'react';
 import { useSelector } from 'react-redux';
-import { makeCancelablePromise } from 'capture-core-utils';
 import { getApi } from '../../d2/d2Instance';
-import React, { useState, useEffect } from 'react';
 
 export const useWidgetProfileData = () => {
-    const { teiId, programId }: Object =
+    const { teiId, programId } =
       useSelector(({ router: { location: { query } } }) => ({ teiId: query.teiId, programId: query.programId }));
 
     const [result, setResult] = useState(null);
+
+    const requestData = useCallback(() => {
+        const programQuery = getApi()
+            .get(
+            // $FlowFixMe[incompatible-type] automated comment
+                `programs/${programId}`,
+                {
+                    fields: ['programTrackedEntityAttributes[id,displayInList,trackedEntityAttribute[id,displayName]]'],
+                },
+            );
+        const attributeQuery = getApi()
+            .get(
+            // $FlowFixMe[incompatible-type] automated comment
+                `trackedEntityInstances/${teiId}`,
+                {
+                    program: programId,
+                },
+            );
+        Promise.all([programQuery, attributeQuery]).then(([{ programTrackedEntityAttributes }, { attributes }]) => {
+            const displayEntities = programTrackedEntityAttributes.reduce((acc, curr) => { acc = [...acc, curr.trackedEntityAttribute]; return acc; }, []);
+
+            const formattedAttributes = [];
+            displayEntities.forEach((entity) => {
+                const displayAttribute = attributes.find(att => att.attribute === entity.id);
+                if (displayAttribute) {
+                    formattedAttributes.push({ ...entity, value: displayAttribute.value });
+                }
+            });
+            setResult(formattedAttributes);
+        });
+    }, [programId, teiId]);
+
     useEffect(
         () => {
             if (result) {
                 return;
             }
-            requestData().then(setResult);
+            requestData();
         },
-        [result],
+        [result, requestData],
     );
 
 
-    const requestData = () => new Promise((resolve) => {
-        const cancelablePromise = makeCancelablePromise(
-            getApi()
-                .get(
-                // $FlowFixMe[incompatible-type] automated comment
-                    `programs/${programId}`,
-                    {
-                        fields: ['programTrackedEntityAttributes[id,displayInList,trackedEntityAttribute[id,displayName]]'],
-                    },
-                ),
-        );
-
-        cancelablePromise.promise.then(
-            ({ programTrackedEntityAttributes }) => {
-                const displayEntities = programTrackedEntityAttributes.reduce((acc, curr) => { acc = [...acc, curr.trackedEntityAttribute]; return acc; }, []);
-
-                getApi()
-                    .get(
-                        // $FlowFixMe[incompatible-type] automated comment
-                        `trackedEntityInstances/${teiId}`,
-                        {
-                            program: programId,
-                        },
-                    ).then(({ attributes }) => {
-                        const formattedAttributes = [];
-                        displayEntities.forEach((entity) => {
-                            const displayAttribute = attributes.find(att => att.attribute === entity.id);
-                            if (displayAttribute) {
-                                formattedAttributes.push({ ...entity, value: displayAttribute.value });
-                            }
-                        });
-                        resolve(formattedAttributes);
-                    });
-            },
-        );
-    });
     return result;
 };
