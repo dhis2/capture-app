c5837fbcf2f4f05642eba79520b2a6bebea7657e
huocha <huongtranguyen.ndht@gmail.com>
2021-04-26 07:46:55 +0200

    chore: [DHIS2-10947] remove unused css

--- src/core_modules/capture-ui/FlatList/FlatList.component.js
+++ src/core_modules/capture-ui/FlatList/FlatList.component.js
@@ -1,15 +1,13 @@
 // @flow
 import React from 'react';
-import cx from 'classnames';
 import uuid from 'uuid/v4';
-import classes from './FlatList.module.css';
 import type { Props } from './flatList.types';
 
 export const FlatList = ({ list, className, dataTest, renderItem, ...passOnProps }: Props) => (
     <div
         {...passOnProps}
         data-test={dataTest}
-        className={cx(classes.button, className)}
+        className={className}
     >
         {list.map(item => (<div key={uuid()}>{renderItem(item)}</div>))}
     </div>
