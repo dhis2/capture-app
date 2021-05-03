7961ded38fd1ee32f74d5801543d920933c1b4ef
Simona Domnisoru <simona.domnisoru@gmail.com>
2021-05-03 08:07:36 +0200

    fix: [DHIS-8998] unspllit and escape translatable strings
    (cherry picked from commit 475b27d142c52672efe9a7b399fb92342bcc1837)

--- src/core_modules/capture-core/hooks/useScopeTitleText.js
+++ src/core_modules/capture-core/hooks/useScopeTitleText.js
@@ -7,10 +7,15 @@ export const useScopeTitleText = (scopeId: ?string) => {
     const { trackedEntityName, programName, scopeType } = useScopeInfo(scopeId);
 
     const text = {
-        [scopeTypes.EVENT_PROGRAM]: `${programName}`,
+        [scopeTypes.EVENT_PROGRAM]: programName,
         [scopeTypes.TRACKER_PROGRAM]:
-            `${i18n.t('{{trackedEntityName}} in program', { trackedEntityName })}: ${programName}`,
-        [scopeTypes.TRACKED_ENTITY_TYPE]: `${trackedEntityName}`,
+            i18n.t('{{trackedEntityName}} in program{{escape}} {{programName}}', {
+                trackedEntityName,
+                escape: ':',
+                programName,
+                interpolation: { escapeValue: false },
+            }),
+        [scopeTypes.TRACKED_ENTITY_TYPE]: trackedEntityName,
     };
 
     return scopeType ? text[scopeType] : '';
