a4bad210de2dfd90396375df58347e90f2ba825e
huocha <huongtranguyen.ndht@gmail.com>
2021-05-10 12:22:23 +0200

    feat: [DHIS2- 11069] run rules for enrollment pages

--- src/core_modules/capture-core/components/Pages/Enrollment/EnrollmentPageDefault/EnrollmentPageDefault.container.js
+++ src/core_modules/capture-core/components/Pages/Enrollment/EnrollmentPageDefault/EnrollmentPageDefault.container.js
@@ -1,20 +1,84 @@
 // @flow
-import React from 'react';
+import React, { useMemo } from 'react';
 // $FlowFixMe
 import { useSelector, shallowEqual } from 'react-redux';
+import log from 'loglevel';
+import { errorCreator } from 'capture-core-utils';
+import { useDataQuery } from '@dhis2/app-runtime';
 import { useProgramInfo } from '../../../../hooks/useProgramInfo';
 import { EnrollmentPageDefaultComponent } from './EnrollmentPageDefault.component';
+import runRulesForEnrollmentPage from '../../../../rules/actionsCreator/runRulesForEnrollmentPage';
+
 
 export const EnrollmentPageDefault = () => {
-    const { teiId, programId } = useSelector(({
+    const { teiId, programId, orgUnitId } = useSelector(({
         router: {
             location: {
                 query,
             },
         },
-    }) => ({ teiId: query.teiId, programId: query.programId }), shallowEqual);
+    }) => ({ teiId: query.teiId, programId: query.programId, orgUnitId: query.orgUnitId }), shallowEqual);
+
+    const orgUnit = useSelector(({ organisationUnits }) => organisationUnits[orgUnitId], shallowEqual);
+    const [dataElements, setDataElements] = React.useState({});
 
     const { program } = useProgramInfo(programId);
+
+
+    const programsQuery = useMemo(() => ({
+        programRules: {
+            resource: 'programs',
+            id: programId,
+            params: {
+                fields:
+                ['programStages[programStageDataElements[dataElement[id,valueType,optionSet[id,version]]'],
+            },
+        },
+    }), [programId]);
+
+
+    const { data, error: programStageError } = useDataQuery(programsQuery);
+    if (programStageError) {
+        log.error(errorCreator('Profile widget could not be loaded')({ programStageError }));
+    }
+
+    React.useEffect(() => {
+        if (!Object.keys(dataElements).length && data) {
+            setDataElements(extractDataElements(data));
+        }
+    }, [data, dataElements]);
+
+    const extractDataElements = (rules) => {
+        if (!rules?.programRules) { return {}; }
+        return rules.programRules.programStages
+            .reduce((acc, curr) => {
+                curr.programStageDataElements.forEach((stage) => {
+                    acc[stage.dataElement.id] = { ...stage.dataElement };
+                });
+                return acc;
+            }, {});
+    };
+
+    const [, setIndicators] = React.useState([]);
+
+    React.useEffect(() => {
+        if (Object.keys(dataElements).length) {
+            const rules = runRulesForEnrollmentPage(program,
+                orgUnit,
+                program.stages,
+                program.stages,
+                dataElements,
+            );
+            setIndicators(rules
+                .filter(rule => rule.id === 'indicators')
+                .reduce((acc, curr) => {
+                    if (!acc.displayKeyValuePairs) { acc.displayKeyValuePairs = []; }
+                    acc.displayKeyValuePairs.push(curr.displayKeyValuePair);
+                    return acc;
+                }, {},
+                ));
+        }
+    }, [orgUnit, program, dataElements]);
 
 
     return (
---
+++ src/core_modules/capture-core/rules/actionsCreator/runRulesForEnrollmentPage.js
@@ -0,0 +1,35 @@
+// @flow
+import { RulesEngine } from '../engine';
+import type { Program } from '../../metaData';
+import type {
+    EventsData,
+    EventData,
+    OrgUnit,
+} from '../engine';
+import constantsStore from '../../metaDataMemoryStores/constants/constants.store';
+import optionSetsStore from '../../metaDataMemoryStores/optionSets/optionSets.store';
+
+
+export default function runRulesForEnrollmentPage(
+    program: Program,
+    orgUnit: OrgUnit,
+    currentEvent: EventData,
+    allEventsData: EventsData,
+    dataElementsInProgram: any,
+) {
+    const { programRuleVariables: programRulesVariables } = program;
+    const programRules = [...program.programRules];
+
+    const constants = constantsStore.get();
+    const optionSets = optionSetsStore.get();
+
+    // returns an array of effects that need to take place in the UI.
+    return RulesEngine.programRuleEffectsForEvent(
+        { programRulesVariables, programRules, constants },
+        { currentEvent, allEventsData },
+        dataElementsInProgram,
+        orgUnit,
+        // $FlowFixMe[prop-missing] automated comment
+        optionSets,
+    );
+}
