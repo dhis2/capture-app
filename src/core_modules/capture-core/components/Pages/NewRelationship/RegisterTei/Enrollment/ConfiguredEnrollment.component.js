481e596c6bfd911932243b4cdc8ed4d749365d09
EirikHaugstulen <eirik.haugstulen@gmail.com>
2021-05-11 10:51:04 +0200

    fix: changed new configured enrollment relationship behaviour to always completing

--- src/core_modules/capture-core/components/Pages/NewRelationship/RegisterTei/Enrollment/ConfiguredEnrollment.component.js
+++ src/core_modules/capture-core/components/Pages/NewRelationship/RegisterTei/Enrollment/ConfiguredEnrollment.component.js
@@ -9,7 +9,7 @@ import withMainButton from './withMainButton';
 
 const SaveHandlerHOC =
     withSaveHandler(
-        { onGetFormFoundation: (props: Object) => props.enrollmentMetadata.enrollmentForm })(
+        { onGetFormFoundation: (props: Object) => props.enrollmentMetadata.enrollmentForm, onIsCompleting: () => true })(
         withMainButton()(EnrollmentDataEntry));
 
 export default SaveHandlerHOC;
