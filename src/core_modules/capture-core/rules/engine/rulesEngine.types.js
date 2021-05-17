c7e073e464029a6ffd8221e2816e9e2051a4db1c
Alexis Rico <sferadev@gmail.com>
2021-05-17 12:43:09 +0200

    fix: Program rules for org unit data elements

--- src/core_modules/capture-core/rules/engine/rulesEngine.types.js
+++ src/core_modules/capture-core/rules/engine/rulesEngine.types.js
@@ -221,6 +221,7 @@ export interface IConvertInputRulesValue {
     convertPercentage(value: any): number | string;
     convertUrl(value: any): string;
     convertAge(value: any): number | string;
+    convertOrgUnit(value: any): string;
 }
 
 export interface IConvertOutputRulesEffectsValue {
@@ -242,6 +243,7 @@ export interface IConvertOutputRulesEffectsValue {
     convertPercentage(value: number): any;
     convertUrl(value: string): any;
     convertAge(value: string): any;
+    convertOrgUnit(value: any): string;
 }
 
 export type D2FunctionParameters = {
