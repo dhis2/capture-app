1c143150a6d8be04ed894f881b198b36bc4a1b40
huocha <huongtranguyen.ndht@gmail.com>
2021-05-20 09:25:36 +0200

    chore: [DSHI2-10218] escape special character

--- src/core_modules/capture-core/metaData/DataElement/statusTypes.js
+++ src/core_modules/capture-core/metaData/DataElement/statusTypes.js
@@ -12,7 +12,11 @@ export const statusTypes = {
 
 export const translatedStatusTypes = (options?: string) => ({
     [statusTypes.ACTIVE]: i18n.t('Active'),
-    [statusTypes.SCHEDULE]: options ? i18n.t('Scheduled due {{ time }}', { time: options }) : i18n.t('Scheduled'),
+    [statusTypes.SCHEDULE]: options ? i18n.t('Scheduled{{ escape }} due {{ time }}', {
+        time: options,
+        escape: ':',
+        interpolation: { escapeValue: false },
+    }) : i18n.t('Scheduled'),
     [statusTypes.COMPLETED]: i18n.t('Completed'),
     [statusTypes.OVERDUE]: i18n.t('Overdue'),
     [statusTypes.SKIPPED]: i18n.t('Skipped'),
