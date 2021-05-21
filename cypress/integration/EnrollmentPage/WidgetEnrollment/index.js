7a9158aadb3bebba72e0b085a04d0b66aba5141c
Simona Domnisoru <simona.domnisoru@gmail.com>
2021-05-21 13:07:26 +0200

    fix: use cypress intercept

--- cypress/integration/EnrollmentPage/WidgetEnrollment/index.js
+++ cypress/integration/EnrollmentPage/WidgetEnrollment/index.js
@@ -73,14 +73,16 @@ When('the user opens the enrollment actions menu', () => {
     cy.get('[data-test="widget-enrollment"]').within(() => {
         cy.get('[data-test="widget-enrollment-actions-button"]').click();
     });
+    cy.intercept({
+        method: 'PUT',
+        url: '**/enrollments/kcX0O3juDAH',
+    }).as('putEnrollment');
 });
 
-When(/^the user changes the enrollment status to (.*)$/, status =>
-    cy
-        .get(`[data-test="widget-enrollment-actions-${status}"]`)
-        .click()
-        .wait(500),
-);
+When(/^the user changes the enrollment status to (.*)$/, (status) => {
+    cy.get(`[data-test="widget-enrollment-actions-${status}"]`).click();
+    cy.wait('@putEnrollment');
+});
 
 Then(/^the user sees the enrollment status is (.*)$/, status =>
     cy.get('[data-test="widget-enrollment"]').within(() => {
@@ -90,12 +92,12 @@ Then(/^the user sees the enrollment status is (.*)$/, status =>
     }),
 );
 
-When(/^the user (.*) the enrollment for followup/, action =>
-    cy
-        .get(`[data-test="widget-enrollment-actions-followup-${action}"]`)
-        .click()
-        .wait(500),
-);
+When(/^the user (.*) the enrollment for followup/, (action) => {
+    cy.get(
+        `[data-test="widget-enrollment-actions-followup-${action}"]`,
+    ).click();
+    cy.wait('@putEnrollment');
+});
 
 Then(/^the user can see the enrollment is ?(.*) marked for follow up/, not =>
     cy.get('[data-test="widget-enrollment"]').within(() => {
