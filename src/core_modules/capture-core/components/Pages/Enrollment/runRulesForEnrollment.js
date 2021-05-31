1f0bb52b10bb1528b83a8660bcb938f50e714e0c
huocha <huongtranguyen.ndht@gmail.com>
2021-05-31 16:29:10 +0200

    feat: [DHIS2-11069] get element values

--- src/core_modules/capture-core/components/Pages/Enrollment/runRulesForEnrollment.js
+++ src/core_modules/capture-core/components/Pages/Enrollment/runRulesForEnrollment.js
@@ -1,29 +1,36 @@
 // @flow
+import { convertValue } from 'capture-core/converters/serverToClient';
 import runRulesForEnrollmentPage from '../../../rules/actionsCreator/runRulesForEnrollmentPage';
 
-const getDataElementsFromProgram = (data) => {
+
+const getDataElementsFromProgram = (data, eventsDataValues) => {
     if (!data?.programStages) { return {}; }
     return data.programStages
         .reduce((acc, curr) => {
             curr.programStageDataElements.forEach((stage) => {
-                acc[stage.dataElement.id] = { ...stage.dataElement };
+                const { id, valueType } = stage.dataElement;
+                const { value } = eventsDataValues.find(item => item.dataElement === id) || { };
+                acc[id] = { ...stage.dataElement };
+                if (value) {
+                    acc[stage.dataElement.id].value = convertValue(value, valueType);
+                }
             });
             return acc;
         }, {});
 };
 
-const getEventValuesFromEnrollment = (enrollment) => {
+const getEventValuesFromEnrollment = (enrollment, dataElements) => {
     const eventValues = enrollment.events.reduce((acc, currentEvent) => {
         currentEvent.dataValues.forEach((dataValue) => {
-            const { dataElement, value } = dataValue;
-            acc[dataElement] = { value };
+            const { dataElement } = dataValue;
+            acc[dataElement] = dataElements[dataElement].value;
         });
         return acc;
     }, {});
     return eventValues;
 };
 
-const getEventsDataFromEnrollment = enrollment => enrollment.events.map(event => ({
+const getEventsDataFromEnrollment = (enrollment, dataElements) => enrollment.events.map(event => ({
     eventId: event.event,
     programId: event.program,
     programStageId: event.programStage,
@@ -35,31 +42,40 @@ const getEventsDataFromEnrollment = enrollment => enrollment.events.map(event =>
     status: event.status,
     eventDate: event.eventDate,
     dueDate: event.dueDate,
-    ...getEventValuesFromEnrollment(enrollment),
+    ...getEventValuesFromEnrollment(enrollment, dataElements),
 }));
+
+
+const getEnrollmentData = enrollment => ({ enrollmentDate: enrollment.enrollmentDate,
+    incidentDate: enrollment.incidentDate,
+    enrollmentId: enrollment.enrollmentId });
+
+const flatDataValuesFromEvents = events => events.reduce((acc, currentEvent) => {
+    acc = [...acc, ...(currentEvent.dataValues)];
+    return acc;
+}, []);
 
 // $FlowFixMe
 export const runRulesForEnrollment = ({ orgUnit, program, programMetadata, enrollments, attributes }: Object) => {
     if (programMetadata && attributes && enrollments) {
-        const dataElements = getDataElementsFromProgram(programMetadata);
+        const dataValueList = flatDataValuesFromEvents(enrollments[0].events);
+        const dataElements = getDataElementsFromProgram(programMetadata, dataValueList);
 
         const trackedEntityAttributes = attributes?.map(item => ({ id: item.attribute, valueType: item.valueType }));
         const teiAttributesValues = attributes?.reduce((acc, item) => {
             acc[item.attribute] = item.value;
             return acc;
         }, {});
-        const eventsData = getEventsDataFromEnrollment(enrollments[0]);
+        const eventsData = getEventsDataFromEnrollment(enrollments[0], dataElements);
 
         return runRulesForEnrollmentPage({
             program,
             orgUnit,
-            allEventsData: eventsData,
+            allEventsData: { all: eventsData },
             dataElementsInProgram: dataElements,
             teiValues: teiAttributesValues,
             trackedEntityAttributes,
-            enrollmentData: { enrollmentDate: enrollments[0].enrollmentDate,
-                incidentDate: enrollments[0].incidentDate,
-                enrollmentId: enrollments[0].enrollmentId },
+            enrollmentData: getEnrollmentData(enrollments[0]),
         });
     }
     return undefined;
