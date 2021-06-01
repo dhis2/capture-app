178f84fb8a8a69ed7237c225bde22dc9af966f31
EirikHaugstulen <eirik.haugstulen@gmail.com>
2021-06-01 14:41:00 +0200

    fix: changed to global color constants & renderfunctions

--- src/core_modules/capture-core/components/WidgetFeedback/WidgetFeedbackContent/WidgetFeedbackContent.js
+++ src/core_modules/capture-core/components/WidgetFeedback/WidgetFeedbackContent/WidgetFeedbackContent.js
@@ -15,46 +15,56 @@ const styles = {
         marginTop: '0px',
         lineHeight: '1.375',
         fontSize: '14px',
-        color: colors.gray900,
+        color: colors.grey900,
     },
     noFeedbackText: {
-        color: colors.gray600,
+        color: colors.grey600,
         paddingLeft: '15px',
+        fontWeight: 400,
         fontSize: '14px',
         marginTop: '0px',
     },
     listItem: {
         marginBottom: '5px',
         '&::marker': {
-            color: colors.gray500,
+            color: colors.grey500,
         },
     },
 };
+
 
 const WidgetFeedbackContentComponent = ({ displayText, displayKeyValue, classes }: Props) => {
     if ((!displayText || displayText.length <= 0) && (!displayKeyValue || displayKeyValue.length <= 0)) {
         return <p className={classes.noFeedbackText}>{i18n.t('No feedback for this enrollment yet')}</p>;
     }
 
+    const renderText = () => (
+        displayText && displayText.length > 0 ? displayText.map(item => (
+            item.message && (
+                <li
+                    className={classes.listItem}
+                    key={item.id}
+                >
+                    {item.message}
+                </li>))) : null
+    );
+
+    const renderKeyValue = () => (
+        displayKeyValue && displayKeyValue.length > 0 ? displayKeyValue.map(item => (
+            item.key && item.value && (
+                <li
+                    key={item.id}
+                    className={classes.listItem}
+                >
+                    {item.key}: {item.value}
+                </li>))) : null
+    );
+
     return (
         <div className={classes.container}>
             <ul className={classes.unorderedList}>
-                {displayText && displayText.length > 0 ? displayText.map(item => (
-                    item.message && (
-                        <li
-                            className={classes.listItem}
-                            key={item.id}
-                        >
-                            {item.message}
-                        </li>))) : null}
-                {displayKeyValue && displayKeyValue.length > 0 ? displayKeyValue.map(item => (
-                    item.key && item.value && (
-                        <li
-                            key={item.id}
-                            className={classes.listItem}
-                        >
-                            {item.key}: {item.value}
-                        </li>))) : null}
+                {renderText()}
+                {renderKeyValue()}
             </ul>
         </div>
     );
