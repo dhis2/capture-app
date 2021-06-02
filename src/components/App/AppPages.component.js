023de5ba59c1b9cfd9fda891f6d710520ed0ab57
huocha <huongtranguyen.ndht@gmail.com>
2021-06-02 08:06:54 +0200

    feat: [DHIS2-11221] Program stage event list page with routing

--- src/components/App/AppPages.component.js
+++ src/components/App/AppPages.component.js
@@ -6,12 +6,14 @@ import { MainPage } from 'capture-core/components/Pages/MainPage';
 import { SearchPage } from 'capture-core/components/Pages/Search';
 import { NewPage } from 'capture-core/components/Pages/New';
 import { EnrollmentPage } from 'capture-core/components/Pages/Enrollment';
+import { StageEventListPage } from 'capture-core/components/Pages/StageEvent';
 
 export const AppPages = () => (
     <Switch>
         <Route path="/viewEvent" component={ViewEventPage} />
         <Route path="/search" component={SearchPage} />
         <Route path="/new" component={NewPage} />
+        <Route path="/enrollment/stageEvents" component={StageEventListPage} />
         <Route path="/enrollment" component={EnrollmentPage} />
         <Route path="/:keys" component={MainPage} />
         <Route path="/" component={MainPage} />
---
+++ src/core_modules/capture-core/components/Pages/StageEvent/index.js
@@ -0,0 +1,2 @@
+// @flow
+export { StageEventListPage } from './StageEventListPage.container';
