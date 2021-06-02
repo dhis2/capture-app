d97d89888c50c1f0dda192415dbd2f255c266307
huocha <huongtranguyen.ndht@gmail.com>
2021-06-02 09:30:59 +0200

    chore: [DHSI2-11221] add test

--- src/core_modules/capture-core/components/Pages/StageEvent/StageEventList/StageEventList.component.js
+++ src/core_modules/capture-core/components/Pages/StageEvent/StageEventList/StageEventList.component.js
@@ -2,16 +2,14 @@
 import React from 'react';
 import type { ComponentType } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
-import { compose } from 'redux';
 import type { Props } from '../StageEventListPage.types';
 
 const getStyles = () => ({});
 
 const StageEventListComponentPlain = () => (<>
-    <div>Stages and Events</div>
+    <div data-test="stage-event-list">Stages and Events</div>
 </>);
 
-export const StageEventListComponent: ComponentType<$Diff<Props, CssClasses>> =
-  compose(
-      withStyles(getStyles),
-  )(StageEventListComponentPlain);
+export const StageEventListComponent: ComponentType<$Diff<Props, CssClasses>> = withStyles(
+    getStyles,
+)(StageEventListComponentPlain);
