aa15612354a543992c97f5c9c5252f36da43037e
EirikHaugstulen <eirik.haugstulen@gmail.com>
2021-06-24 09:52:26 +0200

    fix: Code Review-Comments

--- src/core_modules/capture-core/components/Pages/Enrollment/EnrollmentPageDefault/EnrollmentPageDefault.component.js
+++ src/core_modules/capture-core/components/Pages/Enrollment/EnrollmentPageDefault/EnrollmentPageDefault.component.js
@@ -2,9 +2,10 @@
 import React, { type ComponentType } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { spacersNum } from '@dhis2/ui';
+import i18n from '@dhis2/d2-i18n';
 import { WidgetStagesAndEvents } from '../../../WidgetStagesAndEvents';
 import { WidgetEnrollment } from '../../../WidgetEnrollment';
-import { WidgetProfile } from '../../../WidgetProfile/WidgetProfile.component';
+import { WidgetProfile } from '../../../WidgetProfile';
 import type { Props, PlainProps } from './EnrollmentPageDefault.types';
 import { WidgetFeedback } from '../../../WidgetFeedback';
 
@@ -49,10 +50,12 @@ export const EnrollmentPageDefaultPlain = ({
                 <WidgetStagesAndEvents stages={program.stages} />
             </div>
             <div className={classes.rightColumn}>
-                <WidgetFeedback
-                    feedback={widgetEffects?.feedbacks}
-                    hideWidget={hideFeedbackWidget}
-                />
+                {!hideFeedbackWidget && (
+                    <WidgetFeedback
+                        feedback={widgetEffects?.feedbacks}
+                        emptyText={i18n.t('No feedback for this enrollment yet')}
+                    />
+                )}
                 <WidgetProfile teiId={teiId} programId={program.id} />
                 <WidgetEnrollment
                     teiId={teiId}
