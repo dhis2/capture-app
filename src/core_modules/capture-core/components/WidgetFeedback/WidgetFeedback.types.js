a8d5760074745cbfef28680428aae3d3f7d1f39c
EirikHaugstulen <eirik.haugstulen@gmail.com>
2021-06-30 11:52:33 +0200

    feat: DHIS2-10981 Feedback widget

--- src/core_modules/capture-core/components/WidgetFeedback/WidgetFeedback.types.js
+++ src/core_modules/capture-core/components/WidgetFeedback/WidgetFeedback.types.js
@@ -1,5 +1,26 @@
 // @flow
 
+export type FilteredText = {|
+    id: string,
+    message: string,
+|}
+
+export type FilteredKeyValue = {|
+    id: string,
+    key: string,
+    value: string,
+|}
+
+export type WidgetData = string | FilteredText | FilteredKeyValue;
+
+export type ContentType = {|
+    widgetData?: ?Array<WidgetData>,
+    emptyText: string,
+    ...CssClasses
+|}
+
 export type Props = {|
-    className: ?string,
+    feedback?: ?Array<string | FilteredText | FilteredKeyValue>,
+    emptyText: string,
+    ...CssClasses
 |}
