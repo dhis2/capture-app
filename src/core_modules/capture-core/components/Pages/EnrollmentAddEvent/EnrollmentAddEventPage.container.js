6ee3dd582779e1d3d41d5be710ac2ffcc9fcbc0a
huocha <huongtranguyen.ndht@gmail.com>
2021-07-06 13:41:14 +0200

    chore:[DHIS2-11388] handle program state selector

--- src/core_modules/capture-core/components/Pages/EnrollmentAddEvent/EnrollmentAddEventPage.container.js
+++ src/core_modules/capture-core/components/Pages/EnrollmentAddEvent/EnrollmentAddEventPage.container.js
@@ -1,6 +1,5 @@
 // @flow
 import React from 'react';
-import i18n from '@dhis2/d2-i18n';
 // $FlowFixMe
 import { useSelector, shallowEqual } from 'react-redux';
 import { useProgramInfo } from '../../../hooks/useProgramInfo';
@@ -21,7 +20,7 @@ export const EnrollmentAddEventPage = () => {
     const { program } = useProgramInfo(programId);
     const programStage = [...program.stages.values()].find(item => item.id === stageId);
     if (!programStage) {
-        return <span> {i18n.t('We could not find the stage in the program')}</span>;
+        return <span>[program stage placeholder]</span>;
     }
     return (
         <EnrollmentAddEventPageComponent icon={programStage.icon} name={programStage.stageForm.name} />
