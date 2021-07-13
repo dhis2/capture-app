10fa980a59824631a672963a150007435331b31a
Jasmine <huongtranguyen.ndht@gmail.com>
2021-07-13 10:59:19 +0200

    DHIS2-11392: 6b. create widget title icon Add Event (#1886)
    * feat: [DHIS2-11392] create widget and icon for add event in enrollment
    
    * feat: [DHIS2-11392] add test
    
    * chore: [DHIS2-11392] change props type of add event page

--- cypress/integration/EnrollmentAddEventPage/EnrollmentAddEventPageNavigation/index.js
+++ cypress/integration/EnrollmentAddEventPage/EnrollmentAddEventPageNavigation/index.js
@@ -9,3 +9,12 @@ Given(/^you land on the enrollment event page by having typed (.*)$/, (url) => {
 Then(/^you see the following (.*)$/, (message) => {
     cy.contains(message);
 });
+
+And(/^you see the widget header (.*)$/, (name) => {
+    cy.get('[data-test="add-event-enrollment-page-content"]')
+        .within(() => {
+            cy.get('[data-test="widget-contents"]').should('exist');
+            cy.get('[data-test="widget-header"]').should('exist');
+            cy.contains(name).should('exist');
+        });
+});
--- src/core_modules/capture-core/components/Pages/EnrollmentAddEvent/EnrollmentAddEventPage.component.js
+++ src/core_modules/capture-core/components/Pages/EnrollmentAddEvent/EnrollmentAddEventPage.component.js
@@ -5,10 +5,20 @@ import i18n from '@dhis2/d2-i18n';
 import { spacersNum } from '@dhis2/ui';
 import withStyles from '@material-ui/core/styles/withStyles';
 import type { Props } from './EnrollmentAddEventPage.types';
+import { NonBundledDhis2Icon } from '../../NonBundledDhis2Icon';
+import { Widget } from '../../Widget';
 
 const styles = ({ typography }) => ({
     container: {
         padding: '16px 24px 16px 24px',
+    },
+    header: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: spacersNum.dp8,
+    },
+    icon: {
+        paddingRight: spacersNum.dp8,
     },
     title: {
         ...typography.title,
@@ -17,14 +27,46 @@ const styles = ({ typography }) => ({
 });
 
 const EnrollmentAddEventPagePain = ({
+    programStage,
     classes,
-}) => (
-    <div className={classes.container}>
-        <div className={classes.title}>
-            {i18n.t('Enrollment{{escape}} New Event', { escape: ':' })}
+}) => {
+    const { icon, stageForm } = programStage;
+
+    return (
+        <div
+            className={classes.container}
+            data-test="add-event-enrollment-page-content"
+        >
+            <div className={classes.title}>
+                {i18n.t('Enrollment{{escape}} New Event', { escape: ':' })}
+            </div>
+            <div>
+                <Widget
+                    header={
+                        <div className={classes.header}>
+                            {icon && (
+                                <div className={classes.icon}>
+                                    <NonBundledDhis2Icon
+                                        name={icon?.name}
+                                        color={icon?.color}
+                                        width={30}
+                                        height={30}
+                                        cornerRadius={2}
+                                    />
+                                </div>
+                            )}
+                            <span> {stageForm.name} </span>
+                        </div>
+                    }
+                    noncollapsible
+                >
+                    [event details]
+                </Widget>
+            </div>
+
         </div>
-    </div>
-);
+    );
+};
 
 export const EnrollmentAddEventPageComponent: ComponentType<
     $Diff<Props, CssClasses>,
