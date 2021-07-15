10ca83a8877782cc72aaecd2c6a18fe1f2173f79
EirikHaugstulen <eirik.haugstulen@gmail.com>
2021-07-15 11:28:27 +0200

    fix: flow

--- src/core_modules/capture-core/components/Pages/EnrollmentAddEvent/EnrollmentAddEventPage.types.js
+++ src/core_modules/capture-core/components/Pages/EnrollmentAddEvent/EnrollmentAddEventPage.types.js
@@ -1,10 +1,13 @@
 // @flow
-import { ProgramStage } from '../../../metaData';
+import { Program, ProgramStage } from '../../../metaData';
 import type { WidgetEffects } from '../Enrollment/EnrollmentPageDefault/EnrollmentPageDefault.types';
 
 export type Props = {|
     programStage: ProgramStage,
     widgetEffects: ?WidgetEffects,
+    program: Program,
+    teiId: string,
+    enrollmentId: string,
     onDelete: () => void,
     ...CssClasses,
 |};
