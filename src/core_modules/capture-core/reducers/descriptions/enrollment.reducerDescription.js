3fe30974e65aa11afa71b03051dae73a0d73da49
Simona Domnisoru <simona.domnisoru@gmail.com>
2021-07-20 09:59:58 +0200

    feat: rollback & commit the event

--- src/core_modules/capture-core/reducers/descriptions/enrollment.reducerDescription.js
+++ src/core_modules/capture-core/reducers/descriptions/enrollment.reducerDescription.js
@@ -3,7 +3,12 @@ import { createReducerDescription } from '../../trackerRedux/trackerReducer';
 import { enrollmentActionTypes } from '../../components/Pages/actions/enrollment.actions';
 
 const initialReducerValue = {};
-const { SET_ENROLLMENT } = enrollmentActionTypes;
+const {
+    SET_ENROLLMENT,
+    UPDATE_ENROLLMENT_EVENTS,
+    ROLLBACK_ENROLLMENT_EVENT,
+    COMMIT_ENROLLMENT_EVENT,
+} = enrollmentActionTypes;
 
 export const enrollmentDesc = createReducerDescription(
     {
@@ -11,6 +16,45 @@ export const enrollmentDesc = createReducerDescription(
             ...state,
             ...enrollmentSite,
         }),
+        [UPDATE_ENROLLMENT_EVENTS]: (
+            state,
+            { payload: { eventId, eventData } },
+        ) => {
+            const events = state.events.map(event =>
+                (event.event === eventId
+                    ? {
+                        ...eventData,
+                        pendingApiResponse: true,
+                        dataToRollback: event,
+                    }
+                    : event),
+            );
+
+            return { ...state, events };
+        },
+        [ROLLBACK_ENROLLMENT_EVENT]: (state, { payload: { eventId } }) => {
+            const events = state.events.map(event =>
+                (event.event === eventId ? event.dataToRollback : event),
+            );
+
+            return { ...state, events };
+        },
+        [COMMIT_ENROLLMENT_EVENT]: (state, { payload: { eventId } }) => {
+            const events = state.events.map((event) => {
+                if (event.event === eventId) {
+                    const {
+                        pendingApiResponse,
+                        dataToRollback,
+                        ...dataToCommit
+                    } = event;
+                    console.log(pendingApiResponse);
+                    return dataToCommit;
+                }
+                return event;
+            });
+
+            return { ...state, events };
+        },
     },
     'enrollmentSite',
     initialReducerValue,
