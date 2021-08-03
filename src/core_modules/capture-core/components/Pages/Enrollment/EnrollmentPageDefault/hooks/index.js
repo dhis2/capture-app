b78a5c4a414ab1b7089ce87a5a075a8ea4be04ed
EirikHaugstulen <eirik.haugstulen@gmail.com>
2021-08-03 10:15:03 +0200

    chore: hook-rename

--- src/core_modules/capture-core/components/Pages/Enrollment/EnrollmentPageDefault/hooks/index.js
+++ src/core_modules/capture-core/components/Pages/Enrollment/EnrollmentPageDefault/hooks/index.js
@@ -1,6 +1,6 @@
 import { useEnrollment } from './useEnrollment';
 import { useTeiAttributes } from './useTeiAttributes';
 import { useProgramMetadata } from './useProgramMetadata';
-import { useHideWidgetByRuleLocations } from './useRuleActionLocations';
+import { useHideWidgetByRuleLocations } from './useHideWidgetByRuleLocations';
 
 export { useEnrollment, useProgramMetadata, useTeiAttributes, useHideWidgetByRuleLocations };
