c9538e143de4e6ebf191b727de55cfcdc475da54
Joakim Storløkken Melseth <joakim.melseth@gmail.com>
2021-08-31 15:20:55 +0200

    chore: minor

--- src/core_modules/capture-core/components/Pages/EnrollmentEditEvent/EnrollmentEditEventPage.types.js
+++ src/core_modules/capture-core/components/Pages/EnrollmentEditEvent/EnrollmentEditEventPage.types.js
@@ -1,6 +1,6 @@
 // @flow
 import type { ProgramStage } from '../../../metaData';
-import type { WidgetEffects, HideWidgets } from '../Enrollment/EnrollmentPageDefault/EnrollmentPageDefault.types';
+import type { WidgetEffects, HideWidgets } from '../common/EnrollmentOverviewDomain';
 
 export type PlainProps = {|
     programStage: ?ProgramStage,
