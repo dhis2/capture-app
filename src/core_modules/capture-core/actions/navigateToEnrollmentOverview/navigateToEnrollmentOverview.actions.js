da70dda9f0b7613b03d7e6aa2fbe7d0c747d02b9
EirikHaugstulen <eirik.haugstulen@gmail.com>
2021-09-14 11:02:01 +0200

    chore: seperated type decleration into own file

--- src/core_modules/capture-core/actions/navigateToEnrollmentOverview/navigateToEnrollmentOverview.actions.js
+++ src/core_modules/capture-core/actions/navigateToEnrollmentOverview/navigateToEnrollmentOverview.actions.js
@@ -1,17 +1,11 @@
 // @flow
 
 import { actionCreator } from '../actions.utils';
+import type { NavigateToEnrollmentOverviewProps } from './navigateToEnrollmentOverview.types';
 
 export const actionTypes = Object.freeze({
     NAVIGATE_TO_ENROLLMENT_OVERVIEW: 'enrollmentNavigation.navigateToEnrollmentOverview',
 });
-
-type NavigateToEnrollmentOverviewProps = {|
-    teiId?: string,
-    programId?: string,
-    orgUnitId?: string,
-    enrollmentId?: string,
-|}
 
 export const navigateToEnrollmentOverview = ({ teiId, programId, orgUnitId, enrollmentId }: NavigateToEnrollmentOverviewProps) =>
     actionCreator(actionTypes.NAVIGATE_TO_ENROLLMENT_OVERVIEW)({ teiId, programId, orgUnitId, enrollmentId });
