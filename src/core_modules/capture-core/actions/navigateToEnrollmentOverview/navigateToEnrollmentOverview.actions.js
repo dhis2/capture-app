5a6e7fe4abb37fff2a5ddeb749557ebaf5eff1ea
EirikHaugstulen <eirik.haugstulen@gmail.com>
2021-09-14 15:44:49 +0200

    fix: [TECH-672] Navigate to correct dashboard based on dataStore config  (#2099)

---
+++ src/core_modules/capture-core/actions/navigateToEnrollmentOverview/navigateToEnrollmentOverview.actions.js
@@ -0,0 +1,11 @@
+// @flow
+
+import { actionCreator } from '../actions.utils';
+import type { NavigateToEnrollmentOverviewProps } from './navigateToEnrollmentOverview.types';
+
+export const actionTypes = Object.freeze({
+    NAVIGATE_TO_ENROLLMENT_OVERVIEW: 'enrollmentNavigation.navigateToEnrollmentOverview',
+});
+
+export const navigateToEnrollmentOverview = ({ teiId, programId, orgUnitId, enrollmentId }: NavigateToEnrollmentOverviewProps) =>
+    actionCreator(actionTypes.NAVIGATE_TO_ENROLLMENT_OVERVIEW)({ teiId, programId, orgUnitId, enrollmentId });
