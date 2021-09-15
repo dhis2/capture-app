382cdc28941b2ce4beecf6da16297fa02b4a5006
Simona Domnisoru <simona.domnisoru@gmail.com>
2021-09-15 15:53:24 +0300

    fix: add cypress test

--- cypress/integration/WidgetsForEnrollmentPages/WidgetsForEnrollmentDashboard/index.js
+++ cypress/integration/WidgetsForEnrollmentPages/WidgetsForEnrollmentDashboard/index.js
@@ -6,3 +6,7 @@ import '../WidgetEnrollmentComment';
 beforeEach(() => {
     cy.loginThroughForm();
 });
+
+Then('the user can see the program rules effect in the indicator widget', () => {
+    cy.get('[data-test="indicator-widget"]').contains('Measles + Yellow fever doses');
+});
