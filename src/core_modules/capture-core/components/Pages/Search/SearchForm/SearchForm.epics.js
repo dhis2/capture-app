adf6747d2d69f32df41b9f2e457b964d7055be52
EirikHaugstulen <eirik.haugstulen@gmail.com>
2021-09-20 14:27:39 +0200

    fix: convertValue in ElementConverters

--- src/core_modules/capture-core/components/Pages/Search/SearchForm/SearchForm.epics.js
+++ src/core_modules/capture-core/components/Pages/Search/SearchForm/SearchForm.epics.js
@@ -30,8 +30,7 @@ import {
     navigateToEnrollmentOverview,
 } from '../../../../actions/navigateToEnrollmentOverview/navigateToEnrollmentOverview.actions';
 import { dataElementConvertFunctions } from './SearchFormElementConverter/SearchFormElementConverter';
-import { pipe as pipeD2 } from '../../../../../capture-core-utils';
-import { convertClientToServer, convertFormToClient } from '../../../../converters';
+
 
 const getFiltersForUniqueIdSearchQuery = (formValues) => {
     const fieldId = Object.keys(formValues)[0];
@@ -68,9 +67,8 @@ const getFiltersForAttributesSearchQuery = (formValues, attributes) => Object.ke
         const dataElement = attributes.find(attribute => attribute.id === fieldId);
         if (formValues[fieldId] && dataElement) {
             const dataElementType = dataElementTypes[dataElement.type];
-            const serverValue = dataElement.convertValue(formValues[fieldId], pipeD2(convertFormToClient, convertClientToServer));
             // $FlowFixMe - Function does not require arguments if unsupported type
-            return dataElementConvertFunctions[dataElementType](serverValue, dataElement);
+            return dataElementConvertFunctions[dataElementType](formValues[fieldId], dataElement);
         }
         return null;
     });
