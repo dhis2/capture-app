c12c1d87d649acd41fe3526ec7a3193aa1edaa5b
EirikHaugstulen <eirik.haugstulen@gmail.com>
2021-09-22 12:51:33 +0200

    fix: added a searchable-attribute to built dataElements in a program-context

--- src/core_modules/capture-core/components/WorkingLists/WorkingListsBase/workingListsBase.types.js
+++ src/core_modules/capture-core/components/WorkingLists/WorkingListsBase/workingListsBase.types.js
@@ -43,6 +43,7 @@ export type ColumnConfig = {
     options?: ?Array<{text: string, value: any}>,
     multiValueFilter?: boolean,
     filterDisabled?: boolean,
+    filterHidden?: boolean,
 };
 
 export type ColumnConfigs = Array<ColumnConfig>;
