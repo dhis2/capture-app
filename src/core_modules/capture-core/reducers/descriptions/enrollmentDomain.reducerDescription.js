f437e8c10ace7ca21f0f0f6a61539b4b4eaa6b6e
Simona Domnisoru <simona.domnisoru@gmail.com>
2021-09-24 09:46:23 +0200

    fix: [DHIS2-11745][DHIS2-11797] add missing data & save event app crash (#2119) (#2130)
    * fix: add orgUnitName to the new enrollment event in Redux
    
    * fix: add missing data & save event app crash
    
    * fix: remove depedency to data from app sync & uid annotate
    
    (cherry picked from commit 21249761496627de755cbd121584b325e10146ff)

--- src/core_modules/capture-core/reducers/descriptions/enrollmentDomain.reducerDescription.js
+++ src/core_modules/capture-core/reducers/descriptions/enrollmentDomain.reducerDescription.js
@@ -27,7 +27,7 @@ export const enrollmentDomainDesc = createReducerDescription(
             state,
             { payload: { eventId, eventData } },
         ) => {
-            const events = state.events?.map(event =>
+            const events = state.enrollment.events?.map(event =>
                 (event.event === eventId
                     ? {
                         ...eventData,
@@ -37,17 +37,17 @@ export const enrollmentDomainDesc = createReducerDescription(
                     : event),
             );
 
-            return { ...state, events };
+            return { ...state, enrollment: { ...state.enrollment, events } };
         },
         [ROLLBACK_ENROLLMENT_EVENT]: (state, { payload: { eventId } }) => {
-            const events = state.events?.map(event =>
+            const events = state.enrollment.events?.map(event =>
                 (event.event === eventId ? event.dataToRollback : event),
             );
 
-            return { ...state, events };
+            return { ...state, enrollment: { ...state.enrollment, events } };
         },
         [COMMIT_ENROLLMENT_EVENT]: (state, { payload: { eventId } }) => {
-            const events = state.events?.map((event) => {
+            const events = state.enrollment.events?.map((event) => {
                 if (event.event === eventId) {
                     const {
                         pendingApiResponse,
@@ -59,28 +59,30 @@ export const enrollmentDomainDesc = createReducerDescription(
                 return event;
             });
 
-            return { ...state, events };
+            return { ...state, enrollment: { ...state.enrollment, events } };
         },
         [UPDATE_ENROLLMENT_EVENTS_WITHOUT_ID]: (
             state,
             { payload: { eventData, uid } },
         ) => {
-            const events = [...state.events, { ...eventData, uid, pendingApiResponse: true }];
-            return { ...state, events };
+            const events = [...state.enrollment.events, { ...eventData, uid, pendingApiResponse: true }];
+
+            return { ...state, enrollment: { ...state.enrollment, events } };
         },
         [ROLLBACK_ENROLLMENT_EVENT_WITHOUT_ID]: (state, { payload: { uid } }) => {
-            const events = state.events?.filter(event => (event.uid !== uid));
-            return { ...state, events };
+            const events = state.enrollment.events.filter(event => (event.uid !== uid));
+
+            return { ...state, enrollment: { ...state.enrollment, events } };
         },
         [COMMIT_ENROLLMENT_EVENT_WITHOUT_ID]: (state, { payload: { eventId, uid } }) => {
-            const events = state.events?.map((event) => {
+            const events = state.enrollment.events?.map((event) => {
                 if (event.uid === uid) {
                     const { pendingApiResponse, uid: uidToRemove, ...dataToCommit } = event;
                     return { ...dataToCommit, event: eventId };
                 }
                 return event;
             });
-            return { ...state, events };
+            return { ...state, enrollment: { ...state.enrollment, events } };
         },
         [enrollmentNoteActionTypes.ADD_ENROLLMENT_NOTE]:
         (state, { payload: { note } }) => ({
