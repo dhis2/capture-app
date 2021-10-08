66d650e45733275ca2d81efbc969f2ea449acbe4
EirikHaugstulen <eirik.haugstulen@gmail.com>
2021-10-08 21:52:56 +0200

    feat: added tooltip to disabled stage-button

--- src/core_modules/capture-core/components/WidgetProgramStageSelector/WidgetProgramStageSelector.component.js
+++ src/core_modules/capture-core/components/WidgetProgramStageSelector/WidgetProgramStageSelector.component.js
@@ -1,7 +1,8 @@
 // @flow
 import React from 'react';
+import i18n from '@dhis2/d2-i18n';
 import { Button, spacers, spacersNum } from '@dhis2/ui';
-import { withStyles } from '@material-ui/core';
+import { withStyles, Tooltip } from '@material-ui/core';
 import { NonBundledDhis2Icon } from '../NonBundledDhis2Icon';
 
 const styles = {
@@ -18,43 +19,61 @@ const styles = {
     buttonText: {
         paddingLeft: spacersNum.dp8,
     },
+    buttonContentContainer: {
+        display: 'flex',
+        alignItems: 'center',
+        gap: '5px',
+    },
     cancelbutton: {
         alignSelf: 'start',
-        marginTop: '24px',
+        marginTop: spacersNum.dp24,
     },
 };
 
 const WidgetProgramStageSelectorComponentPlain = ({ programStages, onProgramStageUpdate, onCancel, classes }) => (
     <div className={classes.container}>
-        {programStages.map(programStage => (
-            <div
-                key={programStage.id}
-            >
-                <Button
-                    className={classes.button}
-                    big
-                    secondary
-                    disabled={!programStage.repeatable && programStage.eventCount > 0}
-                    onClick={() => onProgramStageUpdate(programStage.id)}
+        {programStages.map((programStage) => {
+            const disableStage = !programStage.repeatable && programStage.eventCount > 0;
+            return (
+                <div
+                    key={programStage.id}
                 >
-                    <NonBundledDhis2Icon
-                        width={22}
-                        height={22}
-                        name={programStage.icon?.name || 'clinical_fe_outline'}
-                        color={programStage.icon?.color || '#e0e0e0'}
-                        alternativeText={programStage.name}
-                        cornerRadius={2}
-                    />
-                    <p className={classes.buttonText}>{programStage.name}</p>
-                </Button>
-            </div>
-        ))}
+                    <Button
+                        className={classes.button}
+                        big
+                        secondary
+                        disabled={disableStage}
+                        onClick={() => onProgramStageUpdate(programStage.id)}
+                    >
+                        <Tooltip
+                            title={disableStage ? i18n.t('Stage is non-repeatable') : ''}
+                        >
+                            <div
+                                className={classes.buttonContentContainer}
+                            >
+                                <NonBundledDhis2Icon
+                                    width={22}
+                                    height={22}
+                                    name={programStage.icon?.name || 'clinical_fe_outline'}
+                                    color={programStage.icon?.color || '#e0e0e0'}
+                                    alternativeText={programStage.name}
+                                    cornerRadius={2}
+                                />
+                                {programStage.name}
+                            </div>
+                        </Tooltip>
+                    </Button>
+                </div>
+            );
+        })}
         <Button
             className={classes.cancelbutton}
             secondary
             big
             onClick={onCancel}
-        >Cancel without saving</Button>
+        >
+            {i18n.t('Cancel without saving')}
+        </Button>
     </div>
 );
 
