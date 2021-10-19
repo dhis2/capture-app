6fac0e40ab03284151c2e3b76f579301dbd9f0d9
EirikHaugstulen <eirik.haugstulen@gmail.com>
2021-10-19 11:34:30 +0200

    chore: initial push on adding complete and active events

---
+++ cypress/integration/EnrollmentAddEventPage/EnrollmentAddEventPageForm/index.js
@@ -0,0 +1,87 @@
+
+beforeEach(() => {
+    cy.loginThroughForm();
+});
+
+const showAllEventsInProgramStage = () => {
+    cy.get('[data-test="dhis2-uicore-tablefoot"]')
+        .then(($footer) => {
+            if ($footer.find('[data-test="show-more-button"]').length > 0) {
+                $footer.find('[data-test="show-more-button"]')
+                    .click();
+                showAllEventsInProgramStage();
+            }
+        });
+};
+
+Given(/^you land on the enrollment new event page by having typed (.*)$/, (url) => {
+    cy.visit(url);
+});
+
+When(/^you click the create new button number (.*)$/, (eq) => {
+    cy.get('[data-test="create-new-button"]')
+        .eq(eq)
+        .click();
+});
+
+When(/^you type (.*) in the input number (.*)$/, (value, eq) => {
+    cy.get('[data-test="capture-ui-input"]')
+        .eq(eq)
+        .type(value)
+        .blur();
+});
+
+When(/^you click the checkbox number (.*)$/, (eq) => {
+    cy.get('input[type="radio"]')
+        .eq(eq)
+        .click();
+});
+
+When(/^you click the (.*) button/, (buttonText) => {
+    cy.intercept({
+        method: 'POST',
+        url: '**/events',
+    }).as('postEvents');
+
+    cy.get('[data-test="dhis2-uicore-button"]')
+        .contains(buttonText)
+        .click();
+
+    cy.wait('@postEvents');
+});
+
+When('the enrollment overview is finished loading', () => {
+    cy.get('[data-test="dhis2-uicore-circularloader"]').should('not.exist');
+    cy.get('[data-test="dhis2-uicore-datatable"]')
+        .eq(1)
+        .within(() => {
+            cy.get('[data-test="dhis2-uicore-datatablerow"]')
+                .should('exist');
+        });
+});
+
+
+Then('all events should be displayed', () => {
+    showAllEventsInProgramStage();
+});
+
+Then(/^the newest event in datatable nr (.*) should contain (.*)$/, (eq, status) => {
+    cy.get('[data-test="dhis2-uicore-datatable"]')
+        .eq(eq)
+        .within(() => {
+            cy.get('[data-test="dhis2-uicore-datatablerow"]')
+                .filter(':contains("2021-10-15")')
+                .last()
+                .within(() => {
+                    cy.contains(status);
+                    cy.contains('Ngelehun CHC');
+                });
+        });
+});
+
+When(/^the user selects (.*)$/, (value) => {
+    cy.get('.Select')
+        .type(value.slice(0, -1));
+    cy.contains(value)
+        .click();
+});
