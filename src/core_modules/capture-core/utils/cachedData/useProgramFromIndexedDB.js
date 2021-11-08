5bc7d05a030af7f02d81290bc09131d74f154fd1
EirikHaugstulen <eirik.haugstulen@gmail.com>
2021-11-08 11:12:20 +0100

    chore: typo

--- src/core_modules/capture-core/utils/cachedData/useProgramFromIndexedDB.js
+++ src/core_modules/capture-core/utils/cachedData/useProgramFromIndexedDB.js
@@ -19,6 +19,7 @@ export const useProgramFromIndexedDB = (programId: string) => {
             })
             .catch((e) => {
                 setError(e);
+                setLoading(false);
             });
     }, [programId]);
 
