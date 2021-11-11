cc7e6d11181bc6061ead5db42346ae9e8ede487e
EirikHaugstulen <eirik.haugstulen@gmail.com>
2021-11-11 23:30:26 +0100

    fix: added sorting to buildUrlQueryString

--- src/core_modules/capture-core/utils/routing/buildUrlQueryString.js
+++ src/core_modules/capture-core/utils/routing/buildUrlQueryString.js
@@ -1,8 +1,28 @@
 // @flow
+const idTypes = Object.freeze({
+    programId: 'programId',
+    orgUnitId: 'orgUnitId',
+    trackedEntityTypeId: 'trackedEntityTypeId',
+    teiId: 'teiId',
+    enrollmentId: 'enrollmentId',
+    stageId: 'stageId',
+    eventId: 'eventId',
+});
+
+const urlTemplate = [
+    idTypes.programId,
+    idTypes.orgUnitId,
+    idTypes.trackedEntityTypeId,
+    idTypes.teiId,
+    idTypes.enrollmentId,
+    idTypes.stageId,
+    idTypes.eventId,
+];
 
 export const buildUrlQueryString = (queryArgs: { [id: string]: ?string }) =>
     Object
         .entries(queryArgs)
+        .sort((a, b) => urlTemplate.indexOf(a[0]) - urlTemplate.indexOf(b[0]))
         .reduce((searchParams, [key, value]) => {
             // $FlowFixMe
             value && searchParams.append(key, value);
