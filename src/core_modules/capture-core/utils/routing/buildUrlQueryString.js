8fe14f98204f54e46b268ca590ff35b562c0ea17
EirikHaugstulen <eirik.haugstulen@gmail.com>
2021-11-16 12:47:59 +0100

    chore: sort alphabetically

--- src/core_modules/capture-core/utils/routing/buildUrlQueryString.js
+++ src/core_modules/capture-core/utils/routing/buildUrlQueryString.js
@@ -1,32 +1,9 @@
 // @flow
-const idTypes = Object.freeze({
-    programId: 'programId',
-    orgUnitId: 'orgUnitId',
-    trackedEntityTypeId: 'trackedEntityTypeId',
-    teiId: 'teiId',
-    enrollmentId: 'enrollmentId',
-    stageId: 'stageId',
-    eventId: 'eventId',
-});
-
-const urlTemplate = [
-    idTypes.programId,
-    idTypes.orgUnitId,
-    idTypes.trackedEntityTypeId,
-    idTypes.teiId,
-    idTypes.enrollmentId,
-    idTypes.stageId,
-    idTypes.eventId,
-];
 
 export const buildUrlQueryString = (queryArgs: { [id: string]: ?string }) =>
     Object
         .entries(queryArgs)
-        .sort((a, b) => {
-            if (b && (urlTemplate.indexOf(b[0]) === -1)) return -1;
-            if (a && (urlTemplate.indexOf(a[0]) === -1)) return 1;
-            return urlTemplate.indexOf(a[0]) - urlTemplate.indexOf(b[0]);
-        })
+        .sort((a, b) => a[0].localeCompare(b[0]))
         .reduce((searchParams, [key, value]) => {
             // $FlowFixMe
             value && searchParams.append(key, value);
