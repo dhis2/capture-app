df812915aa6c5d5bc7449284c6ddedd491229772
jasminenguyennn <jasmine@dhis2.org>
2021-11-16 07:23:51 +0000

    chore: [DHIS2-10267] minor fix

--- src/components/AppStart/JSSProviderShell.component.js
+++ src/components/AppStart/JSSProviderShell.component.js
@@ -13,6 +13,7 @@ insertionPoint.setAttribute('id', 'jss-insertion-point');
 document.head.appendChild(insertionPoint);
 const generateClassName = createGenerateClassName();
 const jss = create(jssPreset());
+// $FlowFixMe
 jss.options.insertionPoint = insertionPoint;
 
 type Props = {
