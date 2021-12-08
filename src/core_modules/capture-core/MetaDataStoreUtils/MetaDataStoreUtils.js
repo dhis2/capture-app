b48539ccec0ad7a8bf3657330942cdc22f087473
Simona Domnisoru <simona.domnisoru@gmail.com>
2021-12-08 08:33:17 +0100

    refactor: showcase alter promise

--- src/core_modules/capture-core/MetaDataStoreUtils/MetaDataStoreUtils.js
+++ src/core_modules/capture-core/MetaDataStoreUtils/MetaDataStoreUtils.js
@@ -2,12 +2,16 @@
 import { typeof userStores } from '../storageControllers/stores';
 import { getUserStorageController } from '../storageControllers';
 
-export const getCachedSingleResourceFromKeyAsync = (store: $Values<userStores>, key: string) => {
+export const getCachedSingleResourceFromKeyAsync = (
+    store: $Values<userStores>,
+    key: string,
+    propsToPass?: any = {},
+) => {
     const storageController = getUserStorageController();
-    return storageController.get(store, key);
+    return storageController.get(store, key).then(response => ({ response, ...propsToPass }));
 };
 
-export const containsKeyInStorageAsync = (store: $Values<userStores>, key: string) => {
+export const containsKeyInStorageAsync = (store: $Values<userStores>, key: string, propsToPass?: any = {}) => {
     const storageController = getUserStorageController();
-    return storageController.contains(store, key);
+    return storageController.contains(store, key).then(response => ({ response, ...propsToPass }));
 };
