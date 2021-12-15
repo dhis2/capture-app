b5e38915f7de4f693af0aad9abe52fbb829a92e0
Joakim Storløkken Melseth <joakim.melseth@gmail.com>
2021-12-15 14:49:10 +0100

    fix: PR comment

--- src/core_modules/capture-core/rules/converters/dateUtils.js
+++ src/core_modules/capture-core/rules/converters/dateUtils.js
@@ -47,7 +47,6 @@ class DateUtils implements IDateUtils {
         return `'${newRulesDate}'`;
     }
     compareDates(firstRulesDate: string, secondRulesDate: string): number {
-        secondRulesDate = moment().toISOString();
         const diff = dateUtils.daysBetween(secondRulesDate, firstRulesDate);
         if (diff < 0) {
             return -1;
