ed5266cca921fb02ea840c662332532ed8ad2970
jasminenguyennn <jasmine@dhis2.org>
2021-12-24 09:29:04 +0000

    fix: [DHIS2-12142] fix flow

--- src/core_modules/capture-core/components/WidgetProfile/hooks.js
+++ src/core_modules/capture-core/components/WidgetProfile/hooks.js
@@ -3,6 +3,7 @@ import React, { useState, useCallback, useEffect } from 'react';
 import { useDataEngine } from '@dhis2/app-runtime';
 import { makeQuerySingleResource } from 'capture-core/utils/api';
 import { useSelector } from 'react-redux';
+import type { apiProgramTrackedEntityAttribute } from 'capture-core/metaDataStoreLoaders/programs/quickStoreOperations/types';
 import { convertValue as convertServerToClient } from '../../converters/serverToClient';
 import { subValueGetterByElementType } from './getSubValueForTei';
 
@@ -11,7 +12,29 @@ export const useProfileInfo = () => {
     return { attributes: profileInformation };
 };
 
-export const useListAttributes = (programsData, trackedEntityInstancesData) => {
+type InputProgramData = {
+    programs: {
+        programTrackedEntityAttributes: Array<apiProgramTrackedEntityAttribute>
+    }
+}
+
+type InputAttribute = {
+    attribute: string,
+    code: string,
+    created: string,
+    displayName: string,
+    lastUpdated: string,
+    value: string,
+    valueType: string
+}
+
+type InputTEIData = {
+    trackedEntityInstances: {
+        attributes: Array<InputAttribute>
+    }
+}
+
+export const useListAttributes = (programsData: InputProgramData, trackedEntityInstancesData: InputTEIData) => {
     const dataEngine = useDataEngine();
 
     const [listAttributes, setListAttributes] = useState([]);
@@ -34,6 +57,7 @@ export const useListAttributes = (programsData, trackedEntityInstancesData) => {
                                 foundAttribute.value, querySingleResource,
                             );
                         } else {
+                            // $FlowFixMe dataElementTypes flow error
                             value = convertServerToClient(foundAttribute.value, foundAttribute.valueType);
                         }
                     }
