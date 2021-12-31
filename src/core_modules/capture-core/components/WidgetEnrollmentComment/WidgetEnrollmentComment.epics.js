39455b62fb476fe381c151fd3001ffa20f27a472
EirikHaugstulen <eirik.haugstulen@gmail.com>
2021-12-31 15:41:58 +0100

    fix: update comments widget

--- src/core_modules/capture-core/components/WidgetEnrollmentComment/WidgetEnrollmentComment.epics.js
+++ src/core_modules/capture-core/components/WidgetEnrollmentComment/WidgetEnrollmentComment.epics.js
@@ -18,9 +18,11 @@ export const addNoteForEnrollmentEpic = (action$: InputObservable, store: ReduxS
             const { firstName, surname, userName } = getCurrentUser();
             const clientId = uuid();
 
+            // Setting events to empty array temporary - causing import error
             const serverData = {
                 enrollments: [{
                     ...state.enrollmentDomain.enrollment,
+                    events: [],
                     notes: [{ value: note }],
                 }],
             };
