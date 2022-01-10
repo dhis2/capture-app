1d706b8ad7faeb1121b279fc3682fee47dbe36dc
jasminenguyennn <jasmine@dhis2.org>
2022-01-10 06:53:05 +0000

    chore: [TECH-893] add orgunit from redux

--- src/core_modules/capture-core/dataQueries/useOrganisationUnit.js
+++ src/core_modules/capture-core/dataQueries/useOrganisationUnit.js
@@ -1,11 +1,14 @@
 // @flow
 import { useMemo, useEffect, useState } from 'react';
 import { useDataQuery } from '@dhis2/app-runtime';
+import { useSelector } from 'react-redux';
 import log from 'loglevel';
 import { errorCreator } from '../../capture-core-utils';
 
+
 export const useOrganisationUnit = (orgUnitId: string, fields: string) => {
     const [orgUnit, setOrgUnit] = useState();
+    const cachedOrganisationUnits = useSelector(({ organisationUnits }) => organisationUnits);
     const { error, loading, data, refetch, called } = useDataQuery(
         useMemo(
             () => ({
@@ -24,8 +27,10 @@ export const useOrganisationUnit = (orgUnitId: string, fields: string) => {
     );
 
     useEffect(() => {
-        refetch({ variables: { orgUnitId } });
-    }, [refetch, orgUnitId]);
+        if (!cachedOrganisationUnits || !cachedOrganisationUnits[orgUnitId]) {
+            refetch({ variables: { orgUnitId } });
+        }
+    }, [refetch, orgUnitId, cachedOrganisationUnits]);
 
     useEffect(() => {
         if (error) {
@@ -34,16 +39,24 @@ export const useOrganisationUnit = (orgUnitId: string, fields: string) => {
     }, [error]);
 
     useEffect(() => {
-        setOrgUnit(
-            (loading || !called || error) ?
-                undefined : {
-                    id: orgUnitId,
-                    name: data?.organisationUnits?.displayName,
-                    code: data?.organisationUnits?.code,
-                    ...data,
-                },
-        );
-    }, [orgUnitId, data, loading, called, error]);
+        if (cachedOrganisationUnits && cachedOrganisationUnits[orgUnitId]) {
+            setOrgUnit({
+                id: orgUnitId,
+                name: cachedOrganisationUnits[orgUnitId].name,
+                code: cachedOrganisationUnits[orgUnitId].code,
+            });
+        } else {
+            setOrgUnit(
+                (loading || !called || error) ?
+                    undefined : {
+                        id: orgUnitId,
+                        name: data?.organisationUnits?.displayName,
+                        code: data?.organisationUnits?.code,
+                        ...data?.organisationUnits,
+                    },
+            );
+        }
+    }, [orgUnitId, data, loading, called, error, cachedOrganisationUnits]);
 
     return {
         error,
