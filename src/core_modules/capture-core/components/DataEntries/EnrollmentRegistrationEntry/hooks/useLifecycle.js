ae09775fa3cc3d94196232f8a78f4861631b99fc
Tony Valle <tony@devotta.no>
2022-03-04 11:31:38 +0100

    refactor: EnrollmentRegistrationEntry

--- src/core_modules/capture-core/components/DataEntries/EnrollmentRegistrationEntry/hooks/useLifecycle.js
+++ src/core_modules/capture-core/components/DataEntries/EnrollmentRegistrationEntry/hooks/useLifecycle.js
@@ -1,12 +1,12 @@
 // @flow
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useRef } from 'react';
-import { useOrganisationUnit } from 'capture-core/dataQueries/useOrganisationUnit';
 import { startNewEnrollmentDataEntryInitialisation } from '../EnrollmentRegistrationEntry.actions';
 import { scopeTypes, getProgramThrowIfNotFound } from '../../../../metaData';
 import { useLocationQuery } from '../../../../utils/routing';
 import { useScopeInfo } from '../../../../hooks/useScopeInfo';
 import { useCurrentOrgUnitInfo } from '../../../../hooks/useCurrentOrgUnitInfo';
+import { useRulesEngineOrgUnit } from '../../../../hooks/useRulesEngineOrgUnit';
 import { useRegistrationFormInfoForSelectedScope } from '../../common/useRegistrationFormInfoForSelectedScope';
 import { useFormValues, useTrackedEntityInstances } from './index';
 
@@ -14,11 +14,10 @@ export const useLifecycle = (selectedScopeId: string, dataEntryId: string) => {
     const { teiId, programId } = useLocationQuery();
     const dataEntryReadyRef = useRef(false);
     const dispatch = useDispatch();
-    const ready = useSelector(({ dataEntries }) => !!dataEntries[dataEntryId]);
     const program = getProgramThrowIfNotFound(programId);
     const orgUnitId = useCurrentOrgUnitInfo()?.id;
-    // https://jira.dhis2.org/browse/DHIS2-12387 some cases the orgUnit code is missing in the Redux store. Get it from the API for now.
-    const orgUnit = useOrganisationUnit(orgUnitId)?.orgUnit;
+    const orgUnit = useRulesEngineOrgUnit(orgUnitId);
+    const ready = useSelector(({ dataEntries }) => !!dataEntries[dataEntryId]) && !!orgUnit;
     const { scopeType } = useScopeInfo(selectedScopeId);
     const { formFoundation } = useRegistrationFormInfoForSelectedScope(selectedScopeId);
     const { trackedEntityInstanceAttributes } = useTrackedEntityInstances(teiId, programId);
@@ -31,11 +30,11 @@ export const useLifecycle = (selectedScopeId: string, dataEntryId: string) => {
     });
 
     useEffect(() => {
-        if (dataEntryReadyRef.current === false && formValuesReadyRef.current === true && scopeType === scopeTypes.TRACKER_PROGRAM) {
+        if (dataEntryReadyRef.current === false && formValuesReadyRef.current === true && orgUnit && scopeType === scopeTypes.TRACKER_PROGRAM) {
             dataEntryReadyRef.current = true;
             dispatch(
                 startNewEnrollmentDataEntryInitialisation({
-                    selectedOrgUnitId: orgUnit?.id,
+                    selectedOrgUnit: orgUnit,
                     selectedScopeId,
                     dataEntryId,
                     formValues,
