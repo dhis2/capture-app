21ba9b0895ac3eb9976cb41688b8a4090581b108
Simona Domnisoru <simona.domnisoru@gmail.com>
2022-03-11 16:25:05 +0100

    fix: report date label (#2616)

--- src/core_modules/capture-core/metaDataMemoryStoreBuilders/programs/factory/programStage/ProgramStageFactory.js
+++ src/core_modules/capture-core/metaDataMemoryStoreBuilders/programs/factory/programStage/ProgramStageFactory.js
@@ -131,7 +131,7 @@ export class ProgramStageFactory {
                 _form.description = cachedProgramStage.description;
                 _form.featureType = ProgramStageFactory._getFeatureType(cachedProgramStage);
                 _form.access = cachedProgramStage.access;
-                _form.addLabel({ id: 'eventDate', label: cachedProgramStage.executionDateLabel || 'Incident date' });
+                _form.addLabel({ id: 'eventDate', label: cachedProgramStage.executionDateLabel || 'Report date' });
                 _form.validationStrategy =
                     cachedProgramStage.validationStrategy &&
                     camelCaseUppercaseString(cachedProgramStage.validationStrategy);
