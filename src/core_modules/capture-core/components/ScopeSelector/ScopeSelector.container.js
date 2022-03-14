1e8a79c0b4a83a9d9c249d88f50bf4850ebc0c3b
Simona Domnisoru <simona.domnisoru@gmail.com>
2022-03-14 16:45:08 +0100

    chore: clean the ScopeSelector

--- src/core_modules/capture-core/components/ScopeSelector/ScopeSelector.container.js
+++ src/core_modules/capture-core/components/ScopeSelector/ScopeSelector.container.js
@@ -1,11 +1,9 @@
 // @flow
-import React, { type ComponentType, useEffect, useCallback, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { type ComponentType, useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { ScopeSelectorComponent } from './ScopeSelector.component';
-import { resetProgramIdBatchAction } from './ScopeSelector.actions';
 import type { OwnProps } from './ScopeSelector.types';
 import { useOrganizationUnit } from './hooks';
-
 
 const deriveReadiness = (lockedSelectorLoads, selectedOrgUnitId, selectedOrgUnitName) => {
     // because we want the orgUnit to be fetched and stored
@@ -16,78 +14,61 @@ const deriveReadiness = (lockedSelectorLoads, selectedOrgUnitId, selectedOrgUnit
     return !lockedSelectorLoads;
 };
 
-export const ScopeSelector: ComponentType<OwnProps> =
-  ({
-      isUserInteractionInProgress = false,
-      selectedProgramId,
-      selectedOrgUnitId,
-      selectedCategories,
-      onSetProgramId,
-      onSetOrgUnit,
-      onSetCategoryOption,
-      onResetProgramId,
-      onResetOrgUnitId,
-      onResetCategoryOption,
-      onResetAllCategoryOptions,
-      children,
-  }) => {
-      const dispatch = useDispatch();
-      const { refetch: refetchOrganisationUnit, displayName } = useOrganizationUnit();
-      const [selectedOrgUnit, setSelectedOrgUnit] = useState({ name: displayName, id: selectedOrgUnitId });
+export const ScopeSelector: ComponentType<OwnProps> = ({
+    isUserInteractionInProgress = false,
+    selectedProgramId,
+    selectedOrgUnitId,
+    selectedCategories,
+    onSetProgramId,
+    onSetOrgUnit,
+    onSetCategoryOption,
+    onResetProgramId,
+    onResetOrgUnitId,
+    onResetCategoryOption,
+    onResetAllCategoryOptions,
+    children,
+}) => {
+    const { refetch: refetchOrganisationUnit, displayName } = useOrganizationUnit();
+    const [selectedOrgUnit, setSelectedOrgUnit] = useState({ name: displayName, id: selectedOrgUnitId });
 
-      useEffect(() => {
-          const missName = !selectedOrgUnit.name;
-          const hasDifferentId = selectedOrgUnit.id !== selectedOrgUnitId;
+    useEffect(() => {
+        const missName = !selectedOrgUnit.name;
+        const hasDifferentId = selectedOrgUnit.id !== selectedOrgUnitId;
 
-          selectedOrgUnitId && (hasDifferentId || missName) && refetchOrganisationUnit({ variables: { selectedOrgUnitId } });
-      },
-      [selectedOrgUnitId]); // eslint-disable-line react-hooks/exhaustive-deps
+        selectedOrgUnitId &&
+            (hasDifferentId || missName) &&
+            refetchOrganisationUnit({ variables: { selectedOrgUnitId } });
+    }, [selectedOrgUnitId]); // eslint-disable-line react-hooks/exhaustive-deps
 
-      useEffect(() => {
-          displayName && setSelectedOrgUnit(prevSelectedOrgUnit => ({ ...prevSelectedOrgUnit, name: displayName }));
-      },
-      [displayName, setSelectedOrgUnit]);
+    useEffect(() => {
+        displayName && setSelectedOrgUnit(prevSelectedOrgUnit => ({ ...prevSelectedOrgUnit, name: displayName }));
+    }, [displayName, setSelectedOrgUnit]);
 
-      const handleSetOrgUnit = (orgUnitId, orgUnitObject) => {
-          setSelectedOrgUnit(orgUnitObject);
-          onSetOrgUnit(orgUnitId);
-      };
+    const handleSetOrgUnit = (orgUnitId, orgUnitObject) => {
+        setSelectedOrgUnit(orgUnitObject);
+        onSetOrgUnit(orgUnitId);
+    };
 
-      const dispatchOnResetOrgUnitId = useCallback(
-          () => {
-              onResetOrgUnitId();
-          },
-          [onResetOrgUnitId]);
+    const lockedSelectorLoads: string = useSelector(({ activePage }) => activePage.lockedSelectorLoads);
+    const ready = deriveReadiness(lockedSelectorLoads, selectedOrgUnitId, selectedOrgUnit.name);
 
-      const dispatchOnResetProgramId = useCallback(
-          (baseAction: ReduxAction<any, any>) => {
-              dispatch(resetProgramIdBatchAction([baseAction]));
-              onResetProgramId();
-          },
-          [dispatch, onResetProgramId]);
-
-      const lockedSelectorLoads: string =
-        useSelector(({ activePage }) => activePage.lockedSelectorLoads);
-
-      const ready = deriveReadiness(lockedSelectorLoads, selectedOrgUnitId, selectedOrgUnit.name);
-
-      return (
-          <ScopeSelectorComponent
-              onResetProgramId={dispatchOnResetProgramId}
-              onResetOrgUnitId={dispatchOnResetOrgUnitId}
-              onResetAllCategoryOptions={onResetAllCategoryOptions}
-              onResetCategoryOption={onResetCategoryOption}
-              onSetCategoryOption={onSetCategoryOption}
-              onSetProgramId={onSetProgramId}
-              onSetOrgUnit={handleSetOrgUnit}
-              selectedOrgUnit={selectedOrgUnit}
-              selectedOrgUnitId={selectedOrgUnitId}
-              selectedProgramId={selectedProgramId}
-              selectedCategories={selectedCategories}
-              isUserInteractionInProgress={isUserInteractionInProgress}
-              ready={ready}
-          >
-              {children}
-          </ScopeSelectorComponent>
-      );
-  };
+    return (
+        <ScopeSelectorComponent
+            onResetProgramId={onResetProgramId}
+            onResetOrgUnitId={onResetOrgUnitId}
+            onResetAllCategoryOptions={onResetAllCategoryOptions}
+            onResetCategoryOption={onResetCategoryOption}
+            onSetCategoryOption={onSetCategoryOption}
+            onSetProgramId={onSetProgramId}
+            onSetOrgUnit={handleSetOrgUnit}
+            selectedOrgUnit={selectedOrgUnit}
+            selectedOrgUnitId={selectedOrgUnitId}
+            selectedProgramId={selectedProgramId}
+            selectedCategories={selectedCategories}
+            isUserInteractionInProgress={isUserInteractionInProgress}
+            ready={ready}
+        >
+            {children}
+        </ScopeSelectorComponent>
+    );
+};
