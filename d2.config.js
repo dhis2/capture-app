8c14f061066f803264ce9d582c3051907795d9c6
Viktor Varland <viktor@dhis2.org>
2022-03-16 10:58:48 +0000

    chore(release): migrate to new app version scheme (#2566)
    Align all our application versions that range between 0.x.x and 36.x.x
    with different semantics to a single version scheme with distinct
    semantics for what constitutes a breaking change.
    
    We have chosen to bump to v100.0.0 to signify the depature from the old
    version scheme to the new.
    
    For more information:
    dhis2/notes#293
    
    BREAKING CHANGE: App version becomes decoupled from DHIS2 versions, see
    the d2.config.js or App Hub for DHIS2 version compatibility.

--- d2.config.js
+++ d2.config.js
@@ -2,6 +2,9 @@ const config = {
     name: 'capture',
     title: 'Capture',
     type: 'app',
+
+    id: '92b75fd0-34cc-451c-942f-3dd0f283bcbd',
+    minDHIS2Version: '2.38',
     coreApp: true,
 
     entryPoints: {
