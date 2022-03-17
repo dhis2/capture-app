fb016c08678fe1f79edfab232fc003e5a6ae77a2
Tony Valle <tony@devotta.no>
2022-03-17 10:17:48 +0100

    refactor: delete file getRulesEngineOrgUnit.js

--- src/core_modules/capture-core/hooks/useOrgUnitGroups.js
+++ src/core_modules/capture-core/hooks/useOrgUnitGroups.js
@@ -1,13 +1,26 @@
 // @flow
 import { useState, useEffect, useRef } from 'react';
+import { getUserStorageController } from 'capture-core/storageControllers';
 import type { OrgUnitGroup } from 'capture-core-utils/rulesEngine';
-import { getAssociatedOrgUnitGroups } from '../rules/getRulesEngineOrgUnit';
 
 type Request = {
     orgUnitId: ?string,
     requestId: number,
     fetching: boolean,
 };
+
+async function getAssociatedOrgUnitGroups(orgUnitId: string): any {
+    const storageController = getUserStorageController();
+    const orgUnitGroups = await storageController.getAll(
+        'organisationUnitGroups', {
+            project: item => ({ id: item.id, code: item.code }),
+            onIDBGetRequest: source => source
+                .index('organisationUnitId')
+                .openCursor(window.IDBKeyRange.only(orgUnitId)),
+        },
+    );
+    return orgUnitGroups;
+}
 
 export function useOrgUnitGroups(orgUnitId: ?string): ?Array<OrgUnitGroup> {
     const lastRequest = useRef<Request>({ orgUnitId: undefined, requestId: 0, fetching: false });
