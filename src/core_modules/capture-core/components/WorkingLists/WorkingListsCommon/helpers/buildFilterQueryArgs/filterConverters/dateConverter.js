ecd676fd7b23fcd478f29f5ad5630c4eed967d30
Simona Domnisoru <simona.domnisoru@gmail.com>
2022-03-22 13:37:48 +0100

    fix: [DHIS2-11796] adapt the date shape to the API requirements (#2662)

--- src/core_modules/capture-core/components/WorkingLists/WorkingListsCommon/helpers/buildFilterQueryArgs/filterConverters/dateConverter.js
+++ src/core_modules/capture-core/components/WorkingLists/WorkingListsCommon/helpers/buildFilterQueryArgs/filterConverters/dateConverter.js
@@ -112,7 +112,8 @@ function convertRelativeDate(
     storeId: string,
     isInit: boolean,
 ) {
-    return getSelector(key, storeId, isInit)(sourceValue);
+    const requestData = getSelector(key, storeId, isInit)(sourceValue);
+    return requestData.join(':');
 }
 
 function convertAbsoluteDate(sourceValue: AbsoluteDateFilterData) {
@@ -126,7 +127,7 @@ function convertAbsoluteDate(sourceValue: AbsoluteDateFilterData) {
         const toFilterRequest = getFormattedStringFromMomentUsingEuropeanGlyphs(moment(sourceValue.le));
         requestData.push(`le:${toFilterRequest}`);
     }
-    return requestData;
+    return requestData.join(':');
 }
 
 export function convertDate(
