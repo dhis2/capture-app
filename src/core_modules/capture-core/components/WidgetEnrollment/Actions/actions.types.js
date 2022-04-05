b5d227c1c917ff61e9d85c35d74602e1dbe74ef1
JasmineNg <89806888+jasminenguyennn@users.noreply.github.com>
2022-04-05 12:38:43 +0100

    fix: [DHIS2-12880] refetch tei after updating (#2661) (#2710)
    * fix: [DHIS2-12880] refetch tei after updating
    
    * chore: [DHIS2-12880] rename refetch enrollment

--- src/core_modules/capture-core/components/WidgetEnrollment/Actions/actions.types.js
+++ src/core_modules/capture-core/components/WidgetEnrollment/Actions/actions.types.js
@@ -3,7 +3,8 @@ import type { QueryRefetchFunction } from '@dhis2/app-runtime';
 
 export type Props = {|
     enrollment: Object,
-    refetch: QueryRefetchFunction,
+    refetchEnrollment: QueryRefetchFunction,
+    refetchTEI: QueryRefetchFunction,
     onDelete: () => void,
     onAddNew: () => void,
     canAddNew: boolean,
