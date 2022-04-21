68e01a403cd0ce1d12ca30644f01c9089724969f
Simona Domnisoru <simona.domnisoru@gmail.com>
2022-04-21 13:34:19 +0200

    fix: [DHIS2-12675] custom relative range for date (#2646)

---
+++ src/core_modules/capture-core/components/FiltersForTypes/Date/End.component.js
@@ -0,0 +1,51 @@
+// @flow
+import React, { Component } from 'react';
+import i18n from '@dhis2/d2-i18n';
+import { D2TextField } from '../../FormFields/Generic/D2TextField.component';
+import { withInternalChangeHandler } from '../../FormFields/withInternalChangeHandler';
+
+type Props = {
+    value: ?string,
+    error: ?string,
+    onBlur: ({ end: string }) => void,
+    onEnterKey: ({ end: string }) => void,
+    textFieldRef: (instance: any) => void,
+    errorClass: string,
+};
+
+class EndRangeFilterPlain extends Component<Props> {
+    static getValueObject(value: string) {
+        return { end: value.trim() };
+    }
+
+    handleBlur = (value: string) => {
+        this.props.onBlur(EndRangeFilterPlain.getValueObject(value));
+    };
+
+    handleKeyPress = (event: SyntheticKeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            this.props.onEnterKey(EndRangeFilterPlain.getValueObject(this.props.value || ''));
+        }
+    };
+
+    render() {
+        const { error, onBlur, onEnterKey, textFieldRef, errorClass, ...passOnProps } = this.props;
+        return (
+            <div>
+                {/* $FlowFixMe[cannot-spread-inexact] automated comment */}
+                <D2TextField
+                    ref={textFieldRef}
+                    onKeyPress={this.handleKeyPress}
+                    onBlur={this.handleBlur}
+                    placeholder={i18n.t('Days in the future')}
+                    fullWidth
+                    data-test="date-range-filter-end"
+                    {...passOnProps}
+                />
+                <div className={errorClass}>{error}</div>
+            </div>
+        );
+    }
+}
+
+export const EndRangeFilter = withInternalChangeHandler()(EndRangeFilterPlain);
---
+++ src/core_modules/capture-core/components/FiltersForTypes/Date/Start.component.js
@@ -0,0 +1,49 @@
+// @flow
+import React, { Component } from 'react';
+import i18n from '@dhis2/d2-i18n';
+import { D2TextField } from '../../FormFields/Generic/D2TextField.component';
+import { withInternalChangeHandler } from '../../FormFields/withInternalChangeHandler';
+
+type Props = {
+    value: ?string,
+    error: ?string,
+    onBlur: ({ start: string }) => void,
+    onEnterKey: () => void,
+    errorClass: string,
+};
+
+class StartRangeFilterPlain extends Component<Props> {
+    static getValueObject(value: string) {
+        return { start: value.trim() };
+    }
+
+    handleBlur = (value: string) => {
+        this.props.onBlur(StartRangeFilterPlain.getValueObject(value));
+    };
+
+    handleKeyPress = (event: SyntheticKeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') {
+            this.props.onEnterKey();
+        }
+    };
+
+    render() {
+        const { error, errorClass, onBlur, onEnterKey, ...passOnProps } = this.props;
+        return (
+            <div>
+                {/* $FlowFixMe[cannot-spread-inexact] automated comment */}
+                <D2TextField
+                    onKeyPress={this.handleKeyPress}
+                    onBlur={this.handleBlur}
+                    placeholder={i18n.t('Days in the past')}
+                    fullWidth
+                    data-test="date-range-filter-start"
+                    {...passOnProps}
+                />
+                <div className={errorClass}>{error}</div>
+            </div>
+        );
+    }
+}
+
+export const StartRangeFilter = withInternalChangeHandler()(StartRangeFilterPlain);
