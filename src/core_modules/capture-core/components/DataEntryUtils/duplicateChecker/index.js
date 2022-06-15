899eb27b7912fcb2341b500a53f5c98cb0e87dcf
Simona Domnisoru <simona.domnisoru@gmail.com>
2022-06-15 10:21:55 +0100

    fix: remove duplicate count call (#2852)

--- src/core_modules/capture-core/components/DataEntryUtils/duplicateChecker/index.js
+++
@@ -1,4 +0,0 @@
-// @flow
-export { checkForDuplicateActionTypes, checkForDuplicate, resetCheckForDuplicate } from './checkForDuplicate.actions';
-export { checkForDuplicateEpic } from './checkForDuplicate.epic';
-export { dataEntriesSearchGroupResultsReducerDesc } from './checkForDuplicate.reducerDescription';
