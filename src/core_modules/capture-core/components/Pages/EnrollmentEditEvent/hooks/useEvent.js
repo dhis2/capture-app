301051e424ca1734770647e2d7c8cdd4b0610b86
Simona Domnisoru <simona.domnisoru@gmail.com>
2022-08-04 10:40:32 +0200

    fix: fallback missing trackedentity field in the events api

--- src/core_modules/capture-core/components/Pages/EnrollmentEditEvent/hooks/useEvent.js
+++ src/core_modules/capture-core/components/Pages/EnrollmentEditEvent/hooks/useEvent.js
@@ -1,8 +1,14 @@
 // @flow
-import { useMemo } from 'react';
+import { useMemo, useEffect, useState } from 'react';
 import { useDataQuery } from '@dhis2/app-runtime';
 
 export const useEvent = (eventId: string) => {
+    const [event, setEvent] = useState({
+        program: undefined,
+        programStage: undefined,
+        enrollment: undefined,
+        trackedEntity: undefined,
+    });
     const { data, error, loading } = useDataQuery(
         useMemo(
             () => ({
@@ -18,9 +24,46 @@ export const useEvent = (eventId: string) => {
         ),
     );
 
+    const {
+        data: dataFallback,
+        refetch,
+        called,
+    } = useDataQuery(
+        useMemo(
+            () => ({
+                enrollment: {
+                    resource: 'tracker/enrollments',
+                    id: ({ variables: { enrollment } }) => enrollment,
+                    params: {
+                        fields: ['trackedEntity'],
+                    },
+                },
+            }),
+            [],
+        ),
+        { lazy: true },
+    );
+
+    useEffect(() => {
+        if (data?.event) {
+            setEvent(data.event);
+
+            // fallback for the case when the endpoint tracker/events does not return the trackedEntity field.
+            if (data.event.enrollment && data.event.trackedEntity === undefined && !called) {
+                refetch({ variables: { enrollment: data.event.enrollment } });
+            }
+        }
+    }, [data?.event, refetch, called]);
+
+    useEffect(() => {
+        if (dataFallback?.enrollment?.trackedEntity) {
+            setEvent(e => ({ ...e, trackedEntity: dataFallback?.enrollment?.trackedEntity }));
+        }
+    }, [dataFallback?.enrollment]);
+
     return {
         error,
         loading,
-        event: !loading && data ? data.event : undefined,
+        event: !loading && data ? event : undefined,
     };
 };
