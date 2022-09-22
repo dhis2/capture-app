540aa88fd9cfa6ce8a5833fce0b45e042b3c0a09
Joakim Storløkken Melseth <joakim.melseth@gmail.com>
2022-09-22 19:22:45 +0200

    chore: re-add test

--- cypress/integration/AllAccessibleRecordsPage/index.js
+++ cypress/integration/AllAccessibleRecordsPage/index.js
@@ -1,6 +1,6 @@
 import '../sharedSteps';
 
-When('the user opens the main page', () => {
+Given('the user is on the the main page', () => {
     cy.visit('/#/');
 });
 
@@ -30,4 +30,66 @@ When('the user clicks the show accessible button', () => {
 
 When(/^the user navigates to (.*)$/, (url) => {
     cy.visit(url);
+});
+
+When('the user clicks a row', () => {
+    cy.get('[data-test="main-page-working-list"]')
+        .find('tr')
+        .eq(1)
+        .click();
+});
+
+When('edits and save the form', () => {
+    cy.contains('Edit event')
+        .click();
+
+
+    cy.contains('[data-test="form-field"]', 'WHOMCH Hemoglobin value')
+        .find('input')
+        .clear()
+        .type('99')
+        .blur();
+
+    cy.contains('Save')
+        .click();
+});
+
+When('navigates back to the main page', () => {
+    cy.contains('Show all events')
+        .click();
+});
+
+Then('the working list should be updated', () => {
+    cy.get('button[title="Select columns"]')
+        .click();
+
+    cy.contains('WHOMCH Hemoglobin value')
+        .click();
+
+    cy.contains('Save')
+        .click();
+
+    cy.get('[data-test="main-page-working-list"]')
+        .find('tr')
+        .eq(1)
+        .contains('99');
+
+    // clean up
+    cy.get('[data-test="main-page-working-list"]')
+        .find('tr')
+        .eq(1)
+        .click();
+
+    cy.contains('Edit event')
+        .click();
+
+
+    cy.contains('[data-test="form-field"]', 'WHOMCH Hemoglobin value')
+        .find('input')
+        .clear()
+        .type('9')
+        .blur();
+
+    cy.contains('Save')
+        .click();
 });
