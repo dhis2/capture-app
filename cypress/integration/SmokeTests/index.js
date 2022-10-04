67663e6e8b899078df274ea3d0c892e3a232ea1d
Joakim Storløkken Melseth <joakim.melseth@gmail.com>
2022-10-04 20:52:13 +0200

    chore: [TECH-862] [TECH-1409] fix cypress tests

--- cypress/integration/SmokeTests/index.js
+++ cypress/integration/SmokeTests/index.js
@@ -1,3 +1,13 @@
+Given('you open the capture app with english language', () => {
+    cy.loginThroughForm('dhis2UsernameEnglish', 'dhis2PasswordEnglish');
+    cy.visit('/');
+});
+
+Then('you should see the app main selections', () => {
+    cy.get('[data-test="org-unit-selector-container"]');
+    cy.get('[data-test="program-selector-container"]');
+});
+
 Given('you open the App without auth cookie', () => {
     cy.visit('/');
 });
