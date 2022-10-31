0e97f8d6d92c4e1fae3fa66a40e0d763746a8480
jasminenguyennn <jasmine@dhis2.org>
2022-10-31 08:10:30 +0000

    fix: [DHIS2-13941] adjust notice box for schedule events

--- src/core_modules/capture-core/components/WidgetEventSchedule/InfoBox/InfoBox.component.js
+++ src/core_modules/capture-core/components/WidgetEventSchedule/InfoBox/InfoBox.component.js
@@ -2,30 +2,14 @@
 import React, { type ComponentType } from 'react';
 import i18n from '@dhis2/d2-i18n';
 import { withStyles } from '@material-ui/core';
-import { IconInfo24, spacersNum, colors } from '@dhis2/ui';
+import { NoticeBox, spacersNum } from '@dhis2/ui';
 import moment from 'moment';
 import type { Props } from './infoBox.types';
 
 const styles = {
     infoBox: {
-        maxWidth: '41.25rem',
-        display: 'flex',
-        borderRadius: 3,
-        border: `1px solid ${colors.blue200}`,
-        backgroundColor: colors.blue050,
         marginTop: spacersNum.dp16,
         padding: spacersNum.dp16,
-    },
-    textBox: {
-        paddingLeft: spacersNum.dp16,
-        color: colors.grey900,
-    },
-    textBold: {
-        fontWeight: 500,
-        paddingBottom: spacersNum.dp16,
-    },
-    textLine: {
-        lineHeight: '20px',
     },
 };
 
@@ -34,40 +18,32 @@ const InfoBoxPlain = ({ scheduleDate, suggestedScheduleDate, eventCountInOrgUnit
     const differenceScheduleDateAndSuggestedDate = moment(scheduleDate).diff(moment(suggestedScheduleDate), 'days');
 
     return (
-        <div className={classes.infoBox}>
-            <div className={classes.icon}>
-                <IconInfo24 />
+        <NoticeBox className={classes.infoBox} title={i18n.t('Schedule date info')}>
+            <div>{scheduleDate === suggestedScheduleDate ?
+                i18n.t('The scheduled date matches the suggested date, but can be changed if needed.')
+                :
+                i18n.t(
+                    'The scheduled date is {{count}} days {{position}} the suggested date.',
+                    {
+                        position: differenceScheduleDateAndSuggestedDate > 0 ? i18n.t('after') : i18n.t('before'),
+                        count: Math.abs(differenceScheduleDateAndSuggestedDate),
+                        defaultValue: 'The scheduled date is {{count}} day {{position}} the suggested date.',
+                        defaultValue_plural: 'The scheduled date is {{count}} days {{position}} the suggested date.',
+                    })
+            }
             </div>
-            <div className={classes.textBox}>
-                <div className={classes.textBold}>{i18n.t('Schedule date info')}</div>
-                <div className={classes.textLine}>{scheduleDate === suggestedScheduleDate ?
-                    i18n.t(`This date is the suggested scheduled date based on the intervals defined, 
-                    it can be adjusted if needed.`)
-                    :
-                    i18n.t(
-                        'This date is {{count}} days {{position}} the suggested date.',
-                        {
-                            position: differenceScheduleDateAndSuggestedDate > 0 ? i18n.t('after') : i18n.t('before'),
-                            count: Math.abs(differenceScheduleDateAndSuggestedDate),
-                            defaultValue: 'This date is {{count}} day {{position}} the suggested date.',
-                            defaultValue_plural: 'This date is {{count}} days {{position}} the suggested date.',
-                        })
-
-                }
-                </div>
-                <div className={classes.textLine}>
-                    {i18n.t('There are {{count}} scheduled event in {{orgUnitName}} on this day.', {
-                        count: eventCountInOrgUnit,
-                        orgUnitName,
-                        defaultValue: 'There are {{count}} scheduled event in {{orgUnitName}} on this day.',
-                        defaultValue_plural: 'There are {{count}} scheduled events in {{orgUnitName}} on this day.',
-                        interpolation: {
-                            escape: false,
-                        },
-                    })}
-                </div>
+            <div>
+                {i18n.t('There are {{count}} scheduled event in {{orgUnitName}} on this day.', {
+                    count: eventCountInOrgUnit,
+                    orgUnitName,
+                    defaultValue: 'There are {{count}} scheduled event in {{orgUnitName}} on this day.',
+                    defaultValue_plural: 'There are {{count}} scheduled events in {{orgUnitName}} on this day.',
+                    interpolation: {
+                        escape: false,
+                    },
+                })}
             </div>
-        </div>
+        </NoticeBox>
     );
 };
 
