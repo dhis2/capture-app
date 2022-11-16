219687032ac636ae465ebc5f303dee77588f5677
Simona Domnisoru <simona.domnisoru@gmail.com>
2022-11-16 10:58:51 +0100

    feat: [DHIS2-13661][DHIS2-13747][DHIS2-13748][DHIS2-13749][DHIS2-13750] program stages working list (#3062)
    * feat: [DHIS2-13661] program stage working list default filters (#2995)
    
    * fix: handle missing trackedEntity and subvalue
    
    * fix: use occurredAt custom date and fix report date filter
    
    * chore: use getLabel
    
    * fix: order parameter and list column id convertors
    
    * chore: clean empty columns
    
    * feat: [DHIS2-13748] remove filter button (#3088)
    
    * feat: remove filter button
    
    * chore: flow error
    
    * chore: improve the logs when using multiple Filter component
    
    * chore: improve mapping and re-render only the image
    
    * fix: filters ui issues

--- src/core_modules/capture-core/components/ListView/Filters/index.js
+++ src/core_modules/capture-core/components/ListView/Filters/index.js
@@ -1,3 +1,4 @@
 // @flow
 export { Filters } from './Filters.component';
+export { FiltersRows } from './FiltersRows.component';
 export { filterTypesObject } from './filters.const';
--- src/core_modules/capture-core/components/WorkingLists/TeiWorkingLists/epics/teiViewEpics/helpers/updateTeiWorkingListsRecords.js
+++ src/core_modules/capture-core/components/WorkingLists/TeiWorkingLists/epics/teiViewEpics/helpers/updateTeiWorkingListsRecords.js
@@ -3,6 +3,7 @@ import log from 'loglevel';
 import i18n from '@dhis2/d2-i18n';
 import { errorCreator } from 'capture-core-utils';
 import { getTeiListData } from './getTeiListData';
+import { getEventListData } from './getEventListData';
 import {
     updateListSuccess,
     updateListError,
@@ -14,6 +15,7 @@ export const updateTeiWorkingListsRecords = ({
     page,
     pageSize,
     programId,
+    programStageId,
     orgUnitId,
     sortById,
     sortByDirection,
@@ -25,14 +27,16 @@ export const updateTeiWorkingListsRecords = ({
     absoluteApiPath,
 }: Input) => {
     const filters = buildFilterQueryArgs(filterSource, { columns: columnsMetaForDataFetching, filtersOnly: filtersOnlyMetaForDataFetching, storeId });
+    const rawQueryArgs
+    = { programId, orgUnitId, pageSize, page, filters, sortById, sortByDirection };
+    const params = { columnsMetaForDataFetching, filtersOnlyMetaForDataFetching, querySingleResource, absoluteApiPath };
+    const promiseToUpdateRecordsList = programStageId
+        ? getEventListData({ ...rawQueryArgs, programStageId }, params)
+        : getTeiListData(rawQueryArgs, params);
 
-    return getTeiListData({ programId, orgUnitId, pageSize, page, filters, sortById, sortByDirection }, {
-        columnsMetaForDataFetching,
-        filtersOnlyMetaForDataFetching,
-        querySingleResource,
-        absoluteApiPath })
-        .then(({ teis, request }) => updateListSuccess(storeId, {
-            recordContainers: teis,
+    return promiseToUpdateRecordsList
+        .then(({ recordContainers, request }) => updateListSuccess(storeId, {
+            recordContainers,
             request,
         })).catch((error) => {
             log.error(errorCreator('An error occurred when updating the working list records')({ error }));
