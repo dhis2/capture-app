75680b49a92437cf6f91b4e58623af34b81f5ffa
Tony Valle <79843014+superskip@users.noreply.github.com>
2023-03-02 14:08:18 +0100

    fix: [TECH-1499] hiding a section should not erase its fields (#3183)

--- src/core_modules/capture-core/rules/postProcessRulesEffects.js
+++ src/core_modules/capture-core/rules/postProcessRulesEffects.js
@@ -115,7 +115,7 @@ export function postProcessRulesEffects(
     const filteredAssignValueEffects = postProcessAssignEffects({
         // $FlowFixMe
         assignValueEffects,
-        hideFieldEffects: filteredHideFieldEffects,
+        hideFieldEffects,
     });
 
     return [
