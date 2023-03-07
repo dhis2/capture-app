686e02fe98d4be1b4770bccf853016ce4d5e9d52
jasminenguyennn <jasmine@dhis2.org>
2023-03-07 13:09:24 +0000

    fix: minor fix

--- src/core_modules/capture-core/components/Pages/New/EnrollmentRegistrationEntryWrapper.component.js
+++ src/core_modules/capture-core/components/Pages/New/EnrollmentRegistrationEntryWrapper.component.js
@@ -1,9 +1,9 @@
 // @flow
-import React, { useEffect } from 'react';
+import React, { type ComponentType, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { cleanUpUid } from './NewPage.actions';
 import { EnrollmentRegistrationEntry } from '../../DataEntries/EnrollmentRegistrationEntry/EnrollmentRegistrationEntry.container';
-
+import type { OwnProps } from '../../DataEntries/EnrollmentRegistrationEntry/EnrollmentRegistrationEntry.types';
 
 export const EnrollmentRegistrationEntryWrapper: ComponentType<OwnProps> = (props) => {
     const dispatch = useDispatch();
