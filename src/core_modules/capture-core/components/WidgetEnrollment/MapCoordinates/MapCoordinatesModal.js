a3a9dacb4dba2cdd8400b144dd43d6a40d8b9434
jasminenguyennn <jasmine@dhis2.org>
2023-03-24 16:59:27 +0000

    feat: [DHIS2-13237] update view/edit state

--- src/core_modules/capture-core/components/WidgetEnrollment/MapCoordinates/MapCoordinatesModal.js
+++ src/core_modules/capture-core/components/WidgetEnrollment/MapCoordinates/MapCoordinatesModal.js
@@ -1,7 +1,7 @@
 // @flow
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import i18n from '@dhis2/d2-i18n';
-import { spacers, Modal, ModalTitle, ModalContent, ModalActions, Button, ButtonStrip } from '@dhis2/ui';
+import { IconCross24, spacers, Modal, ModalTitle, ModalContent, ModalActions, Button, ButtonStrip } from '@dhis2/ui';
 import log from 'loglevel';
 import { ReactLeafletSearch } from 'react-leaflet-search-unpolyfilled';
 import { capitalizeFirstLetter } from 'capture-core-utils/string';
@@ -23,11 +23,16 @@ const styles = () => ({
         height: 'calc(100vh - 320px)',
     },
     inputWrapper: {
+        paddingTop: spacers.dp8,
         display: 'flex',
     },
     inputContent: {
-        paddingTop: spacers.dp8,
         flexGrow: 1,
+    },
+    fieldButton: {
+        height: '42px !important',
+        width: 42,
+        borderRadius: '0 !important',
     },
 });
 
@@ -58,14 +63,15 @@ const MapCoordinatesModalPlain = ({
     defaultValues,
     onSetCoordinates,
 }: ModalProps) => {
-    const [position, setPosition] = useState(type === dataElementTypes.COORDINATE ? defaultValues : null);
+    const isPoint = useMemo(() => type === dataElementTypes.COORDINATE, [type]);
+    const [position, setPosition] = useState(isPoint ? defaultValues : null);
     const [coordinates, setCoordinates] = useState(type === dataElementTypes.POLYGON ? defaultValues : null);
     const [hasChanges, setChanges] = useState(false);
     const [center, setCenter] = useState(initalCenter);
-    const [mousePosition, setMousePosition] = useState({ lat: null, lng: null });
+    const [isEditing, setEditing] = useState(!(isPoint && defaultValues));
 
     const onHandleMapClicked = (mapCoordinates) => {
-        if (type === dataElementTypes.COORDINATE) {
+        if (isPoint && isEditing) {
             const { lat, lng } = mapCoordinates.latlng;
             const newPosition: [number, number] = [lat, lng];
             setPosition(newPosition);
@@ -91,7 +97,7 @@ const MapCoordinatesModalPlain = ({
 
     const onSearch = (searchPosition: any) => {
         setCenter(searchPosition);
-        if (type === dataElementTypes.COORDINATE) {
+        if (isPoint) {
             setPosition(searchPosition);
             setChanges(true);
         }
@@ -139,7 +145,6 @@ const MapCoordinatesModalPlain = ({
         }}
         className={classes.map}
         onClick={onHandleMapClicked}
-        onMouseMove={(e) => { setMousePosition(e.latlng); }}
     >
         <WrappedLeafletSearch
             position="topleft"
@@ -148,6 +153,7 @@ const MapCoordinatesModalPlain = ({
             search={null}
             mapStateModifier={onSearch}
             showMarker={false}
+            openSearchOnLoad
         />
         <TileLayer
             url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
@@ -172,7 +178,7 @@ const MapCoordinatesModalPlain = ({
                 }}
             />
         </FeatureGroup>}
-        {type === dataElementTypes.COORDINATE && position && <Marker position={position} />}
+        {isPoint && position && <Marker position={position} />}
     </Map>);
 
     const onFeatureGroupReady = (reactFGref: any, featureCollection: ?FeatureCollection) => {
@@ -208,40 +214,72 @@ const MapCoordinatesModalPlain = ({
         (
             <CoordinateInput
                 label={i18n.t('Latitude')}
-                value={mousePosition?.lat}
+                value={position && position[0]}
                 classes={classes}
+                disabled={!isEditing}
                 onBlur={(latValue) => {
+                    if (!latValue) { return; }
+                    const newPosition = [Number(latValue), position && position[1]];
+                    if (newPosition?.length === 2) {
+                        // $FlowFixMe
+                        setPosition(newPosition);
+                        setCenter(newPosition);
+                        setChanges(true);
+                    }
+                }}
+                onChange={(latValue) => {
                     const newPosition = [Number(latValue), position && position[1]];
                     // $FlowFixMe
                     setPosition(newPosition);
-                    setCenter(newPosition);
-                    setChanges(true);
                 }}
-                onChange={(latValue) => {
-                    setMousePosition(prevPos => ({ ...prevPos, lat: latValue }));
-                }}
-                disabled={type !== dataElementTypes.COORDINATE}
             />
         );
+
     const renderLongtitude = () =>
         (
             <CoordinateInput
                 label={i18n.t('Longtitude')}
-                value={mousePosition?.lng}
+                value={position && position[1]}
                 classes={classes}
+                disabled={!isEditing}
                 onBlur={(lngValue) => {
+                    if (!lngValue) { return; }
+                    const newPosition = [position && position[0], Number(lngValue)];
+                    if (newPosition?.length === 2) {
+                        // $FlowFixMe
+                        setPosition(newPosition);
+                        setCenter(newPosition);
+                        setChanges(true);
+                    }
+                }}
+                onChange={(lngValue) => {
                     const newPosition = [position && position[0], Number(lngValue)];
                     // $FlowFixMe
                     setPosition(newPosition);
-                    setCenter(newPosition);
-                    setChanges(true);
                 }}
-                onChange={(lngValue) => {
-                    setMousePosition(prevPos => ({ ...prevPos, lng: lngValue }));
-                }}
-                disabled={type !== dataElementTypes.COORDINATE}
             />
         );
+
+    const renderFieldButton = () => (
+        <div>
+            {!isEditing ? <Button
+                className={classes.fieldButton}
+                primary
+                onClick={() => { setEditing(true); }}
+            >
+                {i18n.t('Edit')}
+            </Button> : <Button
+                className={classes.fieldButton}
+                disabled={!position}
+                icon={<IconCross24 />}
+                onClick={() => {
+                    // $FlowFixMe
+                    setPosition(null);
+                }}
+            />}
+        </div>
+
+    );
 
     const renderActions = () => (<ButtonStrip end>
         <Button
@@ -279,14 +317,15 @@ const MapCoordinatesModalPlain = ({
             <ModalContent>
                 <div className={classes.modalContent}>
                     {renderMap()}
-                    <div className={classes.inputWrapper}>
+                    {isPoint && <div className={classes.inputWrapper}>
                         <div className={classes.inputContent}>
                             {renderLatitude()}
                         </div>
                         <div className={classes.inputContent}>
                             {renderLongtitude()}
                         </div>
-                    </div>
+                        {renderFieldButton()}
+                    </div>}
                 </div>
             </ModalContent>
             <ModalActions>
