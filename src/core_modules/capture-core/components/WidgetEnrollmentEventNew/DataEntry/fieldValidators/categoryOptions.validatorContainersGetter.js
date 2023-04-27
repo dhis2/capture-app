e5a722971373bf8644e4f2c7a24e449c77e83146
jasminenguyennn <jasmine@dhis2.org>
2023-04-27 15:14:30 +0100

    chore: [DHIS2-14465] validation

--- src/core_modules/capture-core/components/WidgetEnrollmentEventNew/DataEntry/fieldValidators/categoryOptions.validatorContainersGetter.js
+++ src/core_modules/capture-core/components/WidgetEnrollmentEventNew/DataEntry/fieldValidators/categoryOptions.validatorContainersGetter.js
@@ -3,8 +3,9 @@ import i18n from '@dhis2/d2-i18n';
 import { hasValue } from 'capture-core-utils/validators/form';
 
 const validateCategories = (value?: ?string, props?: Object, fieldId?: string) => {
-    const categoryName = props?.categories
-        ?.find(category => category.id === fieldId)?.label;
+    const selectedCategory = props?.categories
+        ?.find(category => category.id === fieldId);
+    const categoryName = selectedCategory?.label || selectedCategory?.displayName;
 
     return {
         valid: hasValue(value),
