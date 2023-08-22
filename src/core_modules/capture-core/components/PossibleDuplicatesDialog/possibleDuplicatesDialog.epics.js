c4541167954199ab6dc2008b1a8b21b10cf2762c
Simona Domnisoru <simona.domnisoru@gmail.com>
2023-08-22 12:55:01 +0200

    feat: [DHIS2-15229] search for MULTI_TEXT (#3395)

--- src/core_modules/capture-core/components/PossibleDuplicatesDialog/possibleDuplicatesDialog.epics.js
+++ src/core_modules/capture-core/components/PossibleDuplicatesDialog/possibleDuplicatesDialog.epics.js
@@ -9,7 +9,7 @@ import {
     duplicatesForReviewRetrievalFailed,
 } from './possibleDuplicatesDialog.actions';
 import {
-    scopeTypes, getScopeFromScopeId, EventProgram, TrackerProgram, TrackedEntityType,
+    scopeTypes, getScopeFromScopeId, EventProgram, TrackerProgram, TrackedEntityType, dataElementTypes,
 } from '../../metaData';
 import { getDataEntryKey } from '../DataEntry/common/getDataEntryKey';
 import { convertFormToClient, convertClientToServer } from '../../converters';
@@ -65,7 +65,8 @@ export const loadSearchGroupDuplicatesForReviewEpic = (
                             return null;
                         }
                         const serverValue = element.convertValue(value, pipeD2(convertFormToClient, convertClientToServer));
-                        return `${element.id}:${element.optionSet ? 'eq' : 'like'}:${serverValue}`;
+                        const hasOptionSet = element.optionSet && element.type !== dataElementTypes.MULTI_TEXT;
+                        return `${element.id}:${hasOptionSet ? 'eq' : 'like'}:${serverValue}`;
                     })
                     .filter(f => f);
 
