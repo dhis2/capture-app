e48824dcede983d8c8c294f17e9af26f918835fe
Simona Domnisoru <simona.domnisoru@gmail.com>
2023-09-04 11:33:22 +0300

    chore: move cleanUp at the beginning of the test

--- cypress/integration/EnrollmentPage/HiddenProgramStage/index.js
+++ cypress/integration/EnrollmentPage/HiddenProgramStage/index.js
@@ -1,7 +1,25 @@
 import moment from 'moment';
-import '../../sharedSteps';
+
+const cleanUp = () => {
+    cy.visit(
+        '/#/enrollment?enrollmentId=fmhIsWXVDmS&orgUnitId=s7SLtx8wmRA&programId=WSGAb5XwJ3Y&teiId=uW8Y7AIcRKA',
+    );
+
+    cy.get('[data-test="enrollment-page-content"]').contains('Enrollment Dashboard');
+
+    cy.get('[data-test="stages-and-events-widget"]')
+        .find('[data-test="stage-content"]')
+        .eq(3)
+        .click();
+
+    cy.contains('WHOMCH Pregnancy outcome').should('exist');
+    cy.contains('[data-test="dhis2-uicore-button"]', 'Edit event').click();
+    cy.contains('[data-test="dhis2-uicore-button"]', 'Delete').click();
+    cy.contains('[data-test="dhis2-uicore-button"]', 'Yes, delete event').click();
+};
 
 Given('you add an enrollment event that will result in a rule effect to hide a program stage', () => {
+    cleanUp();
     cy.visit(
         '/#/enrollmentEventNew?enrollmentId=fmhIsWXVDmS&orgUnitId=s7SLtx8wmRA&programId=WSGAb5XwJ3Y&stageId=PFDfvmGpsR3&teiId=uW8Y7AIcRKA',
     );
@@ -22,8 +40,7 @@ Given('you add an enrollment event that will result in a rule effect to hide a p
 });
 
 Then('the New Postpartum care visit event button is disabled in the stages and events widget', () => {
-    cy.get('[data-test="create-new-button"]')
-        .contains('New Postpartum care visit event')
+    cy.contains('[data-test="create-new-button"]', 'New Postpartum care visit event')
         .should('be.disabled');
 });
 
@@ -31,11 +48,11 @@ Then('and an error is show in the Postpartum care visit stage', () => {
     cy.visit(
         '/#/enrollmentEventNew?enrollmentId=fmhIsWXVDmS&orgUnitId=s7SLtx8wmRA&programId=WSGAb5XwJ3Y&teiId=uW8Y7AIcRKA&stageId=bbKtnxRZKEP',
     );
-
-    cy.contains('[data-test="dhis2-uicore-button"]', 'Complete').should('be.disabled');
-    cy.contains('[data-test="dhis2-uicore-button"]', 'Save without completing').should('be.disabled');
-
-    cy.contains('[data-test="dhis2-uicore-noticebox-content"]', 'You can’t add any more Postpartum care visit events')
+    cy.contains('[data-test="dhis2-uicore-button"]', 'Complete')
+        .should('be.disabled');
+    cy.contains('[data-test="dhis2-uicore-button"]', 'Save without completing')
+        .should('be.disabled');
+    cy.contains('[data-test="dhis2-uicore-noticebox-content"]', 'You can\'t add any more Postpartum care visit events')
         .should('exist');
 });
 
@@ -45,21 +62,4 @@ Then('the Postpartum care visit button is disabled in the enrollmentEventNew pag
     );
 
     cy.contains('[data-test="program-stage-selector-button"]', 'Postpartum care visit').should('be.disabled');
-
-    // clean up
-    cy.visit(
-        '/#/enrollment?enrollmentId=fmhIsWXVDmS&orgUnitId=s7SLtx8wmRA&programId=WSGAb5XwJ3Y&teiId=uW8Y7AIcRKA',
-    );
-
-    cy.get('[data-test="enrollment-page-content"]').contains('Enrollment Dashboard');
-
-    cy.get('[data-test="stages-and-events-widget"]')
-        .find('[data-test="stage-content"]')
-        .eq(3)
-        .click();
-
-    cy.contains('WHOMCH Pregnancy outcome').should('exist');
-    cy.contains('[data-test="dhis2-uicore-button"]', 'Edit event').click();
-    cy.contains('[data-test="dhis2-uicore-button"]', 'Delete').click();
-    cy.contains('[data-test="dhis2-uicore-button"]', 'Yes, delete event').click();
 });
