f17d087f368cf4d6b26923fd5cf4fb15d7795ba2
Simona Domnisoru <simona.domnisoru@gmail.com>
2023-10-12 14:14:07 +0200

    fix: [DHIS2-15827] remove effects of 'Prevent adding new events to stage' action from the form (#3418)

--- src/core_modules/capture-core/components/WidgetEnrollmentEventNew/FinishButtons/FinishButtons.component.js
+++ src/core_modules/capture-core/components/WidgetEnrollmentEventNew/FinishButtons/FinishButtons.component.js
@@ -3,7 +3,6 @@ import React, { type ComponentType } from 'react';
 import i18n from '@dhis2/d2-i18n';
 import { withStyles } from '@material-ui/core';
 import { Button, spacersNum } from '@dhis2/ui';
-import { ConditionalTooltip } from 'capture-core/components/ConditionalTooltip';
 import { withCancelButton } from '../../DataEntry/withCancelButton';
 import { addEventSaveTypes } from '../DataEntry/addEventSaveTypes';
 import type { InputProps, Props } from './finishButtons.types';
@@ -18,40 +17,17 @@ const styles = {
     },
 };
 
-const FinishButtonsPlain = ({ onSave, cancelButton, hiddenProgramStage, stageName, classes }: Props) => (
+const FinishButtonsPlain = ({ onSave, cancelButton, classes }: Props) => (
     <div className={classes.container}>
         <div className={classes.button}>
-            <ConditionalTooltip
-                content={i18n.t("You can't add any more {{ programStageName }} events", {
-                    programStageName: stageName,
-                    interpolation: { escapeValue: false },
-                })}
-                enabled={hiddenProgramStage}
-            >
-                <Button
-                    disabled={hiddenProgramStage}
-                    onClick={() => onSave(addEventSaveTypes.COMPLETE)}
-                    primary
-                >
-                    {i18n.t('Complete')}
-                </Button>
-            </ConditionalTooltip>
+            <Button onClick={() => onSave(addEventSaveTypes.COMPLETE)} primary>
+                {i18n.t('Complete')}
+            </Button>
         </div>
         <div className={classes.button}>
-            <ConditionalTooltip
-                content={i18n.t("You can't add any more {{ programStageName }} events", {
-                    programStageName: stageName,
-                    interpolation: { escapeValue: false },
-                })}
-                enabled={hiddenProgramStage}
-            >
-                <Button
-                    disabled={hiddenProgramStage}
-                    onClick={() => onSave(addEventSaveTypes.SAVE_WITHOUT_COMPLETING)}
-                >
-                    {i18n.t('Save without completing')}
-                </Button>
-            </ConditionalTooltip>
+            <Button onClick={() => onSave(addEventSaveTypes.SAVE_WITHOUT_COMPLETING)}>
+                {i18n.t('Save without completing')}
+            </Button>
         </div>
         {cancelButton}
     </div>
