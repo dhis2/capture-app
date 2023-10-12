84ca7a4a19837dda3716867511e7dcdcf5c3af61
Simona Domnisoru <simona.domnisoru@gmail.com>
2023-10-12 14:14:07 +0200

    fix: [DHIS2-15827] remove effects of 'Prevent adding new events to stage' action from the form (#3418)

--- src/core_modules/capture-core/components/WidgetEnrollmentEventNew/FinishButtons/finishButtons.types.js
+++ src/core_modules/capture-core/components/WidgetEnrollmentEventNew/FinishButtons/finishButtons.types.js
@@ -6,14 +6,10 @@ export type InputProps = {|
     onSave: (saveType: $Keys<addEventSaveTypes>) => void,
     onCancel: () => void,
     id: string,
-    hiddenProgramStage: boolean,
-    stageName: string,
 |};
 
 export type Props = {|
     onSave: (saveType: $Keys<addEventSaveTypes>) => void,
     cancelButton: Element<any>,
-    hiddenProgramStage: boolean,
-    stageName: string,
     ...CssClasses,
 |};
