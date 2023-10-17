67c0dec1756bf199a6fd3584f3f0f397869155a3
Tony Valle <tony@devotta.no>
2023-10-17 10:42:58 +0200

    refactor: apply `getReduxOrgUnitEpic`

--- src/core_modules/capture-core/components/OrgUnitFetcher/OrgUnitFetcher.actions.js
+++ src/core_modules/capture-core/components/OrgUnitFetcher/OrgUnitFetcher.actions.js
@@ -1,6 +1,7 @@
 // @flow
 
 import { actionCreator } from '../../actions/actions.utils';
+import type { ReduxOrgUnit } from '../../redux/organisationUnits';
 
 export const actionTypes = {
     FETCH_ORG_UNIT: 'OrgUnitFetcher.FetchOrgUnit',
@@ -11,6 +12,6 @@ export const actionTypes = {
 export const fetchOrgUnit = (orgUnitId: string) =>
     actionCreator(actionTypes.FETCH_ORG_UNIT)({ orgUnitId });
 
-export const setCurrentOrgUnit = (orgUnit: Object) => actionCreator(actionTypes.FETCH_ORG_UNIT_SUCCESS)(orgUnit);
+export const setCurrentOrgUnit = (orgUnit: ReduxOrgUnit) => actionCreator(actionTypes.FETCH_ORG_UNIT_SUCCESS)(orgUnit);
 
 export const errorRetrievingOrgUnit = () => actionCreator(actionTypes.FETCH_ORG_UNIT_FAILURE)();
--- src/core_modules/capture-core/components/OrgUnitFetcher/OrgUnitFetcher.epics.js
+++ src/core_modules/capture-core/components/OrgUnitFetcher/OrgUnitFetcher.epics.js
@@ -1,17 +1,14 @@
 // @flow
 import { ofType } from 'redux-observable';
-import { catchError, map, switchMap } from 'rxjs/operators';
-import { from, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { getOrgUnit } from '../../redux/organisationUnits';
 import { actionTypes, setCurrentOrgUnit, errorRetrievingOrgUnit } from './OrgUnitFetcher.actions';
 
-
-const orgUnitsQuery = id => ({ resource: `organisationUnits/${id}` });
-
-export const orgUnitFetcherEpic = (action$: InputObservable, _: ReduxStore, { querySingleResource }: ApiUtils,
-) => action$.pipe(
+export const orgUnitFetcherEpic = (action$: InputObservable) => action$.pipe(
     ofType(actionTypes.FETCH_ORG_UNIT),
-    switchMap(({ payload: { orgUnitId } }) => from(querySingleResource(orgUnitsQuery(orgUnitId)))
-        .pipe(map(({ id, displayName: name }) => setCurrentOrgUnit({ id, name }))))
-    ,
-    catchError(() => of(errorRetrievingOrgUnit())),
+    map(({ payload: { orgUnitId } }) => getOrgUnit({
+        orgUnitId,
+        onSuccess: orgUnit => setCurrentOrgUnit(orgUnit),
+        onError: errorRetrievingOrgUnit,
+    })),
 );
