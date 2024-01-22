2bcd8e3e3dd43e17634086d2bed28a59f043e646
Simona Domnisoru <simona.domnisoru@gmail.com>
2024-01-22 15:05:21 +0100

    feat: use UserAvatar component

--- src/core_modules/capture-core/components/WidgetAssignee/DisplayMode.component.js
+++ src/core_modules/capture-core/components/WidgetAssignee/DisplayMode.component.js
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react';
 import i18n from '@dhis2/d2-i18n';
-import { IconEdit24, Button, spacers } from '@dhis2/ui';
+import { Button, spacers, UserAvatar } from '@dhis2/ui';
 import { withStyles } from '@material-ui/core/styles';
 import { ConditionalTooltip } from 'capture-core/components/Tooltips/ConditionalTooltip';
 import type { Assignee } from './WidgetAssignee.types';
@@ -12,12 +12,12 @@ const styles = () => ({
         alignItems: 'center',
     },
     editButton: {
-        border: 'none !important',
-        borderRadius: '50% !important',
-        padding: '0px 6px !important',
         margin: spacers.dp4,
     },
     assignButton: {
+        margin: spacers.dp4,
+    },
+    avatar: {
         margin: spacers.dp4,
     },
 });
@@ -32,7 +32,11 @@ type Props = {
 const DisplayModePlain = ({ assignee, onEdit, writeAccess, classes }: Props) => (
     assignee ? (
         <div className={classes.wrapper}>
-            {i18n.t('Event assigned to {{name}}', { name: assignee.name })}
+            <div>
+                {i18n.t('Assigned to')}
+                <UserAvatar name={assignee.name} className={classes.avatar} />
+                {assignee.name}
+            </div>
             <ConditionalTooltip
                 content={i18n.t("You don't have access to edit this assignee")}
                 enabled={!writeAccess}
@@ -42,8 +46,9 @@ const DisplayModePlain = ({ assignee, onEdit, writeAccess, classes }: Props) => 
                     className={classes.editButton}
                     dataTest="widget-assignee-edit"
                     disabled={!writeAccess}
+                    secondary
                 >
-                    <IconEdit24 />
+                    {i18n.t('Edit')}
                 </Button>
             </ConditionalTooltip>
         </div>
