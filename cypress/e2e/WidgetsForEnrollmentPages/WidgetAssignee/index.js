c272d46574195368af60a3d07810fa35e427262c
Simona Domnisoru <simona.domnisoru@gmail.com>
2024-01-23 15:58:37 +0100

    test: scenario for removing the assigned user

--- cypress/e2e/WidgetsForEnrollmentPages/WidgetAssignee/index.js
+++ cypress/e2e/WidgetsForEnrollmentPages/WidgetAssignee/index.js
@@ -2,8 +2,7 @@ import { When, Then } from '@badeball/cypress-cucumber-preprocessor';
 
 When('you assign the user Geetha in the view mode', () => {
     cy.get('[data-test="widget-assignee"]').within(() => {
-        cy.get('[data-test="widget-assignee-edit"]').click();
-        cy.get('[data-test="dhis2-uicore-chip-remove"]').click();
+        cy.get('[data-test="widget-assignee-assign"]').click();
         cy.get('[data-test="capture-ui-input"]').type('Geetha');
         cy.contains('Geetha Alwan').click();
         cy.get('[data-test="widget-assignee-save"]').click();
@@ -26,6 +25,14 @@ When('you assign the user Tracker demo User in the edit mode', () => {
     });
 });
 
+When('you remove the assigned user', () => {
+    cy.get('[data-test="widget-assignee"]').within(() => {
+        cy.get('[data-test="widget-assignee-edit"]').click();
+        cy.get('[data-test="dhis2-uicore-chip-remove"]').click();
+        cy.get('[data-test="widget-assignee-save"]').click();
+    });
+});
+
 Then('the event has the user Geetha Alwan assigned', () => {
     cy.get('[data-test="widget-assignee"]').within(() => {
         cy.get('[data-test="widget-contents"]').contains('Geetha Alwan').should('exist');
@@ -35,5 +42,11 @@ Then('the event has the user Geetha Alwan assigned', () => {
 Then('the event has the user Tracker demo User assigned', () => {
     cy.get('[data-test="widget-assignee"]').within(() => {
         cy.get('[data-test="widget-contents"]').contains('Tracker demo User').should('exist');
+    });
+});
+
+Then('the event has no assignd user', () => {
+    cy.get('[data-test="widget-assignee"]').within(() => {
+        cy.get('[data-test="widget-contents"]').contains('No one is assigned to this event').should('exist');
     });
 });
