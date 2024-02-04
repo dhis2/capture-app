5f08e424ca8c325482a573cd56c3651631a2b62e
eirikhaugstulen <eirik.haugstulen@gmail.com>
2024-02-04 18:32:21 +0100

    chore: fix tests

--- cypress/e2e/SearchThroughAddRelationship/index.js
+++ cypress/e2e/SearchThroughAddRelationship/index.js
@@ -130,12 +130,12 @@ And('you fill in the the form with values that will return exactly 5 results', (
 });
 
 When('you fill in the zip code range numbers', () => {
-    cy.get('[data-test="form-attributes"]')
+    cy.get('[data-test="d2-form-area"]')
         .find('input[description="Zip code"]')
         .eq(0)
         .type('7130')
         .blur();
-    cy.get('[data-test="form-attributes"]')
+    cy.get('[data-test="d2-form-area"]')
         .find('input[description="Zip code"]')
         .eq(1)
         .type('7135')
