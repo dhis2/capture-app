bce0f2fd65dc4525a31a54ec3305ad13327fe847
Simona Domnisoru <sdomnisoru@dhis2.org>
2024-03-05 13:46:25 +0100

    chore: use external ConditionalTooltip component

--- src/core_modules/capture-core/components/WidgetProfile/OverflowMenu/Delete/DeleteMenuItem/DeleteMenuItem.component.js
+++ src/core_modules/capture-core/components/WidgetProfile/OverflowMenu/Delete/DeleteMenuItem/DeleteMenuItem.component.js
@@ -3,7 +3,7 @@ import React, { useMemo } from 'react';
 import i18n from '@dhis2/d2-i18n';
 import { IconDelete16, MenuItem } from '@dhis2/ui';
 import type { Props } from './DeleteMenuItem.types';
-import { ConditionalTooltip } from './ConditionalTooltip';
+import { ConditionalTooltip } from '../../../../Tooltips/ConditionalTooltip/';
 
 const getTooltipContent = (disabled, trackedEntityTypeName) => {
     if (disabled) {
