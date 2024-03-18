0833f8ceaff4fb9b6760999dc078fa911f2d259c
eirikhaugstulen <eirik.haugstulen@gmail.com>
2024-03-18 13:33:11 +0100

    fix: use option set displayName in tables

--- src/core_modules/capture-core/components/Pages/common/TEIRelationshipsWidget/useTEIRelationshipsWidgetMetadata.js
+++ src/core_modules/capture-core/components/Pages/common/TEIRelationshipsWidget/useTEIRelationshipsWidgetMetadata.js
@@ -1,4 +1,5 @@
 // @flow
+import log from 'loglevel';
 import { useIndexedDBQuery } from '../../../../utils/reactQueryHelpers';
 import { getUserStorageController, userStores } from '../../../../storageControllers';
 import type { RelationshipTypes } from '../../../WidgetsRelationship';
@@ -6,6 +7,51 @@ import {
     extractElementIdsFromRelationshipTypes,
     formatRelationshipTypes,
 } from '../../../WidgetsRelationship';
+import { errorCreator } from '../../../../../capture-core-utils';
+
+// map through arrays of either dataElements or attributes and fetch optionSet values for each element if they have a optionSet
+const getOptionSetValuesForElement = async (elements: Array<Object>, userStorageController: Object) => {
+    // should be an object with keys of the element ids and value true
+    const optionSetIds = elements
+        .reduce((acc, { optionSet }) => {
+            if (optionSet) {
+                acc[optionSet.id] = true;
+            }
+            return acc;
+        }, {});
+
+    const optionSets = await userStorageController.getAll(userStores.OPTION_SETS, {
+        predicate: ({ id }) => optionSetIds[id],
+        project: ({ id, options }) => ({
+            id,
+            options: options
+                .map(({ code, displayName }) => ({ code, name: displayName })),
+        }),
+    });
+
+    if (!optionSets.length) {
+        return elements;
+    }
+
+    return elements.map((element) => {
+        if (element.optionSet) {
+            const optionSet = optionSets.find(({ id }) => id === element.optionSet.id);
+            if (!optionSet) {
+                log.error(
+                    errorCreator(`OptionSet with id ${element.optionSet.id} not found in indexedDB.`)({ element }));
+                return element;
+            }
+            return {
+                ...element,
+                optionSet: {
+                    ...element.optionSet,
+                    options: optionSet.options,
+                },
+            };
+        }
+        return element;
+    });
+};
 
 const getRelationshipTypes = async (): Promise<RelationshipTypes> => {
     const userStorageController = getUserStorageController();
@@ -15,18 +61,22 @@ const getRelationshipTypes = async (): Promise<RelationshipTypes> => {
 
     const attributes = (await userStorageController.getAll(userStores.TRACKED_ENTITY_ATTRIBUTES, {
         predicate: ({ id }) => attributeIds[id],
-        project: ({ id, valueType, displayName }) => ({ id, valueType, displayName }),
+        project: ({ id, valueType, displayName, optionSet }) => ({ id, valueType, displayName, optionSet }),
     }));
 
     const dataElements = (await userStorageController.getAll(userStores.DATA_ELEMENTS, {
         predicate: ({ id }) => dataElementIds[id],
-        project: ({ id, valueType, displayName }) => ({ id, valueType, displayName }),
+        project: ({ id, valueType, displayName, optionSet }) => ({ id, valueType, displayName, optionSet }),
     }));
+
+    const attributesWithPossibleOptionSet = await getOptionSetValuesForElement(attributes, userStorageController);
+
+    const dataElementsWithPossibleOptionSet = await getOptionSetValuesForElement(dataElements, userStorageController);
 
     return formatRelationshipTypes({
         relationshipTypes: cachedRelationshipTypes,
-        attributes,
-        dataElements,
+        attributes: attributesWithPossibleOptionSet,
+        dataElements: dataElementsWithPossibleOptionSet,
     });
 };
 
