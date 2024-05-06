140c287f86cc1bb4540d56475fbbe8178fb07531
Adel Bensaad <adel@dhis2.org>
2024-05-06 08:49:42 +0100

    chore: refine GitHub Actions for enhanced parallel execution

---
+++ cypress/support/generateTestMatrix.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+
+const getAllFiles = (dirPath, arrayOfFiles = []) => {
+  const files = fs.readdirSync(dirPath);
+
+  files.forEach((file) => {
+    if (fs.statSync(path.join(dirPath, file)).isDirectory()) {
+      arrayOfFiles = getAllFiles(path.join(dirPath, file), arrayOfFiles);
+    } else {
+      if (path.extname(file) === ".feature") {
+        arrayOfFiles.push(path.join(dirPath, file));
+      }
+    }
+  });
+
+  return arrayOfFiles;
+};
+
+const cypressSpecsPath = "./cypress/e2e";
+const specs = getAllFiles(cypressSpecsPath);
+
+console.log(JSON.stringify(specs));
