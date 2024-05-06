7655d08eac5579c1ca5c818787957667e4bb28b2
Adel Bensaad <adel@dhis2.org>
2024-05-06 09:18:59 +0100

    chore: fix linting

--- cypress/support/generateTestMatrix.js
+++ cypress/support/generateTestMatrix.js
@@ -1,23 +1,23 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const getAllFiles = (dirPath, arrayOfFiles = []) => {
-  const files = fs.readdirSync(dirPath);
+    const files = fs.readdirSync(dirPath);
 
-  files.forEach((file) => {
-    if (fs.statSync(path.join(dirPath, file)).isDirectory()) {
-      arrayOfFiles = getAllFiles(path.join(dirPath, file), arrayOfFiles);
-    } else {
-      if (path.extname(file) === ".feature") {
-        arrayOfFiles.push(path.join(dirPath, file));
-      }
-    }
-  });
+    files.forEach((file) => {
+        if (fs.statSync(path.join(dirPath, file)).isDirectory()) {
+            arrayOfFiles = getAllFiles(path.join(dirPath, file), arrayOfFiles);
+        } else {
+            if (path.extname(file) === '.feature') {
+                arrayOfFiles.push(path.join(dirPath, file));
+            }
+        }
+    });
 
-  return arrayOfFiles;
+    return arrayOfFiles;
 };
 
-const cypressSpecsPath = "./cypress/e2e";
+const cypressSpecsPath = './cypress/e2e';
 const specs = getAllFiles(cypressSpecsPath);
 
 console.log(JSON.stringify(specs));
