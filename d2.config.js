cd100a7f8e7b47a4f3ad83a10f18e8e4f6f84435
Joakim Storløkken Melseth <joakim.melseth@gmail.com>
2024-05-07 12:58:53 +0200

    fix: [DHIS2-17359] major RTL issues

--- d2.config.js
+++ d2.config.js
@@ -2,7 +2,7 @@ const config = {
     name: 'capture',
     title: 'Capture',
     type: 'app',
-
+    direction: 'auto',
     id: '92b75fd0-34cc-451c-942f-3dd0f283bcbd',
     minDHIS2Version: '2.38',
     coreApp: true,
