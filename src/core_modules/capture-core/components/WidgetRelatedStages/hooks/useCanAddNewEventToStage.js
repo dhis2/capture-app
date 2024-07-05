91c2530bfefe945f4fc855964559369b4138afc7
henrikmv <henrik@devOtta.no>
2024-07-05 17:18:27 +0200

    fix: lint

--- src/core_modules/capture-core/components/WidgetRelatedStages/hooks/useCanAddNewEventToStage.js
+++ src/core_modules/capture-core/components/WidgetRelatedStages/hooks/useCanAddNewEventToStage.js
@@ -2,14 +2,14 @@ import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
 export const useCanAddNewEventToStage = (programStage, existingRelatedEvents) => {
-    const hiddenProgramStages = useSelector(({ rulesEffectsHiddenProgramStageDesc }) => 
-        rulesEffectsHiddenProgramStageDesc?.['enrollmentEvent-newEvent']
+    const hiddenProgramStages = useSelector(({ rulesEffectsHiddenProgramStageDesc }) =>
+        rulesEffectsHiddenProgramStageDesc?.['enrollmentEvent-newEvent'],
     );
 
     return useMemo(() => {
         const isProgramStageHidden = hiddenProgramStages?.[programStage.id];
 
-        if (isProgramStageHidden) { return false }
+        if (isProgramStageHidden) { return false; }
 
         return programStage && existingRelatedEvents
             ? programStage.repeatable || (!programStage.repeatable && existingRelatedEvents.length === 0)
