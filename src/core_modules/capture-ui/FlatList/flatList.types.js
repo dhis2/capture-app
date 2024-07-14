cd64f1be1da660e90dc30aa6b67d570d8106f9aa
henrikmv <henrik@devOtta.no>
2024-07-14 18:59:02 +0200

    fix: lint error

--- src/core_modules/capture-ui/FlatList/flatList.types.js
+++ src/core_modules/capture-ui/FlatList/flatList.types.js
@@ -1,8 +1,6 @@
 // @flow
-import { type Node } from 'react';
-
 export type Props = {|
-    //value er et object som har en id og en name
+    // value er et object som har en id og en name
     list: { reactKey: string, key: string, value: { id: string, name: string }, valueType?: string }[],
     dataTest?: string,
     ...CssClasses,
