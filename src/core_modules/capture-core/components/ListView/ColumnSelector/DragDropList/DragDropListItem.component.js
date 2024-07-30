23c9b75acdaf6f8de561f35e94f588c811f6bb80
henrikmv <henrik@devOtta.no>
2024-07-30 08:59:07 +0200

    fix: restore comments

--- src/core_modules/capture-core/components/ListView/ColumnSelector/DragDropList/DragDropListItem.component.js
+++ src/core_modules/capture-core/components/ListView/ColumnSelector/DragDropList/DragDropListItem.component.js
@@ -25,11 +25,18 @@ export const DragDropListItem = ({ id, index, text, visible, handleToggle, moveL
             const dragIndex = item.index;
             const hoverIndex = index;
 
+            // Don't replace items with themselves.   
             if (dragIndex === hoverIndex) {
                 return;
             }
 
+            // Time to actually perform the action
             moveListItem(dragIndex, hoverIndex);
+
+            // Note: we're mutating the item here!
+            // Generally it's better to avoid mutations,
+            // but it's good here for the sake of performance
+            // to avoid expensive index searches.
             item.index = hoverIndex;
         },
     });
