97d11baa3189c0a25917af11481b34c4e8f4acab
eirikhaugstulen <eirik.haugstulen@gmail.com>
2024-08-08 20:52:31 +0200

    chore: remove attribute

--- src/core_modules/capture-core/constants/dataEntries.js
+++ src/core_modules/capture-core/constants/dataEntries.js
@@ -3,7 +3,6 @@
 export const dataEntryIds = Object.freeze({
     SINGLE_EVENT: 'singleEvent',
     ENROLLMENT_EVENT: 'enrollmentEvent',
-    LINKED_ENROLLMENT_EVENT: 'linkedEnrollmentEvent',
 });
 
 export const dataEntryKeys = Object.freeze({
