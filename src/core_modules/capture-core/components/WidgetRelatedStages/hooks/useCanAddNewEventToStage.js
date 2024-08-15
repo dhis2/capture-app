47ed42c4c0d1a23ce93ce4ccbd02461ae22d2a18
henrikmv <110386561+henrikmv@users.noreply.github.com>
2024-08-15 12:25:40 +0200

    fix: [DHIS2-17101] Consider prevent adding events to stage program rule (#3706)
    * fix: Consider program rule
    
    * fix: lint
    
    * fix: consider program rule
    
    * fix: missing flow and types
    
    * fix: remove console lines

--- src/core_modules/capture-core/components/WidgetRelatedStages/hooks/useCanAddNewEventToStage.js
+++ src/core_modules/capture-core/components/WidgetRelatedStages/hooks/useCanAddNewEventToStage.js
@@ -1,8 +1,21 @@
+// @flow
 import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { ProgramStage } from '../../../metaData';
+import type { LinkableEvent } from '../RelatedStagesActions/RelatedStagesActions.types';
 
-export const useCanAddNewEventToStage = (programStage, existingRelatedEvents) => useMemo(() =>
-    (programStage && existingRelatedEvents
-        ? programStage.repeatable || (!programStage.repeatable && existingRelatedEvents.length === 0)
-        : false),
-[programStage, existingRelatedEvents],
-);
+export const useCanAddNewEventToStage = (programStage: ?ProgramStage, existingRelatedEvents: LinkableEvent[]) => {
+    const hiddenProgramStages = useSelector(({ rulesEffectsHiddenProgramStageDesc }) =>
+        rulesEffectsHiddenProgramStageDesc?.['enrollmentEvent-newEvent'],
+    );
+
+    return useMemo(() => {
+        const isProgramStageHidden = hiddenProgramStages?.[programStage?.id];
+
+        if (isProgramStageHidden) { return false; }
+
+        return programStage && existingRelatedEvents
+            ? programStage.repeatable || (!programStage.repeatable && existingRelatedEvents.length === 0)
+            : false;
+    }, [programStage, existingRelatedEvents, hiddenProgramStages]);
+};
