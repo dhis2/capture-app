ecedaff3c624f98ace2b6dd9cd8a2d9815f3c011
Simona Domnisoru <sdomnisoru@dhis2.org>
2024-08-21 14:59:29 +0200

    fix: cypress tests related with enrollment dashboard UI changes

--- cypress/e2e/EnrollmentAddEventPage/EnrollmentAddEventPageForm/EnrollmentAddEventPageForm.js
+++ cypress/e2e/EnrollmentAddEventPage/EnrollmentAddEventPageForm/EnrollmentAddEventPageForm.js
@@ -119,11 +119,6 @@ When(/^you click the button to (.*) without post request/, (buttonText) => {
 
 When('the enrollment overview is finished loading', () => {
     cy.get('[data-test="dhis2-uicore-circularloader"]').should('not.exist');
-    cy.get('[data-test="dhis2-uicore-datatable"]')
-        .within(() => {
-            cy.get('[data-test="dhis2-uicore-datatablerow"]')
-                .should('exist');
-        });
 });
 
 When('the form is finished loading', () => {
