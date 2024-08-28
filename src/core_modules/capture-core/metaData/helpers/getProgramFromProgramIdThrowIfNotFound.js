855a67af2bdcd76ab8094a7eb37f95c4222d7d5e
alaa-yahia <alaay873@live.com>
2024-08-29 02:47:16 +0300

    chore: comment throw error when programId not found

--- src/core_modules/capture-core/metaData/helpers/getProgramFromProgramIdThrowIfNotFound.js
+++ src/core_modules/capture-core/metaData/helpers/getProgramFromProgramIdThrowIfNotFound.js
@@ -1,6 +1,6 @@
 // @flow
 import log from 'loglevel';
-import i18n from '@dhis2/d2-i18n';
+// import i18n from '@dhis2/d2-i18n';
 import { errorCreator } from 'capture-core-utils';
 import { programCollection } from '../../metaDataMemoryStores';
 
@@ -13,6 +13,7 @@ export function getProgramFromProgramIdThrowIfNotFound(programId: string) {
     const program = programCollection.get(programId);
     if (!program) {
         log.error(errorCreator(errorMessages.PROGRAM_NOT_FOUND)({ programId }));
+        // throw Error(i18n.t(errorMessages.GENERIC_ERROR));
     }
     return program;
 }
