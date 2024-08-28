ed3ed5a58f602ea487b90bb5997aad9f3b6e22cb
henrikmv <110386561+henrikmv@users.noreply.github.com>
2024-08-28 12:48:51 +0200

    feat: [DHIS2-11836] respect block entry form (#3759)
    * feat: respect block entry form
    
    * feat: respect block entry form and authorities
    
    * fix: tooltip
    
    * feat: cypress test for disabled edit event button
    
    * fix: set tracker auto test restricted
    
    * fix: cypress test
    
    * feat: cy test for disabled button

---
+++ src/core_modules/capture-core/components/WidgetEventEdit/constants/status.const.js
@@ -0,0 +1,10 @@
+// @flow
+
+export const eventStatuses = Object.freeze({
+    ACTIVE: 'ACTIVE',
+    COMPLETED: 'COMPLETED',
+    VISITED: 'VISITED',
+    SCHEDULE: 'SCHEDULE',
+    OVERDUE: 'OVERDUE',
+    SKIPPED: 'SKIPPED',
+});
