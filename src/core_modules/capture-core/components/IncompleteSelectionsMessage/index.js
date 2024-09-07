d10215a3c30a71fb040add560e969b3fa33f68a7
henrikmv <henrik@devOtta.no>
2024-09-07 15:18:10 +0200

    feat; remove paper

--- src/core_modules/capture-core/components/IncompleteSelectionsMessage/index.js
+++ src/core_modules/capture-core/components/IncompleteSelectionsMessage/index.js
@@ -1,29 +1,23 @@
 import React from 'react';
-import Paper from '@material-ui/core/Paper/Paper';
-import { withStyles } from '@material-ui/core';
 import { colors } from '@dhis2/ui';
-
-const StyledPaper = withStyles({
-    root: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        background: colors.grey200,
-        color: colors.grey700,
-        padding: '12px 16px',
-    },
-})(Paper);
 
 const containerStyle = {
     display: 'flex',
     justifyContent: 'center',
-    width: '100%',
 };
 
-export const IncompleteSelectionsMessage = ({ children, dataTest = 'informative-paper' }) => (
+const messageBoxStyle = {
+    alignItems: 'center',
+    background: colors.grey200,
+    color: colors.grey700,
+    padding: '12px 16px',
+    borderRadius: '4px',
+};
+
+export const IncompleteSelectionsMessage = ({ children, dataTest = 'informative-message' }) => (
     <div style={containerStyle}>
-        <StyledPaper data-test={dataTest} elevation={0}>
+        <div style={messageBoxStyle} data-test={dataTest}>
             {children}
-        </StyledPaper>
+        </div>
     </div>
 );
