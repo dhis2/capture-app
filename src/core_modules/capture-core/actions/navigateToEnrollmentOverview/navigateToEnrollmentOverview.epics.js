006bca75ac5e6b3bca1e28eda9e665bf453fbfaf
Simona Domnisoru <sdomnisoru@dhis2.org>
2024-09-09 16:44:30 +0200

    chore: clean comment

--- src/core_modules/capture-core/actions/navigateToEnrollmentOverview/navigateToEnrollmentOverview.epics.js
+++ src/core_modules/capture-core/actions/navigateToEnrollmentOverview/navigateToEnrollmentOverview.epics.js
@@ -6,14 +6,12 @@ import { EMPTY } from 'rxjs';
 import { actionTypes as NavigateToEnrollmentOverviewActionTypes } from './navigateToEnrollmentOverview.actions';
 import { buildUrlQueryString } from '../../utils/routing';
 
-// TODO does this epic make sense and the NAVIGATE_TO_ENROLLMENT_OVERVIEW actionnstill make sense?
 export const navigateToEnrollmentOverviewEpic = (action$: InputObservable, store: ReduxStore, { history }: ApiUtils) =>
     action$.pipe(
         ofType(NavigateToEnrollmentOverviewActionTypes.NAVIGATE_TO_ENROLLMENT_OVERVIEW),
         switchMap((action) => {
             const { teiId, programId, orgUnitId } = action.payload;
             const enrollmentId = programId && (action.payload?.enrollmentId || 'AUTO');
-
             history.push(
                 `/enrollment?${buildUrlQueryString({
                     teiId,
