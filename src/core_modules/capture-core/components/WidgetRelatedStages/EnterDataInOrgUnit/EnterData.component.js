32b73b970e20decb317100e827054042a0b074bb
henrikmv <henrik@devOtta.no>
2024-10-01 11:18:10 +0200

    feat: add org unit selector

---
+++ src/core_modules/capture-core/components/WidgetRelatedStages/EnterDataInOrgUnit/EnterData.component.js
@@ -0,0 +1,111 @@
+// @flow
+import React from 'react';
+import i18n from '@dhis2/d2-i18n';
+import type { ComponentType } from 'react';
+import { withStyles } from '@material-ui/core';
+import { colors, spacers, spacersNum, IconInfo16 } from '@dhis2/ui';
+import { OrgUnitSelectorForRelatedStages } from '../FormComponents';
+import type { ErrorMessagesForRelatedStages } from '../RelatedStagesActions';
+import type { RelatedStageDataValueStates } from '../WidgetRelatedStages.types';
+
+const styles = {
+    wrapper: {
+        padding: `${spacers.dp16} 0`,
+        maxWidth: '55.75rem',
+    },
+    fieldWrapper: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'start',
+        justifyContent: 'space-between',
+        padding: `${spacers.dp8}  ${spacers.dp16}`,
+    },
+    fieldLabel: {
+        color: colors.grey900,
+        paddingTop: spacersNum.dp12,
+        paddingRight: spacersNum.dp16,
+        flexBasis: '200px',
+    },
+    fieldContent: {
+        flexBasis: '150px',
+        flexGrow: 1,
+    },
+    alternateColor: {
+        backgroundColor: colors.grey100,
+    },
+    infoBox: {
+        margin: '8px 8px',
+        display: 'flex',
+        fontSize: '14px',
+        gap: '5px',
+        background: colors.grey100,
+        padding: '12px 8px',
+        border: `1px solid ${colors.grey600}`,
+    },
+};
+
+type Props = {
+    linkableStageLabel: string,
+    relatedStagesDataValues: RelatedStageDataValueStates,
+    setRelatedStagesDataValues: (() => Object) => void,
+    currentStageLabel: string,
+    saveAttempted: boolean,
+    errorMessages: ErrorMessagesForRelatedStages,
+    ...CssClasses
+}
+
+export const EnterDataInOrgUnitPlain = ({
+    linkableStageLabel,
+    relatedStagesDataValues,
+    setRelatedStagesDataValues,
+    saveAttempted,
+    errorMessages,
+    currentStageLabel,
+    classes,
+}: Props) => {
+    const onSelectOrgUnit = (e: { id: string, displayName: string, path: string }) => {
+        const orgUnit = {
+            id: e.id,
+            name: e.displayName,
+            path: e.path,
+        };
+
+        setRelatedStagesDataValues(prevValues => ({
+            ...prevValues,
+            orgUnit,
+        }));
+    };
+
+    const onDeselectOrgUnit = () => {
+        setRelatedStagesDataValues(prevValues => ({
+            ...prevValues,
+            orgUnit: null,
+        }));
+    };
+
+    return (
+        <div className={classes.wrapper}>
+            <div>
+                <OrgUnitSelectorForRelatedStages
+                    relatedStagesDataValues={relatedStagesDataValues}
+                    onSelectOrgUnit={onSelectOrgUnit}
+                    onDeselectOrgUnit={onDeselectOrgUnit}
+                    errorMessages={errorMessages}
+                    saveAttempted={saveAttempted}
+                />
+            </div>
+            <div className={classes.infoBox}>
+                <IconInfo16 />
+                {i18n.t(
+                    'Enter {{linkableStageLabel}} details in the next step after completing this {{currentStageLabel}}.',
+                    {
+                        linkableStageLabel,
+                        currentStageLabel,
+                    },
+                )}
+            </div>
+        </div>
+    );
+};
+
+export const EnterDataInOrgUnit: ComponentType<$Diff<Props, CssClasses>> = withStyles(styles)(EnterDataInOrgUnitPlain);
