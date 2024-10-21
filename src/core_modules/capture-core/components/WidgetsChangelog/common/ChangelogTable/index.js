3b021cf5569338db5038d8ed231a810a0fa909f3
henrikmv <110386561+henrikmv@users.noreply.github.com>
2024-10-21 09:10:50 +0200

    feat: [DHIS2-16992] Fixed size for changelog modal and columns (#3834)
    * feat: temp
    
    * feat: fixed size table and columns
    
    * fix: remove console log
    
    * fix: fixed size improvement

--- src/core_modules/capture-core/components/WidgetsChangelog/common/ChangelogTable/index.js
+++ src/core_modules/capture-core/components/WidgetsChangelog/common/ChangelogTable/index.js
@@ -1,6 +1,5 @@
 // @flow
 
 export { ChangelogTableHeader } from './ChangelogTableHeader';
-export { ChangelogChangeCell } from './ChangelogChangeCell';
 export { ChangelogTableRow } from './ChangelogTableRow';
 
