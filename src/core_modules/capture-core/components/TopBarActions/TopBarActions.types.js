9133a63e138517c633a192b5d51f39a43c57e327
Alaa Yahia <6881345+alaa-yahia@users.noreply.github.com>
2024-10-22 15:56:58 +0300

    fix: [DHIS2-16010] app crashes on invalid programid (#3765)
    * fix: app crash on invalid programid
    
    * fix: app stuck in loading state when url has invalid enrollmentId or teiId
    
    * fix: useProgramInfo hook breaks the app

--- src/core_modules/capture-core/components/TopBarActions/TopBarActions.types.js
+++ src/core_modules/capture-core/components/TopBarActions/TopBarActions.types.js
@@ -1,13 +1,13 @@
 // @flow
 
 export type Props = {
-    selectedProgramId?: string,
+    selectedProgramId?: ?string,
     selectedOrgUnitId?: string,
     isUserInteractionInProgress?: boolean,
 };
 
 export type PlainProps = {
-    selectedProgramId?: string,
+    selectedProgramId?: ?string,
     onNewClick: () => void,
     onNewClickWithoutProgramId: () => void,
     onFindClick: () => void,
