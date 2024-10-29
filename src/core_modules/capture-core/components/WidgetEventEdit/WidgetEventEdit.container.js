edee6d350fb2672a3470ef4c282460b34fa726eb
henrikmv <110386561+henrikmv@users.noreply.github.com>
2024-10-29 13:35:29 +0100

    fix: [DHIS2-17843] Disable delete enrollment button when user does not have authority (#3859)
    * fix: string adjustment
    
    * fix: disable button and add tooltip
    
    * fix: revert wrong commit
    
    * fix: create generic authorities hook

--- src/core_modules/capture-core/components/WidgetEventEdit/WidgetEventEdit.container.js
+++ src/core_modules/capture-core/components/WidgetEventEdit/WidgetEventEdit.container.js
@@ -32,7 +32,7 @@ import { EventChangelogWrapper } from './EventChangelogWrapper';
 import { FEATURES, useFeature } from '../../../capture-core-utils';
 import { inMemoryFileStore } from '../DataEntry/file/inMemoryFileStore';
 import { eventStatuses } from './constants/status.const';
-import { useAuthorities } from './hooks';
+import { useAuthorities } from '../../utils/authority/useAuthorities';
 
 const styles = {
     header: {
@@ -101,8 +101,8 @@ export const WidgetEventEditPlain = ({
     const loadedValues = useSelector(({ viewEventPage }) => viewEventPage.loadedValues);
 
     const eventAccess = getProgramEventAccess(programId, stageId);
-    const { canEditCompletedEvent } = useAuthorities();
-    const blockEntryForm = stage.blockEntryForm && !canEditCompletedEvent && eventStatus === eventStatuses.COMPLETED;
+    const { hasAuthority } = useAuthorities({ authorities: ['F_UNCOMPLETE_EVENT'] });
+    const blockEntryForm = stage.blockEntryForm && !hasAuthority && eventStatus === eventStatuses.COMPLETED;
     const disableEdit = !eventAccess?.write || blockEntryForm;
 
     const tooltipContent = blockEntryForm ?
