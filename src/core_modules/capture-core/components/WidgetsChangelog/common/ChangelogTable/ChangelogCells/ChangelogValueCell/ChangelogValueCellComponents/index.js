91c1890bd262ba0add12d115b3c8ed8cd46064c5
henrikmv <henrik@devOtta.no>
2024-11-28 15:05:54 +0100

    fix: revert new folder structure

--- src/core_modules/capture-core/components/WidgetsChangelog/common/ChangelogTable/ChangelogCells/ChangelogValueCell/ChangelogValueCellComponents/index.js
+++
@@ -1,5 +0,0 @@
-// @flow
-export { DefaultChangelogComponentsByChangeType } from './ChangelogValueCellDefault';
-export { PlygonChangelogComponentsByChangeType } from './ChangelogValueCellPolygon';
-
-export type { ChangelogValueCellProps } from './ChangelogValueCellComponents.types';
