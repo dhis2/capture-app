6625faa1cfe4748cee33ac12687d95e241399263
Joakim Storløkken Melseth <joakim@dhis2.org>
2025-03-14 22:07:17 +0100

    fix: enrollment dashboard access

---
+++ src/core_modules/capture-core/reducers/descriptions/breakTheGlass.reducerDescription.js
@@ -0,0 +1,14 @@
+// @flow
+import { createReducerDescription } from '../../trackerRedux/trackerReducer';
+import { enrollmentPageActionTypes } from '../../components/Pages/Enrollment/EnrollmentPage.actions';
+
+export const breakTheGlassAccessDesc = createReducerDescription({
+    [enrollmentPageActionTypes.BREAK_THE_GLASS_SUCCESS]: (state, { payload: { teiId, programId } }) => ({
+        ...state,
+        [teiId]: {
+            ...state[teiId],
+            // compose expiry time for temporary access
+            [programId]: new Date().getTime() + 3 * 60 * 60 * 1000,
+        },
+    }),
+}, 'breakTheGlassAccess', {});
