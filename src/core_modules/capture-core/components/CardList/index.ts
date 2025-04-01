0786ad7bad2b552d2c070ca9123f8f6700965841
eirikhaugstulen <eirik.haugstulen@gmail.com>
2025-04-01 12:46:03 +0200

    refactor: update TypeScript configuration and components

--- src/core_modules/capture-core/components/CardList/index.ts
+++ src/core_modules/capture-core/components/CardList/index.ts
@@ -1,4 +1,4 @@
-// @flow
 export { CardList } from './CardList.component';
 export { CardListButtons } from './CardListButtons.component';
+export { ListEntry } from './ListEntry.component';
 export type { RenderCustomCardActions } from './CardList.types';
