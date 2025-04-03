81cb176169d4beb1fda535f2ff79cbb9788bda70
eirikhaugstulen <eirik.haugstulen@gmail.com>
2025-04-03 11:47:58 +0200

    refactor: remove conditional check for alias definitions in ESLint configuration

--- config/eslint/alias.js
+++ config/eslint/alias.js
@@ -8,10 +8,8 @@ function fileExists(filePath) {
     return fs.existsSync(filePath);
 }
 
-if (fileExists(path.resolve(__dirname, '../../src/core_modules/capture-core'))) {
-    alias['capture-core'] = path.resolve(__dirname, '../../src/core_modules/capture-core');
-    alias['capture-ui'] = path.resolve(__dirname, '../../src/core_modules/capture-ui');
-    alias['capture-core-utils'] = path.resolve(__dirname, '../../src/core_modules/capture-core-utils');
-}
+alias['capture-core'] = path.resolve(__dirname, '../../src/core_modules/capture-core');
+alias['capture-ui'] = path.resolve(__dirname, '../../src/core_modules/capture-ui');
+alias['capture-core-utils'] = path.resolve(__dirname, '../../src/core_modules/capture-core-utils');
 
 module.exports = alias;
