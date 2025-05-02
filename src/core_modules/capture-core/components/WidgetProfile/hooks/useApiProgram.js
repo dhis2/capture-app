e86d8cf97a53726bfc53d5a48575aea5ee998298
henrikmv <110386561+henrikmv@users.noreply.github.com>
2025-05-02 15:35:42 +0200

    fix: [DHIS2-19441][DHIS2-19442] Missing translation (#4058)
    * fix: translate edit te string
    
    * fix: translate tea description
    
    * fix: remove console log
    
    * fix: temp
    
    * Revert "fix: temp"
    
    This reverts commit 40cc52f7c2fd2fab96af167b7519522e5fec324a.

--- src/core_modules/capture-core/components/WidgetProfile/hooks/useApiProgram.js
+++ src/core_modules/capture-core/components/WidgetProfile/hooks/useApiProgram.js
@@ -15,7 +15,7 @@ const fields =
         'programStageSections[id,displayName,displayDescription,sortOrder,dataElements[id]],' +
         'programStageDataElements[compulsory,displayInReports,renderOptionsAsRadio,allowFutureDate,renderType[*],dataElement[id,displayName,displayShortName,displayFormName,valueType,translations[*],description,optionSetValue,style,optionSet[id,displayName,version,valueType,options[id,displayName,code,style, translations]]]]' +
     '],' +
-    'programTrackedEntityAttributes[trackedEntityAttribute[id,displayName,displayShortName,displayFormName,description,valueType,optionSetValue,unique,orgunitScope,pattern,translations[property,locale,value],optionSet[id,displayName,version,valueType,options[id,displayName,name,code,style,translations]]],displayInList,searchable,mandatory,renderOptionsAsRadio,allowFutureDate],' +
+    'programTrackedEntityAttributes[trackedEntityAttribute[id,displayName,displayShortName,displayFormName,displayDescription,valueType,optionSetValue,unique,orgunitScope,pattern,translations[property,locale,value],optionSet[id,displayName,version,valueType,options[id,displayName,name,code,style,translations]]],displayInList,searchable,mandatory,renderOptionsAsRadio,allowFutureDate],' +
     'trackedEntityType[id,access,displayName,allowAuditLog,minAttributesRequiredToSearch,featureType,trackedEntityTypeAttributes[trackedEntityAttribute[id],displayInList,mandatory,searchable],translations[property,locale,value]],' +
     'userRoles[id,displayName]';
 
