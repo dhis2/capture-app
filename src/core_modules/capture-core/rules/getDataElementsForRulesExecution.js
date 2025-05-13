e504a545bb63b294fb2bb4bd4329c1c7da7f4866
Tony Valle <tony@dhis2.org>
2025-05-13 11:27:48 +0200

    Revert "fix: provide all program stage data elements to rule engine"
    This reverts commit b284a6611d964cca96c3f57f6e80a42a4204422d.

--- src/core_modules/capture-core/rules/getDataElementsForRulesExecution.js
+++ src/core_modules/capture-core/rules/getDataElementsForRulesExecution.js
@@ -3,8 +3,13 @@ import type { ProgramStage } from '../metaData';
 
 export const getDataElementsForRulesExecution = (stages: Map<string, ProgramStage>) =>
     [...stages.values()]
-        .flatMap(stage => stage.dataElements)
+        .flatMap(stage => stage.stageForm.getElements())
         .reduce((accRulesDataElements, dataElement) => {
-            accRulesDataElements[dataElement.id] = dataElement;
+            accRulesDataElements[dataElement.id] = {
+                id: dataElement.id,
+                valueType: dataElement.type,
+                optionSetId: dataElement.optionSet?.id,
+                name: dataElement.formName || dataElement.name,
+            };
             return accRulesDataElements;
         }, {});
