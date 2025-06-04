1a6bf3e72b3160b8b1f37ae02a1046de7a8458ba
Simona Domnisoru <sdomnisoru@dhis2.org>
2025-06-04 15:20:19 +0200

    fix: use the EQ operator when filtering by unique TEA in the WLs

--- src/core_modules/capture-core/components/WorkingLists/WorkingListsCommon/helpers/buildFilterQueryArgs/filterConverters/textConverter.js
+++ src/core_modules/capture-core/components/WorkingLists/WorkingListsCommon/helpers/buildFilterQueryArgs/filterConverters/textConverter.js
@@ -3,5 +3,6 @@ import type { TextFilterData } from '../../../../../ListView';
 import { escapeString } from '../../../../../../utils/escapeString';
 
 export function convertText({ sourceValue, unique }: { sourceValue: TextFilterData, unique?: boolean }): string {
-    return unique ? `eq:${escapeString(sourceValue.value)}` : `like:${escapeString(sourceValue.value)}`;
+    const operator = unique ? 'eq' : 'like';
+    return `${operator}:${escapeString(sourceValue.value)}`;
 }
