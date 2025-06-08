4a06349f39d0da3e8bb85e8c486d5711160a0a80
henrikmv <henrik@devOtta.no>
2025-06-08 13:46:07 +0200

    Revert "fix: use proper ProgramRulesContainer type instead of any"
    This reverts commit f46967548e6e003f107db3ea93d3d53b743a45fe.

--- src/core_modules/capture-core/components/WidgetProfile/DataEntry/hooks/useRulesContainer.ts
+++ src/core_modules/capture-core/components/WidgetProfile/DataEntry/hooks/useRulesContainer.ts
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import type { ProgramRulesContainer } from '../../../../rules/RuleEngine/types/ruleEngine.types';
 
 import { useProgramRules, useConstants } from './index';
 import { buildRulesContainer } from '../ProgramRules';
@@ -7,7 +6,7 @@ import { buildRulesContainer } from '../ProgramRules';
 export const useRulesContainer = (programAPI: any) => {
     const { programRules, loading: loadingProgramRules } = useProgramRules(programAPI.id);
     const { constants } = useConstants();
-    const [rulesContainer, setRulesContainer] = useState<ProgramRulesContainer>({});
+    const [rulesContainer, setRulesContainer] = useState<any>({});
 
     useEffect(() => {
         if (!loadingProgramRules && constants) {
