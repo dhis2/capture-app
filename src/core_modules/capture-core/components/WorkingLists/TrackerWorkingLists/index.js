a37dfd8ec28cedea818442f7e496f64e037e7bb2
henrikmv <110386561+henrikmv@users.noreply.github.com>
2025-06-26 11:40:55 +0200

    fix: [DHIS2-16601] Rename TEIWorkingLists to TrackerWorkingLists (#4136)
    * fix: rename
    
    * fix: add missing s
    
    * fix: rename test attribute and const
    
    * fix: imports
    
    * fix: typo
    
    * fix: update eslintrc to handle types
    
    * fix: case sensitiv paths name
    
    * fix: temp
    
    * fix: flow and lint
    
    * fix: incorrect filname

---
+++ src/core_modules/capture-core/components/WorkingLists/TrackerWorkingLists/index.js
@@ -0,0 +1,14 @@
+// @flow
+export { TrackerWorkingLists } from './TrackerWorkingLists.component';
+export {
+    initTeiViewEpic,
+    updateTeiListEpic,
+    retrieveTEITemplatesEpic,
+    updateTEITemplateEpic,
+    addTEITemplateEpic,
+    deleteTEITemplateEpic,
+    retrieveAllTemplatesEpic,
+    addProgramStageTemplateEpic,
+    deleteProgramStageTemplateEpic,
+    updateProgramStageTemplateEpic,
+} from './epics';
