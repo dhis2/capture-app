d2c3279001ae1f0975fc2f21c32e0f6a3c9b384f
Simona Domnisoru <sdomnisoru@dhis2.org>
2025-07-09 09:12:47 +0200

    chore: clean code before merge

--- src/core_modules/capture-core/components/BulkDataEntry/BulkDataEntryPlugin/BulkDataEntryPlugin.component.js
+++ src/core_modules/capture-core/components/BulkDataEntry/BulkDataEntryPlugin/BulkDataEntryPlugin.component.js
@@ -1,7 +1,6 @@
 // @flow
 import React, { useEffect, useRef, useState } from 'react';
 import { Plugin } from '@dhis2/app-runtime/experimental';
-import { Button } from '@dhis2/ui';
 import { withStyles } from '@material-ui/core/styles';
 import type { Props } from './BulkDataEntryPlugin.types';
 
@@ -54,28 +53,6 @@ const BulkDataEntryPluginPlain = ({
                 onDefer={onDefer}
                 trackedEntityIds={trackedEntityIds}
             />
-            <div style={{ marginTop: '50px' }}>
-                <p style={{ fontWeight: 'bold', marginBottom: '8px' }}>
-                    TODO delete buttons and trackedEntities list before merge
-                </p>
-                <p style={{ marginBottom: '8px' }}>
-                    Dummy buttons for testing that will be inside the Plugin.
-                </p>
-                <Button onClick={onDefer} style={{ marginRight: '8px' }}>
-                    Back to origin to add trackedEntities
-                </Button>
-                <Button onClick={onComplete} style={{ marginRight: '8px' }}>
-                    Close bulk data entry form
-                </Button>
-                <p style={{ marginTop: '16px' }}>List of the latest trackedEntities passed on to the Plugin for testing. The Plugin will be solely responsible for caching and keeping track of these trackedEntities</p>
-                <ul style={{ paddingLeft: '20px', listStyleType: 'disc' }}>
-                    {trackedEntityIds?.map(te => (
-                        <li key={te} style={{ marginBottom: '4px' }}>
-                            {te}
-                        </li>
-                    ))}
-                </ul>
-            </div>
         </div>
     );
 };
