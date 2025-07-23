8cc2bb1f59e46c15a0fc2383bcb0849cf8c75ad6
Simona Domnisoru <simona.domnisoru@gmail.com>
2025-07-23 16:23:14 +0200

    chore: [DHIS2-19350] use cursor to migrate D2Form/field folder (#4239)

---
+++ src/core_modules/capture-core/components/D2Form/field/Components/ImageField/ImageFieldForCustomForm.component.ts
@@ -0,0 +1,37 @@
+import {
+    withGotoInterface,
+    withHideCompatibility,
+    withDefaultShouldUpdateInterface,
+    withFocusSaver,
+    withCalculateMessages,
+    withDisplayMessages,
+    withInternalChangeHandler,
+} from '../../../../FormFields/New';
+import {
+    withRequiredFieldCalculation,
+    withDisabledFieldCalculation,
+    withCustomElementContainer,
+} from '../internal';
+import { D2Image } from '../../../../FormFields/Image/D2Image.component';
+
+export const ImageFieldForCustomForm = withGotoInterface()(
+    withHideCompatibility()(
+        withDefaultShouldUpdateInterface()(
+            withDisabledFieldCalculation()(
+                withRequiredFieldCalculation()(
+                    withCalculateMessages()(
+                        withFocusSaver()(
+                            withDisplayMessages()(
+                                withCustomElementContainer()(
+                                    withInternalChangeHandler()(
+                                        D2Image,
+                                    ),
+                                ),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+);
