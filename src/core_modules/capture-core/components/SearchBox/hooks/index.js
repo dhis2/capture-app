f29a5ff466ef63ca0db8c064c827e35a32fe0c93
henrikmv <henrik@devOtta.no>
2025-07-24 10:58:08 +0200

    fix: filter range value for prepopultaion

--- src/core_modules/capture-core/components/SearchBox/hooks/index.js
+++ src/core_modules/capture-core/components/SearchBox/hooks/index.js
@@ -3,3 +3,4 @@
 export { buildSearchGroup } from './buildSearchGroup';
 export { useSearchOption } from './useSearchOption';
 export { usePreselectedProgram } from './usePreselectedProgram';
+export { filteredRangeForPrepopulation } from './prepopulateRangeFilter';
