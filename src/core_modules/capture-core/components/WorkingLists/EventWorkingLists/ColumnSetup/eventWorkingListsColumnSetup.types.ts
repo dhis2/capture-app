3a410fe782a0aa2323be7d83c7bb32a683ba7752
Simona Domnisoru <sdomnisoru@dhis2.org>
2025-08-05 12:56:23 +0200

    chore: fix complex typescript errors

--- src/core_modules/capture-core/components/WorkingLists/EventWorkingLists/ColumnSetup/eventWorkingListsColumnSetup.types.ts
+++ src/core_modules/capture-core/components/WorkingLists/EventWorkingLists/ColumnSetup/eventWorkingListsColumnSetup.types.ts
@@ -14,7 +14,6 @@ type ExtractedProps = {
 // had to add customColumnOrder as a non optional type or else it would not be removed. Also, if customColumnOrder is
 // added as non optional to the ExtractedProps only (and not to EventWorkingListsReduxOutputProps),
 // flow complaints about one them being optional.
-// type RestProps = EventWorkingListsReduxOutputProps & OptionalExtractedProps | ExtractedProps;
 
 type RestProps = Omit<EventWorkingListsReduxOutputProps & { customColumnOrder: CustomColumnOrder },
     keyof (ExtractedProps & { customColumnOrder: CustomColumnOrder })>;
