531a816c5a522913f867b53ba59d3640b9ef6439
Tony Valle <tony@dhis2.org>
2025-08-07 12:40:04 +0200

    fix: logic error in previous commit
    Discovered thanks to the unit tests! :)

--- src/core_modules/capture-core/rules/RuleEngine/helpers/InputBuilder.js
+++ src/core_modules/capture-core/rules/RuleEngine/helpers/InputBuilder.js
@@ -256,7 +256,7 @@ export class InputBuilder {
         const dataValues = Object
             .keys(eventData)
             .filter(key => !eventMainKeys.has(key))
-            .filter(key => eventData[key] ?? false)
+            .filter(key => (eventData[key] ?? null) !== null)
             .map(key =>
                 new RuleDataValue(
                     key,
@@ -340,7 +340,7 @@ export class InputBuilder {
 
         const attributeValues = selectedEntity ? Object
             .keys(selectedEntity)
-            .filter(key => selectedEntity[key] ?? false)
+            .filter(key => (selectedEntity[key] ?? null) !== null)
             .map(key => new RuleAttributeValue(
                 key,
                 this.convertTrackedEntityAttributeValue(key, selectedEntity[key]),
