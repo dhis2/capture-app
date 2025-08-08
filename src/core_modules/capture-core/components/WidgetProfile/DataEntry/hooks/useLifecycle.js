21dfe89c4b5d919499edae878496abea7223c6f2
Tony Valle <79843014+superskip@users.noreply.github.com>
2025-08-08 10:54:43 +0200

    fix: update dependency array

--- src/core_modules/capture-core/components/WidgetProfile/DataEntry/hooks/useLifecycle.js
+++ src/core_modules/capture-core/components/WidgetProfile/DataEntry/hooks/useLifecycle.js
@@ -135,7 +135,6 @@ export const useLifecycle = ({
         querySingleResource,
         onGetValidationContext,
         awaitingInitialRulesExecution,
-        setAwaitingInitialRulesExecution,
     ]);
 
     return {
