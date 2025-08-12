466a32466a075b78119819d318762f70e5ac10f1
Simona Domnisoru <sdomnisoru@dhis2.org>
2025-08-12 15:37:03 +0200

    chore: fix complex typescript errors

--- src/core_modules/capture-core/MetaDataStoreUtils/MetaDataStoreUtils.ts
+++ src/core_modules/capture-core/MetaDataStoreUtils/MetaDataStoreUtils.ts
@@ -9,7 +9,7 @@ export const getCachedSingleResourceFromKeyAsync = (
     return storageController.get(store, key).then(response => ({ response, ...propsToPass }));
 };
 
-export const containsKeyInStorageAsync = (store: any, key: string, propsToPass: any = {}) => {
+export const containsKeyInStorageAsync = (store: typeof USER_METADATA_STORES[keyof typeof USER_METADATA_STORES], key: string, propsToPass: any = {}) => {
     const storageController = getUserMetadataStorageController();
     return storageController.contains(store, key).then(response => ({ response, ...propsToPass }));
 };
