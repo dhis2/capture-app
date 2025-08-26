cb7dfc947cc950fdbdeae0cc931cc313ddde6023
henrikmv <henrik@devOtta.no>
2025-08-26 19:07:36 +0200

    fix: add missing props

--- src/core_modules/capture-core/components/Pages/ViewEvent/EventDetailsSection/EventDetailsSection.types.ts
+++ src/core_modules/capture-core/components/Pages/ViewEvent/EventDetailsSection/EventDetailsSection.types.ts
@@ -1,9 +1,11 @@
+import { ProgramStage } from "capture-core/metaData";
+
 export type PlainProps = {
     showEditEvent?: boolean;
     eventId: string;
     eventData: any;
     onOpenEditEvent: (orgUnit: any, programCategory?: any) => void;
-    programStage: any;
+    programStage: ProgramStage;
     eventAccess: { read: boolean, write: boolean };
     programId: string;
     onBackToAllEvents: () => void;
