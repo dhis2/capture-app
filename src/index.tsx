b390d389547aa711412eccf9b41ecf37a43d192f
Simona Domnisoru <sdomnisoru@dhis2.org>
2025-09-10 14:45:32 +0200

    chore: fix regenerator-runtime is not defined

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'regenerator-runtime'; // To fix the 'regeneratorRuntime is not defined' error comming from react-leaflet-search-unpolyfilled
 import 'capture-core-utils/extensions/asyncForEachArray';
 import 'capture-core-utils/extensions/arrayToHashMap';
 import './locales'; //eslint-disable-line
