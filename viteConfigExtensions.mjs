354389e248d79f261c90772a4a079acb83db15fe
Simona Domnisoru <sdomnisoru@dhis2.org>
2025-09-10 12:20:44 +0200

    chore: fix missing import alias

---
+++ viteConfigExtensions.mjs
@@ -0,0 +1,20 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+
+const viteConfig = defineConfig(async (configEnv) => {
+    const { mode } = configEnv;
+    return {
+        // In dev environments, don't clear the terminal after files update
+        clearScreen: mode !== 'development',
+        resolve: {
+            alias: {
+                'capture-core': path.resolve(__dirname, 'src/core_modules/capture-core'),
+                'capture-ui': path.resolve(__dirname, 'src/core_modules/capture-ui'),
+                'capture-core-utils': path.resolve(__dirname, 'src/core_modules/capture-core-utils'),
+                '@dhis2/rules-engine-javascript': path.resolve(__dirname, 'packages/rules-engine/src'),
+            },
+        },
+    };
+});
+
+export default viteConfig;
