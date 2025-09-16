f6298954c9bb1f55e2a0163f51f2b8032995a0cf
Simona Domnisoru <sdomnisoru@dhis2.org>
2025-09-16 09:37:14 +0200

    fix: missing QueryClient error

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import 'regenerator-runtime'; // To fix the 'regeneratorRuntime is not defined' error comming from react-leaflet-search-unpolyfilled
 import 'capture-core-utils/extensions/asyncForEachArray';
 import 'capture-core-utils/extensions/arrayToHashMap';
 import './locales'; //eslint-disable-line
 import { AppStart } from './components/AppStart';
 
-export default () => <AppStart />;
+const queryClient = new QueryClient();
+
+export default () => (
+    <QueryClientProvider client={queryClient}>
+        <AppStart />
+    </QueryClientProvider>
+);
