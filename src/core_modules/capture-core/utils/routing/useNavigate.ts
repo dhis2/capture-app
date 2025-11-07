811171dfdf541198b669c955ff9e1acde5cc1511
Tony Valle <tony@dhis2.org>
2025-11-07 13:17:26 +0100

    perf: remove unnecessary re-renders at EnrollmentEditEventPage-level

--- src/core_modules/capture-core/utils/routing/useNavigate.ts
+++ src/core_modules/capture-core/utils/routing/useNavigate.ts
@@ -1,14 +1,15 @@
+import { useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 
 export const useNavigate = () => {
     const history = useHistory();
 
-    const navigate = (path: string, scrollToTop = true) => {
+    const navigate = useCallback((path: string, scrollToTop = true) => {
         history.push(path);
         if (scrollToTop) {
             window.scrollTo(0, 0);
         }
-    };
+    }, [history]);
 
     return { navigate };
 };
