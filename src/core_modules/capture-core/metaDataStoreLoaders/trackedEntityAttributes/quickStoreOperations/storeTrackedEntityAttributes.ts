404446366cca1e94d5805fc268b1adcbac43107a
Simona Domnisoru <sdomnisoru@dhis2.org>
2025-11-24 13:44:15 +0100

    fix: add missing ,

--- src/core_modules/capture-core/metaDataStoreLoaders/trackedEntityAttributes/quickStoreOperations/storeTrackedEntityAttributes.ts
+++ src/core_modules/capture-core/metaDataStoreLoaders/trackedEntityAttributes/quickStoreOperations/storeTrackedEntityAttributes.ts
@@ -6,7 +6,7 @@ export const storeTrackedEntityAttributes = (ids: Array<string>) => {
         resource: 'trackedEntityAttributes',
         params: {
             fields: 'id,displayName,displayShortName,displayFormName,description,valueType,optionSetValue,unique,' +
-            'orgunitScope,pattern,code,attributeValues,inherit,translations[property,locale,value],optionSet[id]' +
+            'orgunitScope,pattern,code,attributeValues,inherit,translations[property,locale,value],optionSet[id],' +
             'minCharactersToSearch',
             filter: `id:in:[${ids.join(',')}]`,
             pageSize: ids.length,
