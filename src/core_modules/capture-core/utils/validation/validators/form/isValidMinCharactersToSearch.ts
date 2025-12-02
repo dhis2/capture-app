493eae79fa6191af6202193649c268e3985797c5
Simona Domnisoru <sdomnisoru@dhis2.org>
2025-12-02 15:31:34 +0100

    feat: add extra properties type checks for range value types

--- src/core_modules/capture-core/utils/validation/validators/form/isValidMinCharactersToSearch.ts
+++ src/core_modules/capture-core/utils/validation/validators/form/isValidMinCharactersToSearch.ts
@@ -6,20 +6,37 @@ const isValueBiggerThanMinCharactersToSearch = (value: string, minCharactersToSe
     return minCharactersToSearch <= trimmedValue.length;
 };
 
+const isValidMinCharactersToSearchRange = (value: { from: any; to: any }, minCharactersToSearch: number) => {
+    const { from, to } = value;
+
+    if (typeof from === 'string' && typeof to === 'string') {
+        return (
+            isValueBiggerThanMinCharactersToSearch(from, minCharactersToSearch) &&
+            isValueBiggerThanMinCharactersToSearch(to, minCharactersToSearch)
+        );
+    }
+
+    if ('date' in from && 'date' in to) {
+        return (
+            isValueBiggerThanMinCharactersToSearch(from.date, minCharactersToSearch) &&
+            isValueBiggerThanMinCharactersToSearch(to.date, minCharactersToSearch)
+        );
+    }
+
+    return true;
+};
+
 export const isValidMinCharactersToSearch = (value: any, minCharactersToSearch: number) => {
     if (value === undefined) {
         return true;
     }
 
-    if (typeof value == 'string') {
+    if (typeof value === 'string') {
         return isValueBiggerThanMinCharactersToSearch(value, minCharactersToSearch);
     }
 
     if ('from' in value && 'to' in value) {
-        return (
-            isValueBiggerThanMinCharactersToSearch(value.from, minCharactersToSearch) &&
-            isValueBiggerThanMinCharactersToSearch(value.to, minCharactersToSearch)
-        );
+        return isValidMinCharactersToSearchRange(value, minCharactersToSearch);
     }
 
     if ('date' in value) {
