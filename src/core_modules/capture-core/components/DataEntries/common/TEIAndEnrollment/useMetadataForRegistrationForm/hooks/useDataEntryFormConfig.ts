6352214e4130145b661b0cc6d960c714b0b95f7d
Simona Domnisoru <simona.domnisoru@gmail.com>
2025-12-11 14:56:34 +0100

    fix: [DHIS2-20646] handle missing capture namespace in DataStore (#4418)

--- src/core_modules/capture-core/components/DataEntries/common/TEIAndEnrollment/useMetadataForRegistrationForm/hooks/useDataEntryFormConfig.ts
+++ src/core_modules/capture-core/components/DataEntries/common/TEIAndEnrollment/useMetadataForRegistrationForm/hooks/useDataEntryFormConfig.ts
@@ -1,3 +1,6 @@
+import log from 'loglevel';
+import { errorCreator } from 'capture-core-utils';
+import { useEffect } from 'react';
 import { useApiMetadataQuery } from 'capture-core/utils/reactQueryHelpers';
 
 type Props = {
@@ -11,7 +14,9 @@ const configQuery = {
 export const useDataEntryFormConfig = ({ selectedScopeId }: Props) => {
     const {
         data: configExists,
-        isLoading: namespaceIsLoading,
+        isError: namespaceIsError,
+        isFetched: namespaceIsFetched,
+        error: namespaceError,
     } = useApiMetadataQuery<any>(
         ['dataStore', 'capture'],
         { resource: 'dataStore/capture' },
@@ -20,23 +25,33 @@ export const useDataEntryFormConfig = ({ selectedScopeId }: Props) => {
 
     const {
         data: dataEntryFormConfig,
-        isFetched: configIsFetched,
-        isInitialLoading,
+        isError,
+        isFetched,
+        error,
     } = useApiMetadataQuery(['dataEntryFormConfig', selectedScopeId], configQuery, {
-        enabled: !!configExists && !!selectedScopeId,
+        enabled: !!(configExists && selectedScopeId),
         select: (dataEntryFormConfigQuery: any) => dataEntryFormConfigQuery?.[selectedScopeId] ?? null,
     });
 
-    if (!namespaceIsLoading && configExists === false) {
-        return {
+    useEffect(() => {
+        if (namespaceIsError) {
+            log.error(errorCreator('capture namespace could not be fetched from the datastore')({ namespaceError }));
+        }
+        if (isError) {
+            log.error(errorCreator('dataEntryForms namespace could not be fetched from the datastore')({ error }));
+        }
+    }, [isError, error, namespaceIsError, namespaceError]);
+
+    const configIsFetched = (namespaceIsFetched && !configExists) || isFetched;
+
+    return namespaceIsError || isError
+        ? {
             dataEntryFormConfig: null,
             configIsFetched: true,
+            isLoading: false,
+        }
+        : { dataEntryFormConfig,
+            configIsFetched,
+            isLoading: !configIsFetched,
         };
-    }
-
-    return {
-        dataEntryFormConfig,
-        configIsFetched,
-        isLoading: isInitialLoading,
-    };
 };
