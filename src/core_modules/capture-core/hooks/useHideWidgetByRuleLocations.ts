c117c9b3008c2373f69af47cc2d6cb128872727d
henrikmv <henrik@devOtta.no>
2026-01-19 12:16:28 +0100

    fix: sonar qube

--- src/core_modules/capture-core/hooks/useHideWidgetByRuleLocations.ts
+++ src/core_modules/capture-core/hooks/useHideWidgetByRuleLocations.ts
@@ -1,13 +1,15 @@
 import { useMemo } from 'react';
 
 export const useHideWidgetByRuleLocations = (programRules: any) => useMemo(() => {
-    const ruleLocations = programRules.map((item: any) => item.programRuleActions
-        .map((rule: any) => rule.location ?? null))
-        .flat();
+    const ruleLocations = new Set(
+        programRules.flatMap((item: any) =>
+            item.programRuleActions.map((rule: any) => rule.location ?? null),
+        ),
+    );
 
     const hideWidgets: any = {};
-    hideWidgets.feedback = !ruleLocations.includes('feedback');
-    hideWidgets.indicator = !ruleLocations.includes('indicators');
+    hideWidgets.feedback = !ruleLocations.has('feedback');
+    hideWidgets.indicator = !ruleLocations.has('indicators');
 
     return hideWidgets;
 }, [programRules]);
