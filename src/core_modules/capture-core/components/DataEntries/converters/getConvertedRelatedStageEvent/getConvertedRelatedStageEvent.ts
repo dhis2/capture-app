949a82c69fcb01ab50550866b6c087416847c9e4
henrikmv <henrik@devOtta.no>
2026-01-28 18:44:07 +0100

    feat: clean up comments

--- src/core_modules/capture-core/components/DataEntries/converters/getConvertedRelatedStageEvent/getConvertedRelatedStageEvent.ts
+++ src/core_modules/capture-core/components/DataEntries/converters/getConvertedRelatedStageEvent/getConvertedRelatedStageEvent.ts
@@ -38,7 +38,6 @@ const getEventDetailsByLinkMode = ({
     if (linkMode === relatedStageActions.SCHEDULE_IN_ORG) {
         const { scheduledAt: linkedEventScheduledAt, orgUnit: linkedEventOrgUnit } = relatedStageDataValues;
         if (!linkedEventScheduledAt || !linkedEventOrgUnit) {
-            // Business logic dictates that these values will not be null here
             throw new Error(
                 errorCreator('Missing required data for creating related stage event')({
                     linkedEventOrgUnit,
@@ -78,8 +77,6 @@ const getEventDetailsByLinkMode = ({
 
     if (linkMode === relatedStageActions.LINK_EXISTING_RESPONSE) {
         const { linkedEventId } = relatedStageDataValues;
-        // Validerer at linkedEventId er tilstede for å unngå feil når vi prøver å koble til et eksisterende event
-        // Uten denne valideringen kan koden fortsette med null/undefined linkedEventId, noe som forårsaker feil senere
         if (!linkedEventId) {
             throw new Error(
                 errorCreator('Missing required data for creating related stage event')({
