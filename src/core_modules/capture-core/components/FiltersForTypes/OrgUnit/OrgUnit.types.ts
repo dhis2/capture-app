36544f393bf78251d44f7ab5ecdfe26baf010cf4
henrikmv <henrik@devOtta.no>
2026-03-16 13:51:52 +0100

    fix: org unit selector clear without close function

--- src/core_modules/capture-core/components/FiltersForTypes/OrgUnit/OrgUnit.types.ts
+++ src/core_modules/capture-core/components/FiltersForTypes/OrgUnit/OrgUnit.types.ts
@@ -5,5 +5,6 @@ export type Value = OrgUnitValue | null | undefined;
 export type OrgUnitFilterProps = {
     onCommitValue: (value: Value) => void;
     onUpdate: (updatedValue: Value) => void;
+    onClearValue?: () => void;
     value: Value;
 };
--- src/core_modules/capture-core/components/FiltersForTypes/OrgUnit/OrgUnitFilter.component.tsx
+++ src/core_modules/capture-core/components/FiltersForTypes/OrgUnit/OrgUnitFilter.component.tsx
@@ -13,7 +13,11 @@ export class OrgUnitFilter extends Component<OrgUnitFilterProps> implements Upda
 
     handleOrgUnitChange = (value: OrgUnitValue | null) => {
         this.props.onCommitValue(value);
-        this.props.onUpdate(value);
+        if (value === null && this.props.onClearValue) {
+            this.props.onClearValue();
+        } else {
+            this.props.onUpdate(value);
+        }
     };
 
     render() {
