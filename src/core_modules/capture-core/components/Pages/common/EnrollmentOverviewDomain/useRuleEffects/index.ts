f21ee948fede783de4b3f8d44b730b2545959726
henrikmv <110386561+henrikmv@users.noreply.github.com>
2026-09-01 12:17:36 +0000

    fix: (review) remove unused rule effects logic and types

--- src/core_modules/capture-core/components/Pages/common/EnrollmentOverviewDomain/useRuleEffects/index.ts
+++ src/core_modules/capture-core/components/Pages/common/EnrollmentOverviewDomain/useRuleEffects/index.ts
@@ -1,4 +1,2 @@
-export { useRuleEffects } from './useRuleEffects';
 export { useEnrollmentScopeRuleEffects } from './useEnrollmentScopeRuleEffects';
 export { getEnrollmentScopeFormId } from './getEnrollmentScopeFormId';
-export type { UseRuleEffectsInput } from './useRuleEffects.types';
